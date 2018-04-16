4b091b7a4e7b1c6678b8f0aea86863562e380d2f
Edouard360 <mailedouardmehlman@gmail.com>
2018-04-16 15:56:15 -0700

    10x genomics dataset(verified )

--- scvi/dataset/__init__.py
+++ scvi/dataset/__init__.py
@@ -1,11 +1,13 @@
 import numpy as np
 
+from .brain_large import BrainLargeDataset
 from .cortex import CortexDataset
 from .dataset import GeneExpressionDataset
 from .synthetic import SyntheticDataset
 
 __all__ = ['SyntheticDataset',
            'CortexDataset',
+           'BrainLargeDataset',
            'GeneExpressionDataset']
 
 
@@ -14,6 +16,9 @@ def load_datasets(dataset_name):
         gene_dataset_train, gene_dataset_test = SyntheticDataset(), SyntheticDataset()
     elif dataset_name == 'cortex':
         gene_dataset_train, gene_dataset_test = CortexDataset(type="train"), CortexDataset(type="test")
+    elif dataset_name == 'brain_large':
+        gene_dataset = BrainLargeDataset()
+        gene_dataset_train, gene_dataset_test = gene_dataset, gene_dataset  # Return same object for now
     elif dataset_name.endswith('.npy'):
         data = np.load(dataset_name)
         train_data, test_data = GeneExpressionDataset.train_test_split(data)
