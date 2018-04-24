168c39267a79f5b0f2dce401752ffd3c3bf4aaa3
maxime1310 <maxime.langevin@polytechnique.edu>
2018-04-24 14:45:35 -0700

    Added an early stopping criterion. (#73)

--- scvi/train.py
+++ scvi/train.py
@@ -4,12 +4,23 @@ from torch.autograd import Variable
 from scvi.log_likelihood import compute_log_likelihood
 
 
-def train(vae, data_loader_train, data_loader_test, n_epochs=20, learning_rate=0.001, kl=None):
+def train(vae, data_loader_train, data_loader_test, n_epochs=20, learning_rate=0.001, kl=None,
+          early_stopping_criterion=(20, 0.01)):
+
     # Defining the optimizer
     optimizer = torch.optim.Adam(vae.parameters(), lr=learning_rate, eps=0.01)
 
+    # initialize
+    (patience, threshold) = (early_stopping_criterion[0], early_stopping_criterion[1])
+    current_performances_kl = torch.ones(patience)
+    current_performances_reconst = torch.ones(patience)
+
     # Training the model
     for epoch in range(n_epochs):
+        # initialize kl, reconst
+        total_current_kl = 0
+        total_current_reconst = 0
+
         for i_batch, (sample_batch, local_l_mean, local_l_var, batch_index, _) in enumerate(data_loader_train):
 
             sample_batch = Variable(sample_batch)
@@ -33,15 +44,40 @@ def train(vae, data_loader_train, data_loader_test, n_epochs=20, learning_rate=0
             else:
                 train_loss, reconst_loss, kl_divergence = vae.loss(
                     sample_batch, local_l_mean, local_l_var, kl_ponderation)
+
+            total_current_kl += torch.mean(kl_divergence)
+            total_current_reconst += torch.mean(reconst_loss)
+
             real_loss = reconst_loss + kl_divergence
             optimizer.zero_grad()
             train_loss.backward()
             optimizer.step()
 
+        current_performances_kl[:-1] = current_performances_kl[1:]
+        current_performances_reconst[:-1] = current_performances_reconst[1:]
+        current_performances_kl[-1] = total_current_kl.data[0]
+        current_performances_reconst[-1] = total_current_reconst.data[0]
+
+        # Computing the relative improvment of kl and reconstruction loss
+        # over the chosen number of epochs
+        reconst_relative_improvement = ((current_performances_reconst[0] - current_performances_reconst[-1])
+                                        / current_performances_reconst[0])
+        kl_relative_improvement = ((current_performances_kl[0] - current_performances_kl[-1])
+                                   / current_performances_kl[0])
+        # Test whether stopping criterions are met
+        if epoch > patience and kl_relative_improvement < threshold and reconst_relative_improvement < threshold:
+            # We then stop the iterations
+            print("Stopping the training after %d epochs: over the %d last epochs,"
+                  " kl divergence improvement was %.4f, reconstruction loss improvement was %.4f"
+                  % (epoch, patience, kl_relative_improvement, reconst_relative_improvement))
+
+            break
+
             # Simply printing the results
         if epoch % 10 == 0:
             vae.eval()
-            log_likelihood_train = compute_log_likelihood(vae, data_loader_train)
+            # No need to compute it twice
+            log_likelihood_train = total_current_reconst.data[0] + total_current_kl.data[0]
             log_likelihood_test = compute_log_likelihood(vae, data_loader_test)
             print("Epoch[%d/%d], LL-Train %.4f, LL-Test %.4f, Total Loss: %.4f, "
                   % (epoch + 1, n_epochs, log_likelihood_train, log_likelihood_test, real_loss.data[0]))
