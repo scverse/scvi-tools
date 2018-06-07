33d64c01665770121af9fc31620b1ab7aa9725eb
Eddie <mailedouardmehlman@gmail.com>
2018-06-07 10:17:55 -0700

    retina was changed properly on branch metrics_and_baselines but not yet merged (#111)

--- scvi/dataset/retina.py
+++ scvi/dataset/retina.py
@@ -5,7 +5,7 @@ from . import GeneExpressionDataset
 
 
 class RetinaDataset(GeneExpressionDataset):
-    def __init__(self, unit_test=True):
+    def __init__(self, unit_test=False):
 
         if unit_test:
             self.save_path = 'tests/data/'
@@ -14,19 +14,20 @@ class RetinaDataset(GeneExpressionDataset):
         self.data_filename = 'retina_counts.npz'
         self.labels_filename = 'retina_labels.npy'
         self.batches_filename = 'retina_batch.npy'
-        cell_batches = np.reshape((np.load(self.save_path + self.batches_filename)), (5, 1))
-        cell_batches[0] = 1
         if unit_test:
-            labels = np.reshape((np.load(self.save_path + self.labels_filename)), (5, 1))
+            cell_batches = np.reshape((np.load(self.save_path + self.batches_filename)), (5, 1))
+            cell_batches[0] = 1
+            # labels = np.reshape((np.load(self.save_path + self.labels_filename)), (5, 1))
             data = np.load(self.save_path + self.data_filename)['data']
             data = np.reshape(data, (5, 13166))
             data = data.repeat(100, 0)
             cell_batches = cell_batches.repeat(100, 0)
             labels = np.arange(5).reshape(5, 1).repeat(100, 0)  # np.arange(1,5)#labels.repeat(100, 0)
         else:
-            cell_batches = np.reshape((np.loadtxt(self.save_path + self.batches_filename) - 1), (19829, 1))
-            labels = np.reshape((np.loadtxt(self.save_path + self.labels_filename)), (19829, 1))
+            cell_batches = np.reshape((np.load(self.save_path + self.batches_filename)), (19829, 1))
+            labels = np.reshape((np.load(self.save_path + self.labels_filename)), (19829, 1))
             data = sp_sparse.load_npz(self.save_path + self.data_filename).toarray()
+
         data_with_info = np.hstack((data, labels, cell_batches))
         first_batch = data_with_info[data_with_info[:, -1] == 0.0]
         second_batch = data_with_info[data_with_info[:, -1] == 1.0]
