a55338a29b81c57db150f68f83798dfa89bc09b1
imyiningliu <lyining@berkeley.edu>
2018-06-12 11:35:35 -0700

    update version to 0.1.0

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/romain-lopez/scvi',
-    version='0.2.0',
+    version='0.1.0',
     zip_safe=False,
 )
