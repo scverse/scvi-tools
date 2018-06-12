b77063622db8cfffbd3c9d55e85655f88473f116
imyiningliu <lyining@berkeley.edu>
2018-06-12 13:42:12 -0700

    update with the correct url

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/romain-lopez/scvi',
+    url='https://github.com/YosefLab/scVI',
     version='0.1.0',
     zip_safe=False,
 )
