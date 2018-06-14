e41758a54fa392de93545782bba7c65e34d8954b
Jeffrey Regier <regier@gmail.com>
2018-06-13 19:04:54 -0700

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/YosefLab/scVI',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
