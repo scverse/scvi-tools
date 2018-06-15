d21992b4f63c0b4508dc73f63b3cab7c28d1faa2
imyiningliu <lyining@berkeley.edu>
2018-06-14 22:02:58 -0700

    fix run_benchmarks.py

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -4,6 +4,7 @@
 import argparse
 
 from scvi.benchmark import run_benchmarks
+from scvi.dataset import load_datasets
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -14,8 +15,13 @@ if __name__ == '__main__':
                         help="whether to use cuda (will apply only if cuda is available")
     parser.add_argument("--benchmark", action='store_true',
                         help="whether to use cuda (will apply only if cuda is available")
-    parser.add_argument("--url", type=str, help="the url for downloading dataset")
+    parser.add_argument("--url", help="the url for downloading dataset")
+    parser.add_argument("--gene_row", help="the row name for genes")
+    parser.add_argument("--cluster_col", help="the column name for cell types")
+    parser.add_argument("--batch_col", help="the column name for batches")
     args = parser.parse_args()
 
-    run_benchmarks(args.dataset, n_epochs=args.epochs, use_batches=(not args.nobatches), use_cuda=(not args.nocuda),
-                   show_batch_mixing=True, benchmark=args.benchmark, url=args.url)
+    dataset = load_datasets(args.dataset, gene_row=args.gene_row,
+                            cluster_col=args.cluster_col, batch_col=args.batch_col, url=args.url)
+    run_benchmarks(dataset, n_epochs=args.epochs, use_batches=(not args.nobatches), use_cuda=(not args.nocuda),
+                   show_batch_mixing=True, benchmark=args.benchmark)
--- scvi/dataset/loom.py
+++ scvi/dataset/loom.py
@@ -6,7 +6,7 @@ import scipy.sparse as sp_sparse
 
 class LoomDataset(GeneExpressionDataset):
 
-    def __init__(self, filename, save_path, gene_row=False, cluster_col=False, batch_col=False, url=False):
+    def __init__(self, filename, save_path, gene_row=None, cluster_col=None, batch_col=None, url=None):
         self.download_name = filename
         self.save_path = save_path
         self.gene_row = gene_row
@@ -17,7 +17,7 @@ class LoomDataset(GeneExpressionDataset):
         data, gene_names, labels, cell_batches = self.download_and_preprocess()
         print("Finished preprocessing dataset")
 
-        if cluster_col is not False and batch_col is not False:  # File has cluster and batch info
+        if cluster_col is not None and batch_col is not None:  # File has cluster and batch info
             data_with_info = np.hstack((data, labels, cell_batches))
             first_batch = data_with_info[data_with_info[:, -1] == 0.0]
             second_batch = data_with_info[data_with_info[:, -1] == 1.0]
@@ -26,7 +26,7 @@ class LoomDataset(GeneExpressionDataset):
             super(LoomDataset, self).__init__(*GeneExpressionDataset.get_attributes_from_list(
                 [sp_sparse.csr_matrix(first_batch[:, :-1]), sp_sparse.csr_matrix(second_batch[:, :-1])],
                 list_labels=[first_batch[:, -1], second_batch[:, -1]]))
-        elif gene_names is not False:  # File has gene names info
+        elif gene_names is not None:  # File has gene names info
             super(LoomDataset, self).__init__(
                 *GeneExpressionDataset.get_attributes_from_matrix(
                     data, labels=labels), gene_names=gene_names)
