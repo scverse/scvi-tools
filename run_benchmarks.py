5e43e3989fbdb895d2cfe99234664644452efd3d
Yining Liu <lyining@berkeley.edu>
2018-06-16 10:39:05 -0700

    yl/remote loom (#36)
    * test loom with remote loom file (FISH)
    
    * fix run_benchmarks.py
    
    * change False to None
    
    * change fish to lower case
    
    * remove extra parameters
    
    * works with fish dataset
    
    * create unit test file for Cortex.loom
    
    * remove creating unit test code
    
    * update unit test file

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -4,6 +4,7 @@
 import argparse
 
 from scvi.benchmark import run_benchmarks
+from scvi.dataset import load_datasets
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -14,7 +15,9 @@ if __name__ == '__main__':
                         help="whether to use cuda (will apply only if cuda is available")
     parser.add_argument("--benchmark", action='store_true',
                         help="whether to use cuda (will apply only if cuda is available")
+    parser.add_argument("--url", type=str, help="the url for downloading dataset")
     args = parser.parse_args()
 
-    run_benchmarks(args.dataset, n_epochs=args.epochs, use_batches=(not args.nobatches), use_cuda=(not args.nocuda),
+    dataset = load_datasets(args.dataset, url=args.url)
+    run_benchmarks(dataset, n_epochs=args.epochs, use_batches=(not args.nobatches), use_cuda=(not args.nocuda),
                    show_batch_mixing=True, benchmark=args.benchmark)
--- scvi/dataset/loom.py
+++ scvi/dataset/loom.py
@@ -3,35 +3,58 @@ import loompy
 import numpy as np
 import scipy.sparse as sp_sparse
 
+gene_row = 'Gene'
+batch_col = 'BatchID'
+cluster_col = 'ClusterID'
+
 
 class LoomDataset(GeneExpressionDataset):
 
-    def __init__(self, filename, save_path):
-        self.filename = filename
+    def __init__(self, filename, save_path, url=None):
+        self.download_name = filename
         self.save_path = save_path
+        self.url = url
 
-        data, labels, cell_batches = self.preprocess()
+        self.has_gene, self.has_batch, self.has_cluster = False, False, False
 
-        # Currently only supports retina.loom dataset
-        data_with_info = np.hstack((data, labels, cell_batches))
-        first_batch = data_with_info[data_with_info[:, -1] == 0.0]
-        second_batch = data_with_info[data_with_info[:, -1] == 1.0]
-        first_batch = first_batch[:, :-1]
-        second_batch = second_batch[:, :-1]
+        data, gene_names, labels, cell_batches = self.download_and_preprocess()
         print("Finished preprocessing dataset")
-        super(LoomDataset, self).__init__(*GeneExpressionDataset.get_attributes_from_list(
-            [sp_sparse.csr_matrix(first_batch[:, :-1]), sp_sparse.csr_matrix(second_batch[:, :-1])],
-            list_labels=[first_batch[:, -1], second_batch[:, -1]]))
+
+        if self.has_cluster and self.has_batch:  # File has cluster and batch info
+            data_with_info = np.hstack((data, labels, cell_batches))
+            first_batch = data_with_info[data_with_info[:, -1] == 0.0]
+            second_batch = data_with_info[data_with_info[:, -1] == 1.0]
+            first_batch = first_batch[:, :-1]
+            second_batch = second_batch[:, :-1]
+            super(LoomDataset, self).__init__(*GeneExpressionDataset.get_attributes_from_list(
+                [sp_sparse.csr_matrix(first_batch[:, :-1]), sp_sparse.csr_matrix(second_batch[:, :-1])],
+                list_labels=[first_batch[:, -1], second_batch[:, -1]]))
+        elif self.has_gene:  # File has gene names info
+            super(LoomDataset, self).__init__(
+                *GeneExpressionDataset.get_attributes_from_matrix(
+                    data, labels=labels), gene_names=gene_names)
 
     def preprocess(self):
-        ds = loompy.connect(self.save_path + self.filename)
-        cell_batches = ds.ca['Batch_id']
-        cell_batches = np.reshape(cell_batches, (cell_batches.shape[0], 1))
+        gene_names, labels, cell_batches = None, None, None
+        ds = loompy.connect(self.save_path + self.download_name)
+        select = ds[:, :].sum(axis=0) > 0  # Take out cells that doesn't express any gene
 
-        labels = np.array(ds.ca['Clusters'])
-        labels = np.reshape(labels, (labels.shape[0], 1))
+        self.has_gene = gene_row in ds.ra.keys()
+        self.has_batch = batch_col in ds.ca.keys()
+        self.has_cluster = cluster_col in ds.ca.keys()
 
-        data = np.array(ds[:, :]).T  # change matrix to cells by genes
+        if self.has_gene:
+            gene_names = ds.ra[gene_row]
+
+        if self.has_batch:
+            cell_batches = ds.ca[batch_col]
+            cell_batches = np.reshape(cell_batches, (cell_batches.shape[0], 1))[select]
+
+        if self.has_cluster:
+            labels = np.array(ds.ca[cluster_col])
+            labels = np.reshape(labels, (labels.shape[0], 1))[select]
+
+        data = ds[:, select].T  # change matrix to cells by genes
         ds.close()
 
-        return data, labels, cell_batches
+        return data, gene_names, labels, cell_batches
