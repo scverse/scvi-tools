3ebe22fd92ddc4a7ea1593f1a1934fdb23d9a554
Yining Liu <lyining@berkeley.edu>
2018-06-27 13:45:45 -0700

    fix hemato (#65)

--- scvi/dataset/hemato.py
+++ scvi/dataset/hemato.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from zipfile import ZipFile
 from .dataset import GeneExpressionDataset
+from pathlib import Path
 
 
 class HematoDataset(GeneExpressionDataset):
@@ -27,7 +28,7 @@ class HematoDataset(GeneExpressionDataset):
         print("Preprocessing Hemato data")
 
         with ZipFile(self.save_path + 'data.zip', 'r') as zip:
-            zip.extractall(path=self.save_path)
+            zip.extractall(path=Path(self.save_path).parent)
 
         raw_counts = pd.read_csv(self.save_path + self.download_names[0], compression='gzip')
         raw_counts.drop(raw_counts.index[raw_counts["library_id"] == "basal_bm1"], inplace=True)
