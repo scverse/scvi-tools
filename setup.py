704ce19d80e5728542464e488968c136a241eb6f
Yining Liu <lyining@berkeley.edu>
2018-06-29 14:28:30 -0700

    Bump version: 0.1.3 → 0.1.4 (#74)

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/YosefLab/scVI',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
