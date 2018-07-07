7ef56191c9110e77d46a8dd1eeb4f0af90aa41b6
Eddie <mailedouardmehlman@gmail.com>
2018-07-07 11:12:40 -0700

    harmonization for n_layer==1 (#89)
    - sequential layers are [dropout(0.1), linear, batchnorm, relu]. Before, we concatenated [latent_features, one_hot_batch_index] as input to the sequential layer decoder, therefore submitting one_hot_batch_index to the dropout as well, and giving poor batch mixing entropy results. We customize the forward function to circumvent that problem.
    
    - also simplifying the modules surrounding FCLayers. The additional feature is the ability to easily add edges from categorical variables in the graphical model, by specifying n_cat in the construct for each variable as a list (n_cat_list), and providing them as a sequence in the forward function.

--- scvi/models/svaec.py
+++ scvi/models/svaec.py
@@ -24,13 +24,12 @@ class SVAEC(nn.Module, SemiSupervisedModel):
 
         self.y_prior = y_prior if y_prior is not None else (1 / self.n_labels) * torch.ones(self.n_labels)
         # Automatically desactivate if useless
-        self.n_batch = 0 if n_batch == 1 else n_batch
-        self.z_encoder = Encoder(n_input, n_hidden=n_hidden, n_latent=n_latent, n_layers=n_layers,
+        self.n_batch = n_batch
+        self.z_encoder = Encoder(n_input, n_latent=n_latent, n_layers=n_layers, n_hidden=n_hidden,
                                  dropout_rate=dropout_rate)
-        self.l_encoder = Encoder(n_input, n_hidden=n_hidden, n_latent=1, n_layers=1,
-                                 dropout_rate=dropout_rate)
-        self.decoder = DecoderSCVI(n_latent, n_input, n_hidden=n_hidden, n_layers=n_layers,
-                                   dropout_rate=dropout_rate, n_batch=n_batch)
+        self.l_encoder = Encoder(n_input, n_latent=1, n_layers=1, n_hidden=n_hidden, dropout_rate=dropout_rate)
+        self.decoder = DecoderSCVI(n_latent, n_input, n_cat_list=[n_batch], n_layers=n_layers, n_hidden=n_hidden,
+                                   dropout_rate=dropout_rate)
 
         self.dispersion = 'gene'
         self.px_r = torch.nn.Parameter(torch.randn(n_input, ))
@@ -41,8 +40,10 @@ class SVAEC(nn.Module, SemiSupervisedModel):
         else:
             self.classifier = Classifier(n_latent, n_hidden, self.n_labels, n_layers, dropout_rate)
 
-        self.encoder_z2_z1 = Encoder(n_input=n_latent, n_cat=self.n_labels, n_latent=n_latent, n_layers=n_layers)
-        self.decoder_z1_z2 = Decoder(n_latent, n_latent, n_cat=self.n_labels, n_layers=n_layers)
+        self.encoder_z2_z1 = Encoder(n_latent, n_latent, n_cat_list=[self.n_labels], n_layers=n_layers,
+                                     n_hidden=n_hidden, dropout_rate=dropout_rate)
+        self.decoder_z1_z2 = Decoder(n_latent, n_latent, n_cat_list=[self.n_labels], n_layers=n_layers,
+                                     n_hidden=n_hidden, dropout_rate=dropout_rate)
 
         self.use_cuda = use_cuda and torch.cuda.is_available()
         if self.use_cuda:
@@ -77,15 +78,15 @@ class SVAEC(nn.Module, SemiSupervisedModel):
         return library
 
     def get_sample_scale(self, x, y=None, batch_index=None):
-        z = self.sample_from_posterior_z(x, y)
-        px = self.decoder.px_decoder(z, batch_index, y)
+        z = self.sample_from_posterior_z(x)
+        px = self.decoder.px_decoder(z, batch_index)
         px_scale = self.decoder.px_scale_decoder(px)
         return px_scale
 
     def get_sample_rate(self, x, y=None, batch_index=None):
         z = self.sample_from_posterior_z(x)
         library = self.sample_from_posterior_l(x)
-        px = self.decoder.px_decoder(z, batch_index, y)
+        px = self.decoder.px_decoder(z, batch_index)
         return self.decoder.px_scale_decoder(px) * torch.exp(library)
 
     def forward(self, x, local_l_mean, local_l_var, batch_index=None, y=None):
--- scvi/models/vae.py
+++ scvi/models/vae.py
@@ -23,7 +23,7 @@ class VAE(nn.Module, BaseModel):
         self.log_variational = log_variational
         self.reconstruction_loss = reconstruction_loss
         # Automatically desactivate if useless
-        self.n_batch = 0 if n_batch == 1 else n_batch
+        self.n_batch = n_batch
         self.n_labels = n_labels
         self.n_latent_layers = 1
 
@@ -36,12 +36,11 @@ class VAE(nn.Module, BaseModel):
         else:  # gene-cell
             pass
 
-        self.z_encoder = Encoder(n_input, n_hidden=n_hidden, n_latent=n_latent, n_layers=n_layers,
+        self.z_encoder = Encoder(n_input, n_latent=n_latent, n_layers=n_layers, n_hidden=n_hidden,
                                  dropout_rate=dropout_rate)
-        self.l_encoder = Encoder(n_input, n_hidden=n_hidden, n_latent=1, n_layers=1,
-                                 dropout_rate=dropout_rate)
-        self.decoder = DecoderSCVI(n_latent, n_input, n_hidden=n_hidden, n_layers=n_layers,
-                                   dropout_rate=dropout_rate, n_batch=n_batch)
+        self.l_encoder = Encoder(n_input, n_latent=1, n_layers=1, n_hidden=n_hidden, dropout_rate=dropout_rate)
+        self.decoder = DecoderSCVI(n_latent, n_input, n_cat_list=[n_batch], n_layers=n_layers, n_hidden=n_hidden,
+                                   dropout_rate=dropout_rate)
 
         self.use_cuda = use_cuda and torch.cuda.is_available()
         if self.use_cuda:
