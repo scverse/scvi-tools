04c968bd382561bccb00f5494f556c193625d5f7
Jeffrey Regier <regier@gmail.com>
2018-07-19 10:38:53 -0700

    closes #109 ; add xlrd to setup.py; tox tests pass now

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ requirements = [
     "pandas>=0.2",
     "loompy>=2.0",
     "tqdm >= 4",
-    "anndata >= 0.6"
+    "anndata >= 0.6",
+    "xlrd >= 0.9",
 ]
 
 setup_requirements = ['pytest-runner', ]
