adcb5035bd9884f0c52675f484c6465e69da3523
Eddie <mailedouardmehlman@gmail.com>
2018-07-19 15:25:35 -0700

    resolves #106 (#115)

--- scvi/inference/variational_inference.py
+++ scvi/inference/variational_inference.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
+from sklearn.metrics import adjusted_rand_score as ARI
+from sklearn.metrics import normalized_mutual_info_score as NMI
+from sklearn.metrics import silhouette_score
 from torch.nn import functional as F
 
 from scvi.dataset import CortexDataset
@@ -9,7 +13,7 @@ from scvi.dataset.data_loaders import DataLoaders
 from scvi.dataset.data_loaders import TrainTestDataLoaders, AlternateSemiSupervisedDataLoaders, \
     JointSemiSupervisedDataLoaders
 from scvi.metrics.classification import compute_accuracy, compute_accuracy_svc, compute_accuracy_rf
-from scvi.metrics.clustering import get_latent, entropy_batch_mixing
+from scvi.metrics.clustering import get_latent, entropy_batch_mixing, nn_overlap
 from scvi.metrics.differential_expression import de_stats, de_cortex
 from scvi.metrics.imputation import imputation
 from scvi.metrics.log_likelihood import compute_log_likelihood
@@ -70,6 +74,29 @@ class VariationalInference(Inference):
         return imputation_score
 
     imputation.mode = 'min'
+
+    def clustering_scores(self, name, verbose=True):
+        if self.gene_dataset.n_labels > 1:
+            latent, _, labels = get_latent(self.model, self.data_loaders[name])
+            labels_pred = KMeans(self.gene_dataset.n_labels, n_init=200).fit_predict(latent)  # n_jobs>1 ?
+            asw_score = silhouette_score(latent, labels)
+            nmi_score = NMI(labels, labels_pred)
+            ari_score = ARI(labels, labels_pred)
+            if verbose:
+                print("Clustering Scores for %s:\nSilhouette: %.4f\nNMI: %.4f\nARI: %.4f" %
+                      (name, asw_score, nmi_score, ari_score))
+            return asw_score, nmi_score, ari_score
+
+    def nn_overlap_score(self, name='sequential', verbose=True, **kwargs):
+        if hasattr(self.gene_dataset, 'adt_expression_clr'):
+            assert name == 'sequential'  # only works for the sequential data_loader (mapping indices)
+            latent, _, _ = get_latent(self.model, self.data_loaders[name])
+            protein_data = self.gene_dataset.adt_expression_clr
+            spearman_correlation, fold_enrichment = nn_overlap(latent, protein_data, **kwargs)
+            if verbose:
+                print("Overlap Scores for %s:\nSpearman Correlation: %.4f\nFold Enrichment: %.4f" %
+                      (name, spearman_correlation, fold_enrichment))
+            return spearman_correlation, fold_enrichment
 
     def differential_expression_stats(self, name, *args, **kwargs):
         return de_stats(self.model, self.data_loaders[name], *args, **kwargs)
