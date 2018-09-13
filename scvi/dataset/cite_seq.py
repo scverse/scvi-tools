3492b7d97bbd3017226606e1cabacd73f4aba670
adamgayoso <adamgayoso@users.noreply.github.com>
2018-09-13 15:42:22 -0700

    allows a dataset of rna+adt or just rna and retain high var genes

--- scvi/dataset/cite_seq.py
+++ scvi/dataset/cite_seq.py
@@ -82,13 +82,14 @@ class CbmcDataset(CiteSeqDataset):
         >>> gene_dataset = CbmcDataset()
 
     """
-    def __init__(self, save_path='data/citeSeq/', additional_genes=600):
+    def __init__(self, save_path='data/citeSeq/', additional_genes=600, mode='total'):
         self.additional_genes = additional_genes
         super(CbmcDataset, self).__init__(name="cbmc", save_path=save_path)
 
         # This maintains the library statistics for just umi counts
-        self._X = np.ascontiguousarray(np.concatenate([self._X, self.adt_expression], axis=1), dtype=np.float32)
-        self.indexes_adt = np.arange(self._X.shape[1])[-len(self.protein_markers):]
+        if mode == 'total':
+            self._X = np.ascontiguousarray(np.concatenate([self._X, self.adt_expression], axis=1), dtype=np.float32)
+            self.indexes_adt = np.arange(self._X.shape[1])[-len(self.protein_markers):]
 
     def preprocess(self):
         print("Preprocessing cbmc data")
@@ -102,10 +103,11 @@ class CbmcDataset(CiteSeqDataset):
         expression_data = expression.values[:, human_filter]
 
         # Keep top 600 genes by variance as in scVI paper
-        print('Keeping top {} genes by variance'.format(self.additional_genes))
-        gene_vars = np.var(expression_data, axis=0)
-        gene_inds = np.argsort(gene_vars)[-self.additional_genes:]
-        expression_data = expression_data[:, gene_inds]
+        if self.additional_genes is not None:
+            print('Keeping top {} genes by variance'.format(self.additional_genes))
+            gene_vars = np.var(expression_data, axis=0)
+            gene_inds = np.argsort(gene_vars)[-self.additional_genes:]
+            expression_data = expression_data[:, gene_inds]
 
         # Necessary so we know which cells in adt counts to keep (could be improved)
         to_keep = np.array((expression_data.sum(axis=1) > 0)).ravel()
@@ -116,13 +118,13 @@ class CbmcDataset(CiteSeqDataset):
             np.array([name.split('_')[-1] if '_' in name else name for name in gene_symbols], dtype=np.str)
         )
         # ADTs
-        self.adt = adt = pd.read_csv(self.save_path + self.download_name_adt, index_col=0, compression='gzip')
+        self.adt = pd.read_csv(self.save_path + self.download_name_adt, index_col=0, compression='gzip')
         # Remove CCR5, CCR7, and CD10 due to poor enrichments
         # as done in https://satijalab.org/seurat/multimodal_vignette.html
         self.adt = self.adt.drop(['CCR5', 'CCR7', 'CD10'], axis=0)
         self.adt = self.adt.loc[:, to_keep]
-        self.adt_expression = adt.T.values
-        self.protein_markers = np.array(adt.index).astype(np.str)
+        self.adt_expression = self.adt.T.values
+        self.protein_markers = np.array(self.adt.index).astype(np.str)
 
         print("Finish preprocessing data")
         return expression_data
