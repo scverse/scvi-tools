74d8c0854692a10168395291662f44a5e3889cf9
adamgayoso <adamgayoso@users.noreply.github.com>
2018-09-13 15:42:48 -0700

    adds VAECITE to init

--- scvi/models/__init__.py
+++ scvi/models/__init__.py
@@ -3,9 +3,11 @@ from .scanvi import SCANVI
 from .vae import VAE
 from .vae_fish import VAEF
 from .vaec import VAEC
+from .vae_cite import VAECITE
 
 __all__ = ['SCANVI',
            'VAEC',
            'VAE',
            'VAEF',
+           'VAECITE',
            'Classifier']
