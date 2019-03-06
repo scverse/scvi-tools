c2c1b8afa35d0747f42dc1cfc494a31448f56d4b
Romain <romain_lopez@berkeley.edu>
2019-03-05 17:12:55 -0800

    Bump version: 0.2.4 → 0.3.0

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/YosefLab/scVI',
-    version='0.2.4',
+    version='0.3.0',
     zip_safe=False,
 )
