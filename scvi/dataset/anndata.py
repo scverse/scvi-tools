eeafb7881fddbb9d0b337c74bb0ff156bc0bd033
Achille Nazaret <achille.nazaret@gmail.com>
2019-04-09 10:00:55 -0700

    Update AnnDataset loader to accept AnnData objects
    * The AnnDataset constructor used to only read file dumps of AnnData.
      It now accepts AnnData objects and wraps them into scvi AnnDataset
    
    * Also fix the GeneExpressionDataset class which now implements the
      preprocess method (it was calling it in download_and_preprocess,
      hoping that the child class implemented it)

--- scvi/dataset/anndata.py
+++ scvi/dataset/anndata.py
@@ -26,27 +26,57 @@ class AnnDataset(GeneExpressionDataset):
 
     """
 
-    def __init__(self, filename, save_path='data/', url=None, new_n_genes=False, subset_genes=None):
+    def __init__(
+        self,
+        filename_or_anndata,
+        save_path="data/",
+        url=None,
+        new_n_genes=False,
+        subset_genes=None,
+    ):
         """
 
 
         """
-        self.download_name = filename
-        self.save_path = save_path
-        self.url = url
 
-        data, gene_names = self.download_and_preprocess()
+        if type(filename_or_anndata) == str:
+            self.download_name = filename_or_anndata
+            self.save_path = save_path
+            self.url = url
 
-        super().__init__(*GeneExpressionDataset.get_attributes_from_matrix(data),
-                         gene_names=gene_names)
+            data, gene_names = self.download_and_preprocess()
+
+        elif isinstance(filename_or_anndata, anndata.AnnData):
+            ad = filename_or_anndata
+            data, gene_names = self.extract_data_from_anndata(ad)
+
+        else:
+            raise Exception(
+                "Please provide a filename of an AnnData file or an already loaded AnnData object"
+            )
+
+        super().__init__(
+            *GeneExpressionDataset.get_attributes_from_matrix(data),
+            gene_names=gene_names
+        )
 
         self.subsample_genes(new_n_genes=new_n_genes, subset_genes=subset_genes)
 
     def preprocess(self):
         print("Preprocessing dataset")
 
-        ad = anndata.read_h5ad(os.path.join(self.save_path, self.download_name))  # obs = cells, var = genes
-        self.obs = ad.obs  # provide access to observation annotations from the underlying AnnData object.
+        ad = anndata.read_h5ad(
+            os.path.join(self.save_path, self.download_name)
+        )  # obs = cells, var = genes
+        data, gene_names = self.extract_data_from_anndata(ad)
+
+        print("Finished preprocessing dataset")
+        return data, gene_names
+
+    def extract_data_from_anndata(self, ad: anndata.AnnData):
+        self.obs = (
+            ad.obs
+        )  # provide access to observation annotations from the underlying AnnData object.
         gene_names = np.array(ad.var.index.values, dtype=str)
         if isinstance(ad.X, np.ndarray):
             data = ad.X.copy()  # Dense
@@ -55,5 +85,4 @@ class AnnDataset(GeneExpressionDataset):
         select = data.sum(axis=1) > 0  # Take out cells that doesn't express any gene
         data = data[select, :]
 
-        print("Finished preprocessing dataset")
         return data, gene_names
