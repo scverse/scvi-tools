531e0d398147445c0a7c7fe1489be3f96945eea6
gabmis <gabriel.misrachi@gmail.com>
2019-05-20 15:24:05 -0700

    default to 0 cpu workers if GPUs are found.

--- scvi/inference/autotune.py
+++ scvi/inference/autotune.py
@@ -218,7 +218,8 @@ def auto_tune_scvi_model(
     :param save_path: Path where to save best model, trainer, trials and mongo files.
     :param use_batches: If ``False``, pass ``n_batch=0`` to model else pass ``gene_dataset.n_batches``.
     :param parallel: If ``True``, use ``MongoTrials`` object to run trainings in parallel.
-    :param n_cpu_workers: Number of cpu workers to launch.
+    :param n_cpu_workers: Number of cpu workers to launch. If None, and no GPUs are found,
+        defaults to ``os.cpucount() - 1``. Else, defaults to 0.
     :param gpu_ids: Ids of the GPUs to use. If None defaults to all GPUs found by ``torch``.
         Note that considered gpu ids are int from 0 to ``torch.cuda.device_count()``.
     :param n_workers_per_gpu: Number of workers to launch per gpu found by ``torch``.
@@ -443,7 +444,8 @@ def _auto_tune_parallel(
         when performing hyperoptimization. Default: mutable, see source code.
     :param max_evals: Maximum number of evaluations of the objective.
     :param save_path: Path where to save best model, trainer, trials and mongo files.
-    :param n_cpu_workers: Number of cpu workers to launch.
+    :param n_cpu_workers: Number of cpu workers to launch. If None, and no GPUs are found,
+        defaults to ``os.cpucount() - 1``. Else, defaults to 0.
     :param gpu_ids: Ids of the GPUs to use. If None defaults to all GPUs found by ``torch``.
         Note that considered gpu ids are int from ``0`` to ``torch.cuda.device_count()``.
     :param n_workers_per_gpu: Number of workers ton launch per gpu found by ``torch``.
@@ -716,8 +718,8 @@ def launch_workers(
 ):
     """Launches the local workers which are going to run the jobs required by the minimization process.
     Terminates when the worker_watchdog call finishes.
-    Specifically, first ``n_cpu_workers`` CPU workers are launched in their own spawned process.
-    Then, ``n_gpu_workers`` are launched per GPU in ``gpu_ids``, also in their own spawned process.
+    Specifically, first ``n_gpu_workers`` are launched per GPU in ``gpu_ids`` in their own spawned process.
+    Then, ``n_cpu_workers`` CPU workers are launched, also in their own spawned process.
     The use of spawned processes (each have their own python interpreter) is mandatory for compatiblity with CUDA.
     See https://pytorch.org/docs/stable/notes/multiprocessing.html for more information.
 
@@ -725,7 +727,8 @@ def launch_workers(
         which checks that local workers are still running.
     :param exp_key: This key is used by hyperopt as a suffix to the part of the MongoDb
         which corresponds to the current experiment. In particular, it has to be passed to ``MongoWorker``.
-    :param n_cpu_workers: Number of cpu workers to launch.
+    :param n_cpu_workers: Number of cpu workers to launch. If None, and no GPUs are found,
+        defaults to ``os.cpu_count() - 1``. Else, defaults to 0.
     :param gpu_ids: Ids of the GPUs to use. If None defaults to all GPUs found by ``torch``.
         Note that considered gpu ids are int from ``0`` to ``torch.cuda.device_count()``.
     :param n_workers_per_gpu: Number of workers ton launch per gpu found by ``torch``.
@@ -745,11 +748,22 @@ def launch_workers(
     started_processes.append(listener)
 
     if gpu_ids is None:
-        logger.debug("gpu_ids is None, defaulting to all gpus found by torch.")
-        gpu_ids = list(range(torch.cuda.device_count()))
-    if n_cpu_workers is None:
-        logger.debug("n_cpu_workers is None, defaulting to max(0, os.cpu_count() - 1).")
-        n_cpu_workers = max(0, os.cpu_count() - 1)
+        n_gpus = torch.cuda.device_count()
+        logger.debug("gpu_ids is None, defaulting to all {n_gpus} GPUs found by torch.")
+        gpu_ids = list(range(n_gpus))
+        if n_gpus and n_cpu_workers is None:
+            n_cpu_workers = 0
+            logging.debug(
+                "Some GPU.s found and n_cpu_wokers is None, defaulting to n_cpu_workers = 0"
+            )
+        if not n_gpus and n_cpu_workers is None:
+            n_cpu_workers = os.cpu_count() - 1
+            logging.debug(
+                "No GPUs found and n_cpu_wokers is None, defaulting to n_cpu_workers = "
+                "{n_cpu_workers} (os.cpu_count() - 1)".format(
+                    n_cpu_workers=n_cpu_workers
+                )
+            )
     if not gpu_ids and not n_cpu_workers and not multiple_hosts:
         raise ValueError("No hardware (cpu/gpu) selected/found.")
 
@@ -793,7 +807,9 @@ def launch_workers(
 
     # launch cpu workers
     # TODO: add cpu affinity?
-    logger.info("Starting {n_cpu} cpu worker.s".format(n_cpu=n_cpu_workers))
+    logger.info(
+        "Starting {n_cpu_workers} cpu worker.s".format(n_cpu_workers=n_cpu_workers)
+    )
     for cpu_id in range(n_cpu_workers):
         worker_kwargs = {
             "progress_queue": progress_queue,
@@ -1034,10 +1050,10 @@ def _objective_function(
         # select metric from early stopping kwargs if possible
         metric = None
         early_stopping_kwargs = trainer_specific_kwargs.get(
-            "early_stopping_kwargs", default=None
+            "early_stopping_kwargs", None
         )
         if early_stopping_kwargs:
-            metric = early_stopping_kwargs.get("early_stopping_metric", default=None)
+            metric = early_stopping_kwargs.get("early_stopping_metric", None)
 
         # store run results
         if metric:
