aedbf5293366b32426fd1e4001cc9595e3f43b78
Adam Gayoso <adamgayoso@users.noreply.github.com>
2019-06-18 17:53:41 -0700

    improve clarity through updated docstrings and comment (#373)
    * add clarity to method in posterior generate with comment
    
    * update docstring for log_variational
    
    * undo Black formatting

--- scvi/inference/posterior.py
+++ scvi/inference/posterior.py
@@ -521,10 +521,9 @@ class Posterior:
             sample_batch, _, _, batch_index, labels = tensors
             px_dispersion, px_rate = self.model.inference(sample_batch, batch_index=batch_index, y=labels,
                                                           n_samples=n_samples)[1:3]
-
+            # This gamma is really l*w using scVI manuscript notation
             p = (px_rate / (px_rate + px_dispersion)).cpu()
             r = px_dispersion.cpu()
-            #
             l_train = np.random.gamma(r, p / (1 - p))
             X = np.random.poisson(l_train)
             # """
