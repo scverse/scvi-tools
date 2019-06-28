a3409f9216f4d538077032ac099d8c2cf42601d9
adamgayoso <adamgayoso@users.noreply.github.com>
2019-06-27 22:45:53 -0700

    import logging

--- scvi/criticism/ppc.py
+++ scvi/criticism/ppc.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.decomposition import FactorAnalysis
 from typing import Dict, Iterable, List, Tuple, Union, Optional
 from scvi.inference import Posterior
+import logging
 
 logger = logging.getLogger(__name__)
 
