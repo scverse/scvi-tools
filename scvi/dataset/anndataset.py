85e6ee16f4baebc94fcf14600aa856a3c1c564dc
adamgayoso <adamgayoso@users.noreply.github.com>
2019-07-05 11:13:26 -0700

    fix return values in AnnDatasetFromAnnData

--- scvi/dataset/anndataset.py
+++ scvi/dataset/anndataset.py
@@ -31,6 +31,7 @@ class AnnDatasetFromAnnData(GeneExpressionDataset):
             self.obsm,
             self.var,
             self.varm,
+            self.uns
         ) = extract_data_from_anndata(ad)
         self.populate_from_data(
             X=X,
