9be39aaa20cad121a09a9a1bdbdfaee5d3de29e3
gabmis <gabriel.misrachi@gmail.com>
2019-07-05 16:06:16 -0700

    Remove inheritance from `torch.utils.data.Dataset`

--- scvi/dataset/dataset.py
+++ scvi/dataset/dataset.py
@@ -12,7 +12,6 @@ import numpy as np
 import scipy.sparse as sp_sparse
 import torch
 from sklearn.preprocessing import StandardScaler
-from torch.utils.data import Dataset
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +24,7 @@ class CellMeasurement:
     columns: Union[np.ndarray, List[str]]  # Column names: Eg: gene_names
 
 
-class GeneExpressionDataset(Dataset):
+class GeneExpressionDataset():
     """Generic class representing RNA counts and annotation information.
 
     This class is scVI's base dataset class. It gives access to several
@@ -474,10 +473,6 @@ class GeneExpressionDataset(Dataset):
 
     def __len__(self):
         return self.X.shape[0]
-
-    def __getitem__(self, idx):
-        """Implements @abstractcmethod in ``torch.utils.data.dataset.Dataset`` ."""
-        return idx
 
     @property
     def X(self):
