b8a464761532ca050eebafed4fd3ced28be4c32a
adamgayoso <adamgayoso@users.noreply.github.com>
2019-07-18 18:51:27 -0700

    add validation set option
    If train and test size do not add to 1 the remaining samples are placed inside a validation set

--- scvi/inference/inference.py
+++ scvi/inference/inference.py
@@ -14,8 +14,11 @@ class UnsupervisedTrainer(Trainer):
     Args:
         :model: A model instance from class ``VAE``, ``VAEC``, ``SCANVI``
         :gene_dataset: A gene_dataset instance like ``CortexDataset()``
-        :train_size: The train size, either a float between 0 and 1 or and integer for the number of training samples
+        :train_size: The train size, either a float between 0 and 1 or an integer for the number of training samples
          to use Default: ``0.8``.
+        :test_size: The test size, either a float between 0 and 1 or an integer for the number of training samples
+         to use Default: ``None``, which is equivalent to data not in the train set. If ``train_size`` and ``test_size``
+         do not add to 1 or the length of the dataset then the remaining samples are added to a ``validation_set``.
         :n_epochs_kl_warmup: Number of epochs for linear warmup of KL(q(z|x)||p(z)) term. After `n_epochs_kl_warmup`,
             the training objective is the ELBO. This might be used to prevent inactivity of latent units, and/or to
             improve clustering of latent space, as a long warmup turns the model into something more of an autoencoder.
@@ -35,9 +38,10 @@ class UnsupervisedTrainer(Trainer):
         super().__init__(model, gene_dataset, **kwargs)
         self.n_epochs_kl_warmup = n_epochs_kl_warmup
         if type(self) is UnsupervisedTrainer:
-            self.train_set, self.test_set = self.train_test(model, gene_dataset, train_size, test_size)
+            self.train_set, self.test_set, self.validation_set = self.train_test_validation(model, gene_dataset, train_size, test_size)
             self.train_set.to_monitor = ['elbo']
             self.test_set.to_monitor = ['elbo']
+            self.validation_set.to_monitor = ['elbo']
 
     @property
     def posteriors_loop(self):
