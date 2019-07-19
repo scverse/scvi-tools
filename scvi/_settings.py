03e949c616f889a839262d2a474c1a37ce105a58
Gabriel Misrachi <gabriel.misrachi@gmail.com>
2019-07-18 17:24:30 -0700

    set_verbosity function * add docstring * mention it in basic_tutorial

--- scvi/_settings.py
+++ scvi/_settings.py
@@ -5,11 +5,17 @@ scvi_logger = logging.getLogger("scvi")
 
 
 def set_verbosity(level: Union[str, int]):
-    """Sets logging configuration for scvi based on chosen level of verbosity."""
+    """Sets logging configuration for scvi based on chosen level of verbosity.
+
+    Sets "scvi" logging level to `level`
+    If "scvi" logger has no StreamHandler, add one.
+    Else, set its level to `level`.
+    """
     scvi_logger.setLevel(level)
     has_streamhandler = False
     for handler in scvi_logger.handlers:
         if isinstance(handler, logging.StreamHandler):
+            handler.setLevel(level)
             has_streamhandler = True
     if not has_streamhandler:
         ch = logging.StreamHandler()
