5c2c2c8679f49ed0678a32d20b38e8e08158c60d
Romain Lopez <romain_lopez@berkeley.edu>
2019-07-25 00:42:23 -0700

    correct bumpversion

--- setup.py
+++ setup.py
@@ -77,6 +77,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras_requirements,
     url="https://github.com/YosefLab/scVI",
-    version="version='0.4.0'",
+    version="0.4.0",
     zip_safe=False,
 )
