e11173c60c87399751fa3d27ae048f6f9e3e34e7
adamgayoso <adamgayoso@users.noreply.github.com>
2019-10-02 17:23:16 -0700

    test totalVI notebook

--- tests/test_notebook.py
+++ tests/test_notebook.py
@@ -235,3 +235,17 @@ def test_notebooks_autotune(save_path):
         raise
     finally:
         os.chdir(path=base_path)
+
+
+def test_notebooks_totalvi(save_path):
+    try:
+        os.chdir(save_path)
+        import notebooks.totalvi
+
+        print(save_path)
+        notebooks.totalvi.allow_notebook_for_test()
+        plt.close("all")
+    except BaseException:
+        raise
+    finally:
+        os.chdir(path=base_path)
