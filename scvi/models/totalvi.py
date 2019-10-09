6a40ad9168f1770b74486a0127e3af16bdebe1d0
adamgayoso <adamgayoso@users.noreply.github.com>
2019-10-08 17:41:33 -0700

    decoder gets n_layers

--- scvi/models/totalvi.py
+++ scvi/models/totalvi.py
@@ -133,7 +133,7 @@ class TOTALVI(nn.Module):
             n_latent,
             n_input_genes,
             self.n_input_proteins,
-            n_layers=1,
+            n_layers=n_layers,
             n_cat_list=[n_batch, n_labels],
             n_hidden=n_hidden,
             dropout_rate=dropout_rate_decoder,
