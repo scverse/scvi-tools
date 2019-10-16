23f3a9a96afc4bdb51c1c6559e8f9d92a98abe65
Romain <romain_lopez@berkeley.edu>
2019-10-16 11:19:00 -0700

    black again

--- setup.py
+++ setup.py
@@ -49,7 +49,12 @@ extras_requirements = {
         "seaborn>=0.9.0",
         "leidenalg>=0.7.0",
     ],
-    "docs": ["sphinx>=1.7.1", "nbsphinx", "sphinx_autodoc_typehints", "sphinx-rtd-theme"],
+    "docs": [
+        "sphinx>=1.7.1",
+        "nbsphinx",
+        "sphinx_autodoc_typehints",
+        "sphinx-rtd-theme",
+    ],
     "test": test_requirements,
 }
 author = (
