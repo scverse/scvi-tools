0636f9f7b74e77799e8ed98dcba979bfdf56fe22
adamgayoso <adamgayoso@users.noreply.github.com>
2020-04-14 09:37:12 -0700

    Bump version: 0.6.3 → 0.6.4

--- setup.py
+++ setup.py
@@ -103,6 +103,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras_requirements,
     url="https://github.com/YosefLab/scVI",
-    version="0.6.3",
+    version="0.6.4",
     zip_safe=False,
 )
