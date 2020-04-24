531ae9fad13f121c5f859a1a5c1c44a055c17ac5
mortonjt <jamietmorton@gmail.com>
2020-04-24 15:37:50 -0600

    Adding biom compatability

---
+++ tests/dataset/test_biom.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from scvi.dataset import BiomDataset
+from .utils import unsupervised_training_one_epoch
+
+
+class TestBiomDataset(TestCase):
+    def test_populate(self):
+        dataset = BiomDataset(filename="feature-table.biom",
+                              save_path="tests/data")
+        unsupervised_training_one_epoch(dataset)
+
