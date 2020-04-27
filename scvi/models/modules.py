Fixing numerical issues for Negative binomial distribution

scvi/models/modules.py
@@ -267,7 +267,8 @@ class DecoderSCVI(nn.Module):
         px_scale = self.px_scale_decoder(px)
         px_dropout = self.px_dropout_decoder(px)
         # Clamp to high value: exp(12) ~ 160000 to avoid nans (computational stability)
-        px_rate = torch.exp(library) * px_scale  # torch.clamp( , max=12)
+        px_rate = torch.log(library) + px_scale  # torch.clamp( , max=12)
+        #px_scale = torch.log(library) # probably don't need scale factor
         px_r = self.px_r_decoder(px) if dispersion == "gene-cell" else None
         return px_scale, px_r, px_rate, px_dropout
 
@@ -312,9 +313,9 @@ class LinearDecoderSCVI(nn.Module):
     ):
         # The decoder returns values for the parameters of the ZINB distribution
         raw_px_scale = self.factor_regressor(z, *cat_list)
-        px_scale = torch.softmax(raw_px_scale, dim=-1)
+        px_scale = torch.logsoftmax(raw_px_scale, dim=-1)
         px_dropout = self.px_dropout_decoder(z, *cat_list)
-        px_rate = torch.exp(library) * px_scale
+        px_rate = library + px_scale
         px_r = None
 
         return px_scale, px_r, px_rate, px_dropout
@@ -495,7 +496,7 @@ class MultiDecoder(nn.Module):
 
         px_scale = self.px_scale_decoder(px)
         px_dropout = self.px_dropout_decoder(px)
-        px_rate = torch.exp(library) * px_scale
+        px_rate = torch.log(library) + px_scale
         px_r = self.px_r_decoder(px) if dispersion == "gene-cell" else None
 
         return px_scale, px_r, px_rate, px_dropout