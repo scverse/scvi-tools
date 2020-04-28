Adding fixes for numerical stability

scvi/models/distributions.py
@@ -43,7 +43,8 @@ def _convert_counts_logits_to_mean_disp(total_count, logits):
         :return: the mean and inverse overdispersion of the NB distribution.
     """
     theta = total_count
-    mu = logits.exp() * theta
+    # mu = logits.exp() * theta
+    mu = logits + torch.log(theta)
     return mu, theta
 
 
@@ -60,8 +61,8 @@ class NegativeBinomial(Distribution):
     one parameterization to another.
     """
     arg_constraints = {
-        "mu": constraints.greater_than_eq(0),
-        "theta": constraints.greater_than_eq(0),
+        "log_mu": constraints.real,
+        "log_theta": constraints.real,
     }
     support = constraints.nonnegative_integer
 
@@ -70,12 +71,12 @@ class NegativeBinomial(Distribution):
         total_count: torch.Tensor = None,
         probs: torch.Tensor = None,
         logits: torch.Tensor = None,
-        mu: torch.Tensor = None,
-        theta: torch.Tensor = None,
+        log_mu: torch.Tensor = None,
+        log_theta: torch.Tensor = None,
         validate_args=True,
     ):
         self._eps = 1e-8
-        if (mu is None) == (total_count is None):
+        if (log_mu is None) == (total_count is None):
             raise ValueError(
                 "Please use one of the two possible parameterizations. Refer to the documentation for more information."
             )
@@ -87,11 +88,11 @@ class NegativeBinomial(Distribution):
             logits = logits if logits is not None else probs_to_logits(probs)
             total_count = total_count.type_as(logits)
             total_count, logits = broadcast_all(total_count, logits)
-            mu, theta = _convert_counts_logits_to_mean_disp(total_count, logits)
+            log_mu, log_theta = _convert_counts_logits_to_mean_disp(total_count, logits)
         else:
-            mu, theta = broadcast_all(mu, theta)
-        self.mu = mu
-        self.theta = theta
+            log_mu, log_theta = broadcast_all(log_mu, log_theta)
+        self.log_mu = log_mu
+        self.log_theta = log_theta
         super().__init__(validate_args=validate_args)
 
     def sample(self, sample_shape=torch.Size()):
@@ -115,11 +116,13 @@ class NegativeBinomial(Distribution):
                     "The value argument must be within the support of the distribution",
                     UserWarning,
                 )
-        return log_nb_positive(value, mu=self.mu, theta=self.theta, eps=self._eps)
+        return log_nb_positive(value, log_mu=self.log_mu,
+                               log_theta=self.log_theta, eps=self._eps)
 
     def _gamma(self):
-        concentration = self.theta
-        rate = self.theta / self.mu
+        concentration = torch.exp(self.log_theta)
+
+        rate = torch.exp(self.log_theta - self.log_mu)
         # Important remark: Gamma is parametrized by the rate = 1/scale!
         gamma_d = Gamma(concentration=concentration, rate=rate)
         return gamma_d

scvi/models/log_likelihood.py
@@ -246,7 +246,7 @@ def log_zinb_positive(x, mu, theta, pi, eps=1e-8):
     return res
 
 
-def log_nb_positive(x, mu, theta, eps=1e-8):
+def log_nb_positive(x, log_mu, log_theta, eps=1e-8):
     """
     Note: All inputs should be torch Tensors
     log likelihood (scalar) of a minibatch according to a nb model.
@@ -256,21 +256,17 @@ def log_nb_positive(x, mu, theta, eps=1e-8):
     theta: inverse dispersion parameter (has to be positive support) (shape: minibatch x genes)
     eps: numerical stability constant
     """
-    if theta.ndimension() == 1:
-        theta = theta.view(
-            1, theta.size(0)
+    if log_theta.ndimension() == 1:
+        log_theta = log_theta.view(
+            1, log_theta.size(0)
         )  # In this case, we reshape theta for broadcasting
 
-    log_theta_mu_eps = torch.log(theta + mu + eps)
-
-    res = (
-        theta * (torch.log(theta + eps) - log_theta_mu_eps)
-        + x * (torch.log(mu + eps) - log_theta_mu_eps)
-        + torch.lgamma(x + theta)
-        - torch.lgamma(theta)
-        - torch.lgamma(x + 1)
+    theta = torch.exp(log_theta)
+    res = (theta * log_theta + x * log_mu
+           + torch.lgamma(x + theta)
+           + torch.lgamma(x + theta)
+           - torch.lgamma(theta)
     )
-
     return res
 
 

scvi/models/modules.py
@@ -314,7 +314,7 @@ class LinearDecoderSCVI(nn.Module):
         raw_px_scale = self.factor_regressor(z, *cat_list)
         px_scale = torch.softmax(raw_px_scale, dim=-1)
         px_dropout = self.px_dropout_decoder(z, *cat_list)
-        px_rate = torch.exp(library) * px_scale
+        px_rate = library + px_scale
         px_r = None
 
         return px_scale, px_r, px_rate, px_dropout

scvi/models/vae.py
@@ -172,13 +172,13 @@ class VAE(nn.Module):
         :param transform_batch: int of batch to transform samples into
         :return: tensor of predicted frequencies of expression with shape ``(batch_size, n_input)``
         """
-        return self.inference(
+        return torch.exp(self.inference(
             x,
             batch_index=batch_index,
             y=y,
             n_samples=n_samples,
             transform_batch=transform_batch,
-        )["px_scale"]
+        )["px_scale"])
 
     def get_sample_rate(
         self, x, batch_index=None, y=None, n_samples=1, transform_batch=None
@@ -192,13 +192,13 @@ class VAE(nn.Module):
         :param transform_batch: int of batch to transform samples into
         :return: tensor of means of the negative binomial distribution with shape ``(batch_size, n_input)``
         """
-        return self.inference(
+        return torch.exp(self.inference(
             x,
             batch_index=batch_index,
             y=y,
             n_samples=n_samples,
             transform_batch=transform_batch,
-        )["px_rate"]
+        )["px_rate"])
 
     def get_reconstruction_loss(
         self, x, px_rate, px_r, px_dropout, **kwargs
@@ -216,7 +216,7 @@ class VAE(nn.Module):
             )
         elif self.reconstruction_loss == "nb":
             reconst_loss = (
-                -NegativeBinomial(mu=px_rate, theta=px_r).log_prob(x).sum(dim=-1)
+                -NegativeBinomial(mu=px_rate, theta=px_r).log_prob(x).mean(dim=-1)
             )
         elif self.reconstruction_loss == "poisson":
             reconst_loss = -Poisson(px_rate).log_prob(x).sum(dim=-1)