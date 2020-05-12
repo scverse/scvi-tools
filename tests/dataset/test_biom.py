5c210d7ee8b2e8d9088a88ba41ade6089831018f
mortonjt <jamietmorton@gmail.com>
2020-05-11 20:50:09 -0600

    adding more eposh

--- tests/dataset/test_biom.py
+++ tests/dataset/test_biom.py
@@ -1,24 +1,26 @@
 from unittest import TestCase
 
 from scvi.dataset import BiomDataset
-from .utils import unsupervised_training_one_epoch, unsupervised_nb_training_one_epoch
+from .utils import unsupervised_training_one_epoch, unsupervised_training_ten_epoch
+# , unsupervised_nb_training_one_epoch
 
 
 class TestBiomDataset(TestCase):
     # fix once NB is fixed
-    # def test_populate(self):
-    #     dataset = BiomDataset(filename="feature-table.biom",
-    #                           save_path="tests/data")
-    #     unsupervised_training_one_epoch(dataset)
-
-    def test_populate_nb(self):
+    def test_populate(self):
         dataset = BiomDataset(filename="feature-table.biom",
                               save_path="tests/data")
-        unsupervised_nb_training_one_epoch(dataset)
+        unsupervised_training_one_epoch(dataset)
+
+    # Add NB test
+    # def test_populate_nb(self):
+    #     dataset = BiomDataset(filename="feature-table.biom",
+    #                           save_path="tests/data")
+    #     unsupervised_nb_training_one_epoch(dataset)
 
     def test_ag_url(self):
         url = (r'https://github.com/biocore/American-Gut/blob/master/'
                r'data/AG/AG_100nt.biom?raw=true')
         dataset = BiomDataset(filename='AG_100nt.biom', url=url, save_path="tests/data")
-        unsupervised_nb_training_one_epoch(dataset)
+        unsupervised_training_one_epoch(dataset)
 
--- tests/dataset/utils.py
+++ tests/dataset/utils.py
@@ -11,3 +11,6 @@ def unsupervised_training_one_epoch(dataset: GeneExpressionDataset):
     vae = VAE(dataset.nb_genes, dataset.n_batches, dataset.n_labels)
     trainer = UnsupervisedTrainer(vae, dataset, train_size=0.5, use_cuda=use_cuda)
     trainer.train(n_epochs=1)
+
+
+
