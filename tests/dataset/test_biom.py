560d64f7cfd06678a7a99718841f4021ecba6e15
mortonjt <jamietmorton@gmail.com>
2020-05-26 23:28:10 -0600

    simplifying unittests

--- tests/dataset/test_biom.py
+++ tests/dataset/test_biom.py
@@ -12,15 +12,3 @@ class TestBiomDataset(TestCase):
                               save_path="tests/data")
         unsupervised_training_one_epoch(dataset)
 
-    # Add NB test
-    # def test_populate_nb(self):
-    #     dataset = BiomDataset(filename="feature-table.biom",
-    #                           save_path="tests/data")
-    #     unsupervised_nb_training_one_epoch(dataset)
-
-    def test_ag_url(self):
-        url = (r'https://github.com/biocore/American-Gut/blob/master/'
-               r'data/AG/AG_100nt.biom?raw=true')
-        dataset = BiomDataset(filename='AG_100nt.biom', url=url, save_path="tests/data")
-        unsupervised_training_one_epoch(dataset)
-
