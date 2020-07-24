7ee6b379e4743318e536483ec453fd972883c69c
galen xing <gx2113@columbia.edu>
2020-07-23 23:18:27 -0400

    add run_setup_anndata option to synthetic

--- scvi/dataset/_built_in_data/_synthetic.py
+++ scvi/dataset/_built_in_data/_synthetic.py
@@ -1,6 +1,7 @@
 import logging
 import pandas as pd
 import numpy as np
+from scvi.dataset import setup_anndata
 from anndata import AnnData
 
 logger = logging.getLogger(__name__)
@@ -12,6 +13,7 @@ def _generate_synthetic(
     n_proteins: int = 100,
     n_batches: int = 2,
     n_labels: int = 3,
+    run_setup_anndata: bool = True,
 ):
 
     data = np.random.negative_binomial(5, 0.3, size=(batch_size * n_batches, n_genes))
@@ -32,5 +34,13 @@ def _generate_synthetic(
     p_data = np.random.negative_binomial(5, 0.3, size=(adata.shape[0], n_proteins))
     adata.obsm["protein_expression"] = p_data
     adata.uns["protein_names"] = np.arange(n_proteins).astype(str)
+    if run_setup_anndata:
+        setup_anndata(
+            adata,
+            batch_key="batch",
+            labels_key="labels",
+            protein_expression_obsm_key="protein_expression",
+            protein_names_uns_key="protein_names",
+        )
 
     return adata
