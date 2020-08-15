f709c0d014963471c3188a46e6f8b28b90bd9218
adamgayoso <adamgayoso@users.noreply.github.com>
2020-08-15 15:12:48 -0700

    autozi in init

--- scvi/models/__init__.py
+++ scvi/models/__init__.py
@@ -1,5 +1,6 @@
 from .scvi import SCVI
 from .totalvi import TOTALVI
 from .linear_scvi import LinearSCVI
+from .autozi import AUTOZI
 
-__all__ = ["SCVI", "TOTALVI", "LinearSCVI"]
+__all__ = ["SCVI", "TOTALVI", "LinearSCVI", "AUTOZI"]
