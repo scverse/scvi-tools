25fb6c8157cc2b0f1c0aa9dc782b0258ff99f5a5
adamgayoso <adamgayoso@users.noreply.github.com>
2020-09-03 15:27:31 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ from setuptools import setup, find_packages
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
-
 requirements = [
     "numpy>=1.17.0",
     "torch>=1.3",
