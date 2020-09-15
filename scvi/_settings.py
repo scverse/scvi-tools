8789beffe967b3d9468eea525cfc348e56885af4
adamgayoso <adamgayoso@users.noreply.github.com>
2020-09-15 13:50:09 -0700

    duplications

--- scvi/_settings.py
+++ scvi/_settings.py
@@ -96,19 +96,6 @@ class ScviConfig:
 
     @property
     def seed(self) -> int:
-        """Random seed."""
-        return self._seed
-
-    @seed.setter
-    def seed(self, seed: int) -> int:
-        """Random seed for torch and numpy."""
-        torch.manual_seed(seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-        np.random.seed(seed)
-
-    @property
-    def seed(self) -> int:
         """Random seed for torch and numpy."""
         return self._seed
 
