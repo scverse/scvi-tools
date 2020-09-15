ee7f95771ca8f1197ffc03c949ff974dc792d002
adamgayoso <adamgayoso@users.noreply.github.com>
2020-09-14 21:52:04 -0700

    more cleanup, fix versions, remove tox ini

--- scvi/__init__.py
+++ scvi/__init__.py
@@ -8,6 +8,18 @@ from ._constants import _CONSTANTS
 from ._settings import settings
 from . import dataset, models
 
+import toml
+from pathlib import Path
+
+
+def get_version():
+    path = Path(__file__).resolve().parents[1] / "pyproject.toml"
+    pyproject = toml.loads(open(str(path)).read())
+    return pyproject["tool"]["poetry"]["version"]
+
+
+__version__ = get_version()
+
 logger = logging.getLogger(__name__)
 logger.addHandler(NullHandler())
 
