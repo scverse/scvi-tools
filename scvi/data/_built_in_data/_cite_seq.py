a0d6a41a4a24a1bf99db6ed9599105277e688323
adamgayoso <adamgayoso@users.noreply.github.com>
2020-09-23 10:52:23 -0700

    add sln dataset from totalvi

--- scvi/data/_built_in_data/_cite_seq.py
+++ scvi/data/_built_in_data/_cite_seq.py
@@ -69,3 +69,63 @@ def _load_pbmcs_10x_cite_seq(
         )
 
     return dataset
+
+
+def _load_spleen_lymph_cite_seq(
+    save_path: str = "data/",
+    protein_join: str = "inner",
+    run_setup_anndata: bool = True,
+):
+    """
+    Immune cells from the murine spleen and lymph nodes [GayosoSteier20]_.
+
+    This dataset was used throughout the totalVI manuscript, and named SLN-all.
+    Parameters
+    ----------
+    save_path
+        Location to use when saving/loading the data.
+    protein_join
+        Whether to take an inner join or outer join of proteins
+    remove_outliers
+        Whether to remove clusters annotated as doublet or low quality
+    run_setup_anndata
+        If true, runs setup_anndata() on dataset before returning
+
+    Returns
+    -------
+    `AnnData` with `.obsm["protein_expression"]
+
+    Missing protein values are zero, and are identified during `AnnData` setup.
+    """
+    url = "https://github.com/YosefLab/scVI-data/raw/master/sln_111.h5ad?raw=true"
+    save_fn = "sln_111.h5ad"
+    _download(url, save_path, save_fn)
+    dataset1 = anndata.read_h5ad(os.path.join(save_path, save_fn))
+
+    url = "https://github.com/YosefLab/scVI-data/raw/master/sln_208.h5ad?raw=true"
+    save_fn = "sln_208.h5ad"
+    _download(url, save_path, save_fn)
+    dataset2 = anndata.read_h5ad(os.path.join(save_path, save_fn))
+
+    common_genes = dataset1.var_names.intersection(dataset2.var_names)
+    dataset1 = dataset1[:, common_genes]
+    dataset2 = dataset2[:, common_genes]
+
+    del dataset1.uns["protein_names"]
+    del dataset2.uns["protein_names"]
+
+    dataset = dataset1.concatenate(
+        dataset2, join=protein_join, batch_key="anndata_batch"
+    )
+    del dataset.obs["anndata_batch"]
+    dataset.obsm["protein_expression"] = dataset.obsm["protein_expression"].fillna(0)
+
+    if run_setup_anndata:
+        setup_anndata(
+            dataset,
+            batch_key="batch",
+            labels_key="cell_types",
+            protein_expression_obsm_key="protein_expression",
+        )
+
+    return dataset
