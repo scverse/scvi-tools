13a3ac8eec86fedf736f6d7c5f0ec169190de8d7
adamgayoso <adamgayoso@users.noreply.github.com>
2020-09-25 12:36:28 -0700

    fix to concat

--- scvi/data/_built_in_data/_cite_seq.py
+++ scvi/data/_built_in_data/_cite_seq.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import anndata
 import os
 
@@ -37,11 +36,13 @@ def _load_pbmcs_10x_cite_seq(
     save_fn = "pbmc_10k_protein_v3.h5ad"
     _download(url, save_path, save_fn)
     dataset1 = anndata.read_h5ad(os.path.join(save_path, save_fn))
+    dataset1.obs["batch"] = "PBMC10k"
 
     url = "https://github.com/YosefLab/scVI-data/raw/master/pbmc_5k_protein_v3.h5ad?raw=true"
     save_fn = "pbmc_5k_protein_v3.h5ad"
     _download(url, save_path, save_fn)
     dataset2 = anndata.read_h5ad(os.path.join(save_path, "pbmc_5k_protein_v3.h5ad"))
+    dataset2.obs["batch"] = "PBMC5k"
 
     common_genes = dataset1.var_names.intersection(dataset2.var_names)
     dataset1 = dataset1[:, common_genes]
@@ -61,8 +62,6 @@ def _load_pbmcs_10x_cite_seq(
 
     dataset = anndata.concat([dataset1, dataset2], join=protein_join)
     dataset.obsm["protein_expression"] = dataset.obsm["protein_expression"].fillna(0)
-    dataset.obs["labels"] = np.zeros(dataset.shape[0], dtype=np.int64)
-    dataset.obs["batch"] = dataset.obs["batch"].astype(np.int64)
 
     if run_setup_anndata:
         setup_anndata(
@@ -121,9 +120,9 @@ def _load_spleen_lymph_cite_seq(
     del dataset2.uns["protein_names"]
 
     dataset = anndata.concat(
-        [dataset1, dataset2], join=protein_join, batch_key="anndata_batch"
+        [dataset1, dataset2],
+        join=protein_join,
     )
-    del dataset.obs["anndata_batch"]
     dataset.obsm["protein_expression"] = dataset.obsm["protein_expression"].fillna(0)
 
     if remove_outliers:
