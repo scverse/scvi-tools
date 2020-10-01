b10a1563e0bd0f8b9f5f65af3fd0eee68cb2ce0e
adamgayoso <adamgayoso@users.noreply.github.com>
2020-10-01 10:55:51 -0700

    fdr -> fdr_target

--- scvi/core/models/rnamixin.py
+++ scvi/core/models/rnamixin.py
@@ -159,7 +159,7 @@ class RNASeqMixin:
         batch_correction: bool = False,
         batchid1: Optional[Iterable[str]] = None,
         batchid2: Optional[Iterable[str]] = None,
-        fdr: float = 0.05,
+        fdr_target: float = 0.05,
         **kwargs,
     ) -> pd.DataFrame:
         r"""
@@ -202,7 +202,7 @@ class RNASeqMixin:
             batchid2,
             delta,
             batch_correction,
-            fdr,
+            fdr_target,
             **kwargs,
         )
 
