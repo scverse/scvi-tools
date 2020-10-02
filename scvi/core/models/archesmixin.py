ec565724b4c758411c0d63cb3bc393d448725471
adamgayoso <adamgayoso@users.noreply.github.com>
2020-10-01 19:10:06 -0700

    cleanup and options

--- scvi/core/models/archesmixin.py
+++ scvi/core/models/archesmixin.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import pickle
-from typing import Optional
 
 import torch
 from anndata import AnnData
@@ -19,15 +18,8 @@ class ArchesMixin:
         adata: AnnData,
         dir_path: str,
         use_cuda: bool = True,
-        n_epochs: Optional[int] = None,
-        train_size: float = 0.9,
-        test_size: Optional[float] = None,
-        lr: float = 1e-3,
-        n_epochs_kl_warmup: int = 400,
-        n_iter_kl_warmup: Optional[int] = None,
-        frequency: Optional[int] = None,
-        train_fun_kwargs: dict = {},
-        **kwargs,
+        freeze_dropout: bool = False,
+        freeze_batchnorm: bool = False,
     ):
         """
         Online update of a reference model using method of scArches.
@@ -42,26 +34,10 @@ class ArchesMixin:
             Path to saved outputs for reference model.
         use_cuda
             Whether to load model on GPU.
-        n_epochs
-            Number of passes through the query dataset.
-        train_size
-            Size of training set in the range [0.0, 1.0].
-        test_size
-            Size of the test set. If `None`, defaults to 1 - `train_size`. If
-            `train_size + test_size < 1`, the remaining cells belong to a validation set.
-        lr
-            Learning rate for optimization.
-        n_epochs_kl_warmup
-            Number of passes through dataset for scaling term on KL divergence to go from 0 to 1.
-        n_iter_kl_warmup
-            Number of minibatches for scaling term on KL divergence to go from 0 to 1.
-            To use, set to not `None` and set `n_epochs_kl_warmup` to `None`.
-        frequency
-            Frequency with which metrics are computed on the data for train/test/val sets.
-        train_fun_kwargs
-            Keyword args for the train method of :class:`~scvi.core.trainers.UnsupervisedTrainer`.
-        **kwargs
-            Other keyword args for :class:`~scvi.core.trainers.UnsupervisedTrainer`.
+        freeze_dropout
+            Whether to freeze dropout during training
+        freeze_batchnorm
+            Whether to freeze batchnorm statistics during training
         """
         model_path = os.path.join(dir_path, "model_params.pt")
         setup_dict_path = os.path.join(dir_path, "attr.pkl")
@@ -110,22 +86,28 @@ class ArchesMixin:
         model.model.load_state_dict(load_state_dict)
         model.model.eval()
 
-        _set_params_online_update(model.model)
+        _set_params_online_update(
+            model.model,
+            freeze_batchnorm=freeze_batchnorm,
+            freeze_dropout=freeze_dropout,
+        )
         model.is_trained_ = False
 
         return model
 
 
-def _set_params_online_update(model):
+def _set_params_online_update(model, freeze_batchnorm, freeze_dropout):
 
     for key, mod in model.named_modules():
         if isinstance(mod, FCLayers):
             mod.set_online_update_hooks()
         if isinstance(mod, torch.nn.Dropout):
-            mod.p = 0
+            if freeze_dropout:
+                mod.p = 0
         if isinstance(mod, torch.nn.BatchNorm1d):
-            mod.affine = False
-            mod.track_running_stats = False
+            if freeze_batchnorm:
+                mod.affine = False
+                mod.track_running_stats = False
 
     for key, par in model.named_parameters():
         # gets the linear layer
