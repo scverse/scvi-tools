eefab632738e714888befaf81578bec6467975c8
galen xing <gx2113@columbia.edu>
2020-10-14 10:39:47 -0700

    abstract vae

---
+++ scvi/core/modules/_base/_base_module.py
@@ -0,0 +1,64 @@
+from abc import abstractmethod
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+
+
+class AbstractVAE(nn.Module):
+    def __init__(
+        self,
+    ):
+        super().__init__()
+
+    def forward(
+        self,
+        tensors,
+        inference_kwargs={},
+        generative_kwargs={},
+        loss_kwargs={},
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Forward pass through the network
+        Parameters
+        ----------
+        tensors
+            tensors to pass through
+        """
+        inference_kwargs = dict(inference_kwargs)
+        generative_kwargs = dict(generative_kwargs)
+        loss_kwargs = dict(loss_kwargs)
+
+        inference_inputs = self._get_inference_input(tensors)
+        inference_outputs = self.inference(**inference_inputs, **inference_kwargs)
+        generative_inputs = self._get_generative_input(tensors, inference_outputs)
+        generative_outputs = self.generative(**generative_inputs, **generative_kwargs)
+
+        model_outputs = dict(**inference_outputs, **generative_outputs)
+        losses = self.loss(tensors, model_outputs, **loss_kwargs)
+
+        return model_outputs, losses
+
+    @abstractmethod
+    def _get_inference_input(self, tensors):
+        pass
+
+    @abstractmethod
+    def _get_generative_input(self, tensors, inference_outputs):
+        pass
+
+    @abstractmethod
+    def inference(
+        self,
+        *args,
+        **kwargs,
+    ):
+        pass
+
+    @abstractmethod
+    def generative(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def sample(self):
+        pass
