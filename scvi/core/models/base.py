8523b2798d7c8e7fb66e8c8afe40649ca710bf0b
adamgayoso <adamgayoso@users.noreply.github.com>
2020-10-28 21:38:53 -0700

    n_epochs

--- scvi/core/models/base.py
+++ scvi/core/models/base.py
@@ -239,7 +239,7 @@ class BaseModelClass(ABC):
             self._pl_task = VAETask(
                 self.model,
             )
-            self.trainer = pl.Trainer(checkpoint_callback=False)
+            self.trainer = pl.Trainer(max_epochs=n_epochs, checkpoint_callback=False)
             train_dl, val_dl, test_dl = self._train_test_val_split(
                 self.adata, train_size=train_size, validation_size=validation_size
             )
