edbce7980d39885fdddaafbca949e8a9eb05dc32
adamgayoso <adamgayoso@users.noreply.github.com>
2020-11-06 14:55:58 -0800

    store order of cat covariates

--- scvi/data/_utils.py
+++ scvi/data/_utils.py
@@ -132,3 +132,77 @@ def _get_batch_mask_protein_data(
         batch_mask[b] = ~all_zero
 
     return batch_mask
+
+
+def _check_anndata_setup_equivalence(adata_source, adata_target):
+    """Checks if target setup is equivalent to source."""
+    if isinstance(adata_source, anndata.AnnData):
+        _scvi_dict = adata_source.uns["_scvi"]
+    else:
+        _scvi_dict = adata_source
+    adata = adata_target
+
+    stats = _scvi_dict["summary_stats"]
+
+    target_n_vars = adata.shape[1]
+    error_msg = (
+        "Number of {} in anndata different from initial anndata used for training."
+    )
+    if target_n_vars != stats["n_vars"]:
+        raise ValueError(error_msg.format("vars"))
+
+    error_msg = (
+        "There are more {} categories in the data than were originally registered. "
+        + "Please check your {} categories as well as adata.uns['_scvi']['categorical_mappings']."
+    )
+    self_categoricals = _scvi_dict["categorical_mappings"]
+    self_batch_mapping = self_categoricals["_scvi_batch"]["mapping"]
+
+    adata_categoricals = adata.uns["_scvi"]["categorical_mappings"]
+    adata_batch_mapping = adata_categoricals["_scvi_batch"]["mapping"]
+    # check if the categories are the same
+    error_msg = (
+        "Categorial encoding for {} is not the same between "
+        + "the anndata used to train the model and the anndata just passed in. "
+        + "Categorical encoding needs to be same elements, same order, and same datatype.\n"
+        + "Expected categories: {}. Received categories: {}.\n"
+        + "Try running `dataset.transfer_anndata_setup()` or deleting `adata.uns['_scvi']."
+    )
+    if not _assert_equal_mapping(self_batch_mapping, adata_batch_mapping):
+        raise ValueError(
+            error_msg.format("batch", self_batch_mapping, adata_batch_mapping)
+        )
+    self_labels_mapping = self_categoricals["_scvi_labels"]["mapping"]
+    adata_labels_mapping = adata_categoricals["_scvi_labels"]["mapping"]
+    if not _assert_equal_mapping(self_labels_mapping, adata_labels_mapping):
+        raise ValueError(
+            error_msg.format("label", self_labels_mapping, adata_labels_mapping)
+        )
+
+    # validate any extra categoricals
+    if "extra_categorical_mappings" in _scvi_dict.keys():
+        target_extra_cat_maps = adata.uns["_scvi"]["extra_categorical_mappings"]
+        for key, val in _scvi_dict["extra_categorical_mappings"].items():
+            target_map = target_extra_cat_maps[key]
+            if not _assert_equal_mapping(val, target_map):
+                raise ValueError(error_msg.format(key, val, target_map))
+        # check order of cols in df is same
+        target_cat_keys = adata.uns["_scvi"]["extra_categorical_keys"]
+        if not np.array_equal(_scvi_dict["extra_categorical_keys"], target_cat_keys):
+            raise ValueError(
+                "extra_continous_keys are not the same between source and target"
+            )
+    # validate any extra continuous covs
+    if "extra_continuous_keys" in _scvi_dict.keys():
+        if "extra_continuous_keys" not in adata.uns["_scvi"].keys():
+            raise ValueError('extra_continuous_keys not in adata.uns["_scvi"]')
+        target_cont_keys = adata.uns["_scvi"]["extra_continuous_keys"]
+        if not np.array_equal(_scvi_dict["extra_continuous_keys"], target_cont_keys):
+            raise ValueError(
+                "extra_continous_keys are not the same between source and target"
+            )
+
+
+def _assert_equal_mapping(mapping1, mapping2):
+
+    return pd.Index(mapping1).equals(pd.Index(mapping2))
