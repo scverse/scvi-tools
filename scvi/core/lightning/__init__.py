1691207cce2d39e1de87a4645e9b3c9491b1e078
galen xing <gx2113@columbia.edu>
2020-11-17 01:47:28 -0800

    semisupervisedtask and scanvi

--- scvi/core/lightning/__init__.py
+++ scvi/core/lightning/__init__.py
@@ -1,4 +1,4 @@
-from .vaetask import VAETask
+from .vaetask import VAETask, SemiSupervisedTask
 from .trainer import Trainer
 
-__all__ = ["VAETask", "Trainer"]
+__all__ = ["VAETask", "Trainer", "SemiSupervisedTask"]
--- scvi/core/trainers/__init__.py
+++ scvi/core/trainers/__init__.py
@@ -1,4 +1,3 @@
-from .annotation import ClassifierTrainer, SemiSupervisedTrainer
 from .inference import UnsupervisedTrainer
 from .jvae_trainer import JVAETrainer
 from .total_inference import TotalTrainer
@@ -6,7 +5,5 @@ from .total_inference import TotalTrainer
 __all__ = [
     "UnsupervisedTrainer",
     "TotalTrainer",
-    "SemiSupervisedTrainer",
     "JVAETrainer",
-    "ClassifierTrainer",
 ]
