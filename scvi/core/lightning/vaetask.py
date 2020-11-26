handle elbo with global kl, various fixes

scvi/core/lightning/vaetask.py
@@ -1,3 +1,4 @@
+from inspect import getfullargspec
 from typing import Union
 
 import pytorch_lightning as pl
@@ -20,15 +21,42 @@ class VAETask(pl.LightningModule):
     Parameters
     ----------
     vae_model
-        A model instance from class ``AbstractVAE``
+        A model instance from class ``AbstractVAE``.
+    n_obs_training
+        Number of observations in the training set.
+    lr
+        Learning rate used for optimization :class:`~torch.optim.Adam`.
+    weight_decay
+        Weight decay used in :class:`~torch.optim.Adam`.
+    n_steps_kl_warmup
+        Number of training steps (minibatches) to scale weight on KL divergences from 0 to 1.
+        Only activated when `n_epochs_kl_warmup` is set to None.
+    n_epochs_kl_warmup
+        Number of epochs to scale weight on KL divergences from 0 to 1.
+        Overrides `n_steps_kl_warmup` when both are not `None`.
+    reduce_lr_on_plateau
+        Whether to monitor validation loss and reduce learning rate when validation set
+        `lr_scheduler_metric` plateaus.
+    lr_factor
+        Factor to reduce learning rate.
+    lr_patience
+        Number of epochs with no improvement after which learning rate will be reduced.
+    lr_threshold
+        Threshold for measuring the new optimum.
+    lr_scheduler_metric
+        Which metric to track for learning rate reduction.
+    **loss_kwargs
+        Keyword args to pass to the loss method of the `vae_model`.
+        `kl_weight` should not be passed here and is handled automatically.
     """
 
     def __init__(
         self,
         vae_model: AbstractVAE,
+        n_obs_training: int,
         lr=1e-3,
         weight_decay=1e-6,
-        n_iter_kl_warmup: Union[int, None] = None,
+        n_steps_kl_warmup: Union[int, None] = None,
         n_epochs_kl_warmup: Union[int, None] = 400,
         reduce_lr_on_plateau: bool = False,
         lr_factor: float = 0.6,
@@ -37,26 +65,40 @@ class VAETask(pl.LightningModule):
         lr_scheduler_metric: Literal[
             "elbo_validation", "reconstruction_loss_validation", "kl_local_validation"
         ] = "elbo_validation",
+        **loss_kwargs,
     ):
         super(VAETask, self).__init__()
         self.model = vae_model
+        self.n_obs_training = n_obs_training
         self.lr = lr
         self.weight_decay = weight_decay
-        self.n_iter_kl_warmup = n_iter_kl_warmup
+        self.n_steps_kl_warmup = n_steps_kl_warmup
         self.n_epochs_kl_warmup = n_epochs_kl_warmup
         self.reduce_lr_on_plateau = reduce_lr_on_plateau
         self.lr_factor = lr_factor
         self.lr_patience = lr_patience
         self.lr_scheduler_metric = lr_scheduler_metric
         self.lr_threshold = lr_threshold
+        self.loss_kwargs = loss_kwargs
+
+        # automatic handling of kl weight
+        loss_args = getfullargspec(self.model.loss)[0]
+        if "kl_weight" in loss_args:
+            self.loss_kwargs.update({"kl_weight": self.kl_weight})
+
+        if "n_obs" in loss_args:
+            self.loss_kwargs.update({"n_obs": n_obs_training})
 
     def forward(self, *args, **kwargs):
-        """Passthrough to model.forward()."""
+        """Passthrough to `model.forward()`."""
         return self.model(*args, **kwargs)
 
     def training_step(self, batch, batch_idx, optimizer_idx=0):
-        loss_kwargs = dict(kl_weight=self.kl_weight)
-        inference_outputs, _, scvi_loss = self.forward(batch, loss_kwargs=loss_kwargs)
+        if "kl_weight" in self.loss_kwargs:
+            self.loss_kwargs.update({"kl_weight": self.kl_weight})
+        inference_outputs, _, scvi_loss = self.forward(
+            batch, loss_kwargs=self.loss_kwargs
+        )
         reconstruction_loss = scvi_loss.reconstruction_loss
         # pytorch lightning automatically backprops on "loss"
         return {
@@ -135,23 +177,65 @@ class VAETask(pl.LightningModule):
     def kl_weight(self):
         """Scaling factor on KL divergence during training."""
         epoch_criterion = self.n_epochs_kl_warmup is not None
-        iter_criterion = self.n_iter_kl_warmup is not None
+        step_criterion = self.n_steps_kl_warmup is not None
         if epoch_criterion:
             kl_weight = min(1.0, self.current_epoch / self.n_epochs_kl_warmup)
-        elif iter_criterion:
-            kl_weight = min(1.0, self.global_step / self.n_iter_kl_warmup)
+        elif step_criterion:
+            kl_weight = min(1.0, self.global_step / self.n_steps_kl_warmup)
         else:
             kl_weight = 1.0
         return kl_weight
 
 
 class AdvesarialTask(VAETask):
+    """
+    Train vaes with adversarial loss option to encourage latent space mixing.
+
+    Parameters
+    ----------
+    vae_model
+        A model instance from class ``AbstractVAE``.
+    n_obs_training
+        Number of observations in the training set.
+    lr
+        Learning rate used for optimization :class:`~torch.optim.Adam`.
+    weight_decay
+        Weight decay used in :class:`~torch.optim.Adam`.
+    n_steps_kl_warmup
+        Number of training steps (minibatches) to scale weight on KL divergences from 0 to 1.
+        Only activated when `n_epochs_kl_warmup` is set to None.
+    n_epochs_kl_warmup
+        Number of epochs to scale weight on KL divergences from 0 to 1.
+        Overrides `n_steps_kl_warmup` when both are not `None`.
+    reduce_lr_on_plateau
+        Whether to monitor validation loss and reduce learning rate when validation set
+        `lr_scheduler_metric` plateaus.
+    lr_factor
+        Factor to reduce learning rate.
+    lr_patience
+        Number of epochs with no improvement after which learning rate will be reduced.
+    lr_threshold
+        Threshold for measuring the new optimum.
+    lr_scheduler_metric
+        Which metric to track for learning rate reduction.
+    adversarial_classifier
+        Whether to use adversarial classifier in the latent space
+    scale_adversarial_loss
+        Scaling factor on the adversarial components of the loss.
+        By default, adversarial loss is scaled from 1 to 0 following opposite of
+        kl warmup.
+    **loss_kwargs
+        Keyword args to pass to the loss method of the `vae_model`.
+        `kl_weight` should not be passed here and is handled automatically.
+    """
+
     def __init__(
         self,
         vae_model: AbstractVAE,
+        n_obs_training,
         lr=1e-3,
         weight_decay=1e-6,
-        n_iter_kl_warmup: Union[int, None] = None,
+        n_steps_kl_warmup: Union[int, None] = None,
         n_epochs_kl_warmup: Union[int, None] = 400,
         reduce_lr_on_plateau: bool = False,
         lr_factor: float = 0.6,
@@ -162,12 +246,14 @@ class AdvesarialTask(VAETask):
         ] = "elbo_validation",
         adversarial_classifier: Union[bool, Classifier] = False,
         scale_adversarial_loss: Union[float, Literal["auto"]] = "auto",
+        **loss_kwargs,
     ):
         super(AdvesarialTask, self).__init__(
             vae_model=vae_model,
+            n_obs_training=n_obs_training,
             lr=lr,
             weight_decay=weight_decay,
-            n_iter_kl_warmup=n_iter_kl_warmup,
+            n_steps_kl_warmup=n_steps_kl_warmup,
             n_epochs_kl_warmup=n_epochs_kl_warmup,
             reduce_lr_on_plateau=reduce_lr_on_plateau,
             lr_factor=lr_factor,
@@ -309,7 +395,7 @@ class SemiSupervisedTask(VAETask):
         vae_model: AbstractVAE,
         lr=1e-3,
         weight_decay=1e-6,
-        n_iter_kl_warmup: Union[int, None] = None,
+        n_steps_kl_warmup: Union[int, None] = None,
         n_epochs_kl_warmup: Union[int, None] = 400,
         n_labelled_samples_per_class=50,
         indices_labelled=None,
@@ -332,7 +418,7 @@ class SemiSupervisedTask(VAETask):
             vae_model=vae_model,
             lr=lr,
             weight_decay=weight_decay,
-            n_iter_kl_warmup=n_iter_kl_warmup,
+            n_steps_kl_warmup=n_steps_kl_warmup,
             n_epochs_kl_warmup=n_epochs_kl_warmup,
             reduce_lr_on_plateau=reduce_lr_on_plateau,
             lr_factor=lr_factor,

scvi/core/models/vaemixin.py
@@ -161,7 +161,7 @@ class VAEMixin:
                 # does each model need to have this latent distribution param?
                 if self.model.latent_distribution == "ln":
                     samples = Normal(qz_m, qz_v.sqrt()).sample([mc_samples])
-                    z = self.z_encoder.z_transformation(samples)
+                    z = torch.nn.functional.softmax(samples, dim=-1)
                     z = z.mean(dim=0)
                 else:
                     z = qz_m

scvi/core/modules/autozivae.py
@@ -364,34 +364,9 @@ class AutoZIVAE(VAE):
         tensors,
         inference_outputs,
         generative_outputs,
-        kl_weight=1.0,
-        scale_loss: float = 1.0,
+        kl_weight: int = 1.0,
+        n_obs: int = 1.0,
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        r"""
-        Returns the reconstruction loss and the Kullback divergences.
-
-        Parameters
-        ----------
-        x
-            tensor of values with shape (batch_size, n_input)
-        local_l_mean
-            tensor of means of the prior distribution of latent variable l
-            with shape (batch_size, 1)
-        local_l_var
-            tensor of variancess of the prior distribution of latent variable l
-            with shape (batch_size, 1)
-        batch_index
-            array that indicates which batch the cells belong to with shape ``batch_size``
-        y
-            tensor of cell-types labels with shape (batch_size, n_labels)
-
-
-        Returns
-        -------
-        2-tuple of :py:class:`torch.FloatTensor`
-            the reconstruction loss and the Kullback divergences
-
-        """
         # Parameters for z latent distribution
         qz_m = inference_outputs["qz_m"]
         qz_v = inference_outputs["qz_v"]
@@ -425,15 +400,12 @@ class AutoZIVAE(VAE):
             x, px_rate, px_r, px_dropout, bernoulli_params
         )
 
-        reconst_loss = reconst_loss
-
         kl_global = kl_divergence_bernoulli
         kl_local_for_warmup = kl_divergence_l
         kl_local_no_warmup = kl_divergence_z
 
         weighted_kl_local = kl_weight * kl_local_for_warmup + kl_local_no_warmup
-        loss = torch.mean(reconst_loss + weighted_kl_local) + kl_global
-        loss = loss * scale_loss
+        loss = n_obs * torch.mean(reconst_loss + weighted_kl_local) + kl_global
         kl_local = dict(
             kl_divergence_l=kl_divergence_l, kl_divergence_z=kl_divergence_z
         )

scvi/core/modules/vae.py
@@ -300,7 +300,6 @@ class VAE(AbstractVAE):
         inference_outputs,
         generative_outputs,
         kl_weight: float = 1.0,
-        n_obs: float = 1.0,
     ):
         x = tensors[_CONSTANTS.X_KEY]
         local_l_mean = tensors[_CONSTANTS.LOCAL_L_MEAN_KEY]
@@ -333,14 +332,10 @@ class VAE(AbstractVAE):
 
         kl_local_for_warmup = kl_divergence_l
         kl_local_no_warmup = kl_divergence_z
-        kl_global_for_warmup = 0.0
-        kl_global_no_warmup = 0.0
 
         weighted_kl_local = kl_weight * kl_local_for_warmup + kl_local_no_warmup
-        weighted_kl_global = kl_weight * kl_global_for_warmup + kl_global_no_warmup
 
-        loss = n_obs * torch.mean(reconst_loss + weighted_kl_local) + weighted_kl_global
-        loss /= n_obs
+        loss = torch.mean(reconst_loss + weighted_kl_local)
 
         kl_local = dict(
             kl_divergence_l=kl_divergence_l, kl_divergence_z=kl_divergence_z