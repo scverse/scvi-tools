handle elbo with global kl, various fixes

scvi/core/models/base.py
@@ -241,22 +241,16 @@ class BaseModelClass(ABC):
             use_gpu = self.use_gpu
         else:
             use_gpu = use_gpu and torch.cuda.is_available()
-        if frequency is None:
-            check_val_every_n_epoch = np.inf
-        else:
-            check_val_every_n_epoch = frequency
-
-        if task_class is None:
-            task_class = self._task_class
-
-        task_kwargs = vae_task_kwargs if isinstance(vae_task_kwargs, dict) else dict()
-        self._pl_task = task_class(self.model, **task_kwargs)
         if use_gpu:
             gpus = 1
             pin_memory = True
         else:
             gpus = None
             pin_memory = False
+        if frequency is None:
+            check_val_every_n_epoch = np.inf
+        else:
+            check_val_every_n_epoch = frequency
 
         self.trainer = Trainer(
             max_epochs=n_epochs,
@@ -274,6 +268,12 @@ class BaseModelClass(ABC):
         self.train_indices_ = train_dl.indices
         self.test_indices_ = test_dl.indices
         self.validation_indices_ = val_dl.indices
+
+        if task_class is None:
+            task_class = self._task_class
+
+        task_kwargs = vae_task_kwargs if isinstance(vae_task_kwargs, dict) else dict()
+        self._pl_task = task_class(self.model, len(self.train_indices_), **task_kwargs)
 
         self.trainer.fit(self._pl_task, train_dl, val_dl)
         try:

scvi/core/modules/totalvae.py
@@ -602,9 +602,7 @@ class TOTALVAE(AbstractVAE):
                 .sum(dim=-1)
             )
             p_z = Normal(0, 1).log_prob(z).sum(dim=-1)
-            p_mu_back = self.model.back_mean_prior.log_prob(log_pro_back_mean).sum(
-                dim=-1
-            )
+            p_mu_back = self.back_mean_prior.log_prob(log_pro_back_mean).sum(dim=-1)
             p_xy_zl = -(reconst_loss_gene + reconst_loss_protein)
             q_z_x = Normal(qz_m, qz_v.sqrt()).log_prob(z).sum(dim=-1)
             q_l_x = Normal(ql_m, ql_v.sqrt()).log_prob(log_library).sum(dim=-1)

tests/models/test_models.py
@@ -154,7 +154,7 @@ def test_saving_and_loading(save_path):
         model = cls(adata, latent_distribution="normal")
         model.train(1, train_size=0.2)
         z1 = model.get_latent_representation(adata)
-        test_idx1 = model.test_indices
+        test_idx1 = model.validation_indices
         model.save(save_path, overwrite=True, save_anndata=True)
         model = cls.load(save_path)
         model.get_latent_representation()
@@ -163,7 +163,7 @@ def test_saving_and_loading(save_path):
             cls.load(save_path, tmp_adata)
         model = cls.load(save_path, adata)
         z2 = model.get_latent_representation()
-        test_idx2 = model.test_indices
+        test_idx2 = model.validation_indices
         np.testing.assert_array_equal(z1, z2)
         np.testing.assert_array_equal(test_idx1, test_idx2)
         assert model.is_trained is True
@@ -293,9 +293,9 @@ def test_autozi():
         autozivae.train(1, lr=1e-2, frequency=1)
         assert len(autozivae.history["elbo_train"]) == 1
         assert len(autozivae.history["elbo_test"]) == 1
-        autozivae.get_elbo(indices=autozivae.test_indices)
-        autozivae.get_reconstruction_error(indices=autozivae.test_indices)
-        autozivae.get_marginal_ll(indices=autozivae.test_indices)
+        autozivae.get_elbo(indices=autozivae.validation_indices)
+        autozivae.get_reconstruction_error(indices=autozivae.validation_indices)
+        autozivae.get_marginal_ll(indices=autozivae.validation_indices)
         autozivae.get_alphas_betas()
 
 
@@ -342,9 +342,9 @@ def test_totalvi(save_path):
     )
     # model.get_likelihood_parameters()
 
-    model.get_elbo(indices=model.test_indices)
-    model.get_marginal_ll(indices=model.test_indices)
-    model.get_reconstruction_error(indices=model.test_indices)
+    model.get_elbo(indices=model.validation_indices)
+    model.get_marginal_ll(indices=model.validation_indices)
+    model.get_reconstruction_error(indices=model.validation_indices)
 
     adata2 = synthetic_iid()
     norm_exp = model.get_normalized_expression(adata2, indices=[1, 2, 3])