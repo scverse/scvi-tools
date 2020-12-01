7f03ba15e29257f7233a40d23851ef48eb7936b7
adamgayoso <adamgayoso@users.noreply.github.com>
2020-12-01 10:55:26 -0800

    stereoscope skeleton

---
+++ scvi/external/stereoscope/_module.py
@@ -0,0 +1,11 @@
+from scvi.compose import AbstractVAE
+
+
+class RNADeconv(AbstractVAE):
+    def __init__():
+        pass
+
+
+class SpatialDeconv(AbstractVAE):
+    def __init__():
+        pass
---
+++ scvi/external/stereoscope/model.py
@@ -0,0 +1,11 @@
+from scvi.model.base import BaseModelClass
+
+
+class RNAStereoscope(BaseModelClass):
+    def __init__():
+        pass
+
+
+class SpatialStereoscope(BaseModelClass):
+    def __init__():
+        pass
