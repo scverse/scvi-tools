e90dacd7485e5342badb02c791d977fd9e73609d
romain-lopez <romain_lopez@berkeley.edu>
2021-01-31 00:59:46 +0000

    added scvi-pyro

--- tests/models/test_pyro.py
+++ tests/models/test_pyro.py
@@ -4,9 +4,10 @@ import torch
 import torch.nn as nn
 from pyro.infer.autoguide import AutoDiagonalNormal
 from pyro.nn import PyroModule
+from torch.distributions import constraints
 
 from scvi import _CONSTANTS
-from scvi.compose import PyroBaseModuleClass
+from scvi.compose import DecoderSCVI, Encoder, PyroBaseModuleClass
 from scvi.data import synthetic_iid
 from scvi.dataloaders import AnnDataLoader
 from scvi.lightning import PyroTrainingPlan, Trainer
@@ -42,6 +43,75 @@ class BayesianRegression(PyroModule, PyroBaseModuleClass):
         return self._auto_guide(x, y)
 
 
+class SCVIPyro(PyroModule, PyroBaseModuleClass):
+    def __init__(self, n_input, n_latent):
+
+        super().__init__()
+        self.n_input = n_input
+        self.n_latent = n_latent
+        self.epsilon = 5.0e-3
+        # z encoder goes from the n_input-dimensional data to an n_latent-d
+        # latent space representation
+        self.encoder = Encoder(
+            n_input,
+            n_latent,
+            n_layers=1,
+            n_hidden=128,
+            dropout_rate=0.1,
+        )
+        # decoder goes from n_latent-dimensional space to n_input-d data
+        self.decoder = DecoderSCVI(
+            n_latent,
+            n_input,
+            n_layers=1,
+            n_hidden=128,
+        )
+
+    @staticmethod
+    def _get_fn_args_from_batch(tensor_dict):
+        x = tensor_dict[_CONSTANTS.X_KEY]
+        log_library = torch.log(torch.sum(x, dim=1, keepdim=True) + 1e-6)
+        return (x, log_library), {}
+
+    def forward(self, x, log_library):
+        # register PyTorch module `decoder` with Pyro
+        pyro.module("decoder", self.decoder)
+
+        # This gene-level parameter modulates the variance of the observation distribution
+        px_r = pyro.param(
+            "inverse_dispersion",
+            10.0 * x.new_ones(self.n_input),
+            constraint=constraints.positive,
+        )
+        with pyro.plate("data", x.shape[0]):
+            # setup hyperparameters for prior p(z)
+            z_loc = x.new_zeros(torch.Size((x.shape[0], self.n_latent)))
+            z_scale = x.new_ones(torch.Size((x.shape[0], self.n_latent)))
+            # sample from prior (value will be sampled by guide when computing the ELBO)
+            z = pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
+            # decode the latent code z
+            px_scale, _, px_rate, px_dropout = self.decoder("gene", z, log_library)
+            # build count distribution
+            nb_logits = (px_rate + self.epsilon).log() - (px_r + self.epsilon).log()
+            x_dist = dist.ZeroInflatedNegativeBinomial(
+                gate_logits=px_dropout, total_count=px_r, logits=nb_logits
+            )
+            # score against actual counts
+            pyro.sample("obs", x_dist.to_event(1), obs=x)
+        return None
+
+    def guide(self, x, log_library):
+        # define the guide (i.e. variational distribution) q(z|x)
+        pyro.module("encoder", self.encoder)
+        with pyro.plate("data", x.shape[0]):
+            # use the encoder to get the parameters used to define q(z|x)
+            x_ = torch.log(1 + x)
+            z_loc, z_scale, _ = self.encoder(x_)
+            # sample the latent code z
+            pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
+        return None
+
+
 def test_pyro_bayesian_regression():
     use_gpu = int(torch.cuda.is_available())
     adata = synthetic_iid()
@@ -75,81 +145,6 @@ def test_pyro_bayesian_regression_jit():
     )
     trainer.fit(plan, train_dl)
 
-from scvi import _CONSTANTS
-from scvi.compose import (
-    BaseModuleClass,
-    DecoderSCVI,
-    Encoder,
-    LossRecorder,
-    auto_move_data,
-)
-from torch.distributions import constraints
-
-
-class SCVIPyro(PyroModule, PyroBaseModuleClass):
-    def __init__(self, n_input, n_latent):
-        
-        super().__init__()
-        self.n_input = n_input
-        self.n_latent = n_latent
-        self.epsilon = 5.0e-3
-        # z encoder goes from the n_input-dimensional data to an n_latent-d
-        # latent space representation
-        self.encoder = Encoder(
-            n_input,
-            n_latent,
-            n_layers=1,
-            n_hidden=128,
-            dropout_rate=0.1,
-        )
-        # decoder goes from n_latent-dimensional space to n_input-d data
-        self.decoder = DecoderSCVI(
-            n_latent,
-            n_input,
-            n_layers=1,
-            n_hidden=128,
-        )
-
-    @staticmethod
-    def _get_fn_args_from_batch(tensor_dict):
-        x = tensor_dict[_CONSTANTS.X_KEY]
-        library = torch.sum(x, dim=1, keepdim=True)
-        return (x, library), {}
-
-    def forward(self, x, l):
-        # register PyTorch module `decoder` with Pyro
-        pyro.module("decoder", self.decoder)
-
-        # This gene-level parameter modulates the variance of the observation distribution
-        px_r = pyro.param("inverse_dispersion", 10.0 * x.new_ones(self.n_input),
-                           constraint=constraints.positive)
-        with pyro.plate("data", x.shape[0]):
-            # setup hyperparameters for prior p(z)
-            z_loc = x.new_zeros(torch.Size((x.shape[0], self.n_latent)))
-            z_scale = x.new_ones(torch.Size((x.shape[0], self.n_latent)))
-            # sample from prior (value will be sampled by guide when computing the ELBO)
-            z = pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
-            # decode the latent code z
-            px_scale, _, px_rate, px_dropout = self.decoder("gene", z, l)
-            # build count distribution
-            nb_logits = (px_rate + self.epsilon).log() - (px_r + self.epsilon).log()
-            x_dist = dist.ZeroInflatedNegativeBinomial(gate_logits=px_dropout, total_count=px_r,
-                                                       logits=nb_logits)
-            # score against actual counts
-            pyro.sample("obs", x_dist.to_event(1), obs=x)
-        return None
-
-    def guide(self, x, l):
-        # define the guide (i.e. variational distribution) q(z|x)
-        pyro.module("encoder", self.encoder)
-        with pyro.plate("data", x.shape[0]):
-            # use the encoder to get the parameters used to define q(z|x)
-            x_ = torch.log(1 + x)
-            z_loc, z_scale, _ = self.encoder(x_)
-            # sample the latent code z
-            pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
-        return None
-
 
 def test_pyro_scvi():
     use_gpu = int(torch.cuda.is_available())
@@ -157,7 +152,6 @@ def test_pyro_scvi():
     train_dl = AnnDataLoader(adata, shuffle=True, batch_size=128)
     pyro.clear_param_store()
     model = SCVIPyro(adata.n_vars, 10)
-    # warmup guide for JIT
     plan = PyroTrainingPlan(model)
     trainer = Trainer(
         gpus=use_gpu,
