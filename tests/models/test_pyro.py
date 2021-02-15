fb04cdbca2de5ae801a4886ef16aaeed5eb7c8cd
adamgayoso <adamgayoso@users.noreply.github.com>
2021-02-14 18:52:44 -0800

    fully bayesian pyro test

--- tests/models/test_pyro.py
+++ tests/models/test_pyro.py
@@ -6,7 +6,8 @@ import pyro.distributions as dist
 import torch
 import torch.nn as nn
 from pyro.infer.autoguide import AutoDiagonalNormal
-from pyro.nn import PyroModule
+from pyro.nn import PyroModule, PyroSample
+from pytorch_lightning.callbacks import Callback
 
 from scvi import _CONSTANTS
 from scvi.compose import PyroBaseModuleClass
@@ -15,9 +16,33 @@ from scvi.dataloaders import AnnDataLoader
 from scvi.lightning import PyroTrainingPlan, Trainer
 
 
+class PyroSampleCallback(Callback):
+    def on_train_start(self, trainer, pl_module):
+        """
+        Way to use PyroSample and have it be on GPU.
+
+        In this case, the PyroSample objects are added after the
+        model has already been moved to cuda.
+        """
+
+        pyro_model = pl_module.module.model
+        pyro_model.linear.weight = PyroSample(
+            dist.Normal(pyro_model.zero, pyro_model.one)
+            .expand([pyro_model.out_features, pyro_model.in_features])
+            .to_event(2)
+        )
+        pyro_model.linear.bias = PyroSample(
+            dist.Normal(pyro_model.zero, pyro_model.ten)
+            .expand([pyro_model.out_features])
+            .to_event(1)
+        )
+
+
 class BayesianRegressionPyroModel(PyroModule):
     def __init__(self, in_features, out_features):
         super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
 
         self.register_buffer("zero", torch.tensor(0.0))
         self.register_buffer("one", torch.tensor(1.0))
@@ -49,26 +74,19 @@ class BayesianRegressionModule(PyroBaseModuleClass):
 
 
 def test_pyro_bayesian_regression(save_path):
-    use_gpu = 0
+    use_gpu = int(torch.cuda.is_available())
     adata = synthetic_iid()
     train_dl = AnnDataLoader(adata, shuffle=True, batch_size=128)
     pyro.clear_param_store()
     model = BayesianRegressionModule(adata.shape[1], 1)
     plan = PyroTrainingPlan(model)
-    trainer = Trainer(
-        gpus=use_gpu,
-        max_epochs=2,
-    )
+    trainer = Trainer(gpus=use_gpu, max_epochs=2, callbacks=[PyroSampleCallback()])
     trainer.fit(plan, train_dl)
 
     # test save and load
-    post_dl = AnnDataLoader(adata, shuffle=False, batch_size=128)
-    mean1 = []
-    with torch.no_grad():
-        for tensors in post_dl:
-            args, kwargs = model._get_fn_args_from_batch(tensors)
-            mean1.append(model(*args, **kwargs).cpu().numpy())
-    mean1 = np.concatenate(mean1)
+    quants = model.guide.quantiles([0.5])
+    sigma_median = quants["sigma"][0].cpu().detach().numpy()
+    linear_median = quants["linear.weight"][0].cpu().detach().numpy()
 
     model_save_path = os.path.join(save_path, "model_params.pt")
     torch.save(model.state_dict(), model_save_path)
@@ -82,26 +100,23 @@ def test_pyro_bayesian_regression(save_path):
         if isinstance(new_model, PyroBaseModuleClass):
             plan = PyroTrainingPlan(new_model)
             trainer = Trainer(
-                gpus=use_gpu,
-                max_steps=1,
+                gpus=use_gpu, max_steps=1, callbacks=[PyroSampleCallback()]
             )
             trainer.fit(plan, train_dl)
             new_model.load_state_dict(torch.load(model_save_path))
         else:
             raise err
 
-    mean2 = []
-    with torch.no_grad():
-        for tensors in post_dl:
-            args, kwargs = new_model._get_fn_args_from_batch(tensors)
-            mean2.append(new_model(*args, **kwargs).cpu().numpy())
-    mean2 = np.concatenate(mean2)
+    quants = new_model.guide.quantiles([0.5])
+    sigma_median_new = quants["sigma"][0].cpu().detach().numpy()
+    linear_median_new = quants["linear.weight"][0].cpu().detach().numpy()
 
-    np.testing.assert_array_equal(mean1, mean2)
+    np.testing.assert_array_equal(sigma_median_new, sigma_median)
+    np.testing.assert_array_equal(linear_median_new, linear_median)
 
 
 def test_pyro_bayesian_regression_jit():
-    use_gpu = 0
+    use_gpu = int(torch.cuda.is_available())
     adata = synthetic_iid()
     train_dl = AnnDataLoader(adata, shuffle=True, batch_size=128)
     pyro.clear_param_store()
