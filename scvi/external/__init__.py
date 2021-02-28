20a5f047b076e4b7b608a9ba42b2da7dbcd46acb
Kathy Wu <kathywu@berkeley.edu>
2021-02-27 20:56:19 -0800

    isort

--- scvi/external/__init__.py
+++ scvi/external/__init__.py
@@ -1,6 +1,6 @@
+from .cellassign import CellAssign
 from .gimvi import GIMVI
 from .solo import SOLO
 from .stereoscope import RNAStereoscope, SpatialStereoscope
-from .cellassign import CellAssign
 
 __all__ = ["SOLO", "GIMVI", "RNAStereoscope", "SpatialStereoscope", "CellAssign"]
