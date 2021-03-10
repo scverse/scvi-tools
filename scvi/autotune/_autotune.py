2eb27f7060f2dcd5a5db393f0dd33b38a497f204
mjayasur <mjayasur@berkeley.edu>
2021-03-10 12:16:03 -0800

    basic working autotune api for any scvi-tools model

--- scvi/autotune/_autotune.py
+++ scvi/autotune/_autotune.py
@@ -5,37 +5,50 @@ from ray.tune.schedulers import ASHAScheduler
 
 
 class Autotune:
-    def __init__(self, adata, model, n_samples=10):
+    def __init__(self, adata, model, name, metric, metrics, config, num_samples=10):
         self.adata = adata
         self.model = model
-        self.n_samples = n_samples
+        self.name = name
+        self.metric = metric
+        self.metrics = metrics
+        self.config = config
+        self.num_samples = num_samples
+        self.scheduler = ASHAScheduler(max_t=10, grace_period=1, reduction_factor=2)
+        self.reporter = CLIReporter(metric_columns=self.metrics)
 
-    def train_scvi_tune(self, config, adata=None):
-        _model = self.model(adata, **config)
-        callback = TuneReportCallback(
-            {"loss": "avg_val_loss", "mean_accuracy": "avg_val_accuracy"},
-            on="validation_end",
+    def set_reporter(self, reporter):
+        self.reporter = reporter
+
+    def set_scheduler(self, scheduler):
+        self.scheduler = scheduler
+
+    def train_model_tune(self, config, adata, metrics):
+        _current_model = self.model(adata, **config)
+        _current_model.train(
+            check_val_every_n_epoch=1,
+            callbacks=[
+                TuneReportCallback(
+                    metrics,
+                    on="validation_end",
+                )
+            ],
+            max_epochs=2,  # remove this later
         )
-        _model.train(callbacks=[callback], max_epochs=2)
 
-    def tune_scvi_asha(self):
-        config = {
-            "dropout_rate": tune.loguniform(1e-4, 1e-1),
-        }
-        scheduler = ASHAScheduler(max_t=10, grace_period=1, reduction_factor=2)
-        reporter = CLIReporter(
-            metric_columns=["loss", "mean_accuracy", "training_iteration"]
-        )
-
+    def __call__(self):
         analysis = tune.run(
-            tune.with_parameters(self.train_scvi_tune, adata=self.adata),
-            metric="loss",
+            tune.with_parameters(
+                self.train_model_tune, adata=self.adata, metrics=self.metrics
+            ),
+            metric=self.metric,
             mode="min",
-            config=config,
-            num_samples=self.n_samples,
-            scheduler=scheduler,
-            progress_reporter=reporter,
-            name="tune_scvi_asha",
+            config=self.config,
+            num_samples=self.num_samples,
+            scheduler=self.scheduler,
+            progress_reporter=self.reporter,
+            name=self.name,
         )
+        best_config = analysis.best_config
+        print("Best hyperparameters found were: ", best_config)
 
-        print("Best hyperparameters found were: ", analysis.best_config)
+        return self.model(self.adata, **best_config)
--- tests/autotune/test_autotune.py
+++ tests/autotune/test_autotune.py
@@ -1,3 +1,5 @@
+from ray.tune import loguniform
+
 from scvi.autotune import Autotune
 from scvi.data import synthetic_iid
 from scvi.model import SCVI
@@ -5,5 +7,12 @@ from scvi.model import SCVI
 
 def test_autotune():
     adata = synthetic_iid()
-    tuner = Autotune(adata=adata, model=SCVI)
-    tuner.tune_scvi_asha()
+    metrics = [
+        "elbo_validation",
+        "reconstruction_loss_validation",
+        "kl_local_validation",
+        "kl_global_validation",
+    ]
+    config = {"dropout_rate": loguniform(1e-4, 1e-1)}
+    tuner = Autotune(adata, SCVI, "scvi_tuner", "elbo_validation", metrics, config)
+    tuner()
