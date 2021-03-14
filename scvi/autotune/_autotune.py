1f148b5211091b5afce38071fd20f6bbacc48e3c
mjayasur <mjayasur@berkeley.edu>
2021-03-14 00:05:04 -0800

    autotune reformatting

--- scvi/autotune/_autotune.py
+++ scvi/autotune/_autotune.py
@@ -2,26 +2,33 @@ from ray import tune
 from ray.tune import CLIReporter
 from ray.tune.integration.pytorch_lightning import TuneReportCallback
 from ray.tune.schedulers import ASHAScheduler
+from typing import Optional
+
+"""
+user needs to be able to set search space for model kwargs, trainer kwargs, training plan kwargs. 
+track validation metrics, as well as metrics after training that come from metric functions
+"""
 
 
 class Autotune:
-    def __init__(self, adata, model, name, metric, metrics, config, num_samples=10):
-        self.adata = adata
-        self.model = model
-        self.name = name
-        self.metric = metric
-        self.metrics = metrics
-        self.config = config
-        self.num_samples = num_samples
-        self.scheduler = ASHAScheduler(max_t=10, grace_period=1, reduction_factor=2)
-        self.reporter = CLIReporter(metric_columns=self.metrics)
-
-    def set_reporter(self, reporter):
-        self.reporter = reporter
-
-    def set_scheduler(self, scheduler):
-        self.scheduler = scheduler
-
+    def __init__(
+        self,
+        adata,
+        model,
+        name: str = "scvi-experiment",
+        metric: str = "",
+        training_metrics: dict = None,
+        post_training_metric_functions: dict = None,
+        model_hyperparams: dict = None,
+        trainer_hyperparams: dict = None,
+        training_plan_hyperparams: dict = None,
+        search_alg: Searcher = None,
+        scheduler: TrialScheduler = None,
+        checkpoint_dir: str = "experiment-checkpoints"
+    ):
+        if not training_metrics
+        
+        
     def train_model_tune(self, config, adata, metrics):
         _current_model = self.model(adata, **config)
         _current_model.train(
@@ -35,18 +42,18 @@ class Autotune:
             max_epochs=2,  # remove this later
         )
 
-    def __call__(self):
+    def run(self, metric, mode, name, num_samples):
         analysis = tune.run(
             tune.with_parameters(
                 self.train_model_tune, adata=self.adata, metrics=self.metrics
             ),
-            metric=self.metric,
+            metric=metric,
             mode="min",
             config=self.config,
-            num_samples=self.num_samples,
+            num_samples=num_samples,
             scheduler=self.scheduler,
             progress_reporter=self.reporter,
-            name=self.name,
+            name=name,
         )
         best_config = analysis.best_config
         print("Best hyperparameters found were: ", best_config)
