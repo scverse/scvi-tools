8d84b2d0be3637d49a01938ae77ff0678a4a1d40
mjayasur <mjayasur@berkeley.edu>
2021-03-26 09:48:02 -0700

    really janky checkpointing, need to fix this

--- tests/autotune/test_autotune.py
+++ tests/autotune/test_autotune.py
@@ -1,5 +1,5 @@
 from ray.tune import loguniform
-from ray.tune.schedulers import ASHAScheduler, PopulationBasedTraining
+from ray.tune.schedulers import ASHAScheduler
 
 from scvi.autotune import Autotune
 from scvi.data import synthetic_iid
@@ -23,32 +23,7 @@ def test_autotune():
         plan_hyperparams=plan_config,
     )
     asha_scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
-    tuner.run(metric="elbo_validation", scheduler=asha_scheduler)
-
-
-def test_pbt():
-    adata = synthetic_iid()
-    metrics = [
-        "elbo_validation",
-        "reconstruction_loss_validation",
-    ]
-    model_config = {"dropout_rate": 1e-3}
-    plan_config = {"lr": 1e-3}
-    tuner = Autotune(
-        adata,
-        SCVI,
-        training_metrics=metrics,
-        model_hyperparams=model_config,
-        plan_hyperparams=plan_config,
-    )
-    pbt_scheduler = PopulationBasedTraining(
-        perturbation_interval=4,
-        hyperparam_mutations={
-            "lr": loguniform(1e-4, 1e-1),
-            "dropout_rate": loguniform(1e-4, 1e-1),
-        },
-    )
-    tuner.run(metric="elbo_validation", scheduler=pbt_scheduler)
+    print(tuner.run(metric="elbo_validation", scheduler=asha_scheduler))
 
 
 def test_metric_functions():
