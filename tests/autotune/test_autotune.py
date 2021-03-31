138934eacd972bde54f862d3e13aabb024769dd6
mjayasur <mjayasur@berkeley.edu>
2021-03-31 14:49:35 -0700

    adds support for metric functions using callback

--- tests/autotune/test_autotune.py
+++ tests/autotune/test_autotune.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from ray.tune import loguniform
 from ray.tune.schedulers import ASHAScheduler
 
@@ -23,8 +25,26 @@ def test_autotune():
         plan_hyperparams=plan_config,
     )
     asha_scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
-    print(tuner.run(metric="elbo_validation", scheduler=asha_scheduler))
+    model, _ = tuner.run(metric="elbo_validation", scheduler=asha_scheduler)
 
 
-def test_metric_functions():
-    pass
+def test_metric_function_dummy():
+    adata = synthetic_iid()
+    metrics = [
+        "elbo_validation",
+        "reconstruction_loss_validation",
+    ]
+    metric_functions = {"dummy": lambda x: randint(1, 10)}
+    model_config = {"dropout_rate": loguniform(1e-4, 1e-1)}
+    plan_config = {"lr": loguniform(1e-4, 1e-1)}
+    num_epochs = 2
+    tuner = Autotune(
+        adata,
+        SCVI,
+        metric_functions=metric_functions,
+        training_metrics=metrics,
+        model_hyperparams=model_config,
+        plan_hyperparams=plan_config,
+    )
+    asha_scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
+    model, analysis = tuner.run(metric="elbo_validation", scheduler=asha_scheduler)
