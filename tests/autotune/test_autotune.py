e563ad359bcfbc90382ff947618ea42a0ae3b241
mjayasur <mjayasur@berkeley.edu>
2021-03-31 15:44:36 -0700

    adds silhouette score test with terrible workaround for is_trained_ error

--- tests/autotune/test_autotune.py
+++ tests/autotune/test_autotune.py
@@ -1,7 +1,9 @@
 from random import randint
 
+import scanpy as sc
 from ray.tune import loguniform
 from ray.tune.schedulers import ASHAScheduler
+from sklearn.metrics import silhouette_score
 
 from scvi.autotune import Autotune
 from scvi.data import synthetic_iid
@@ -35,6 +37,41 @@ def test_metric_function_dummy():
         "reconstruction_loss_validation",
     ]
     metric_functions = {"dummy": lambda x: randint(1, 10)}
+    model_config = {"dropout_rate": loguniform(1e-4, 1e-1)}
+    plan_config = {"lr": loguniform(1e-4, 1e-1)}
+    num_epochs = 2
+    tuner = Autotune(
+        adata,
+        SCVI,
+        metric_functions=metric_functions,
+        training_metrics=metrics,
+        model_hyperparams=model_config,
+        plan_hyperparams=plan_config,
+    )
+    asha_scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
+    model, analysis = tuner.run(metric="elbo_validation", scheduler=asha_scheduler)
+
+
+def test_silhouette():
+    def silhouette_metric(model):
+        model.is_trained_ = True
+        latent = model.get_latent_representation()
+        model.is_trained_ = False
+        adata.obsm["X_scVI"] = latent
+        sc.pp.neighbors(adata, use_rep="X_scVI")
+        sc.tl.leiden(adata, key_added="leiden_scVI", resolution=0.5)
+        return silhouette_score(
+            adata.obsm["X_scVI"],
+            adata.obs["leiden_scVI"],
+        )
+
+    adata = synthetic_iid()
+
+    metrics = [
+        "elbo_validation",
+        "reconstruction_loss_validation",
+    ]
+    metric_functions = {"silhouette_score": silhouette_metric}
     model_config = {"dropout_rate": loguniform(1e-4, 1e-1)}
     plan_config = {"lr": loguniform(1e-4, 1e-1)}
     num_epochs = 2
