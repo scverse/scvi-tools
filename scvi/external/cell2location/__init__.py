c84040870148ff9f7ab76bba69b79eb3d0fa2b6f
Vitalii Kleshchevnikov <vk7@sanger.ac.uk>
2021-06-30 02:29:13 +0100

    import regression model

--- scvi/external/cell2location/__init__.py
+++ scvi/external/cell2location/__init__.py
@@ -1,7 +1,7 @@
 from ._cell2location_v1 import Cell2location
-from ._reference_module import compute_cluster_averages
+from ._reference_module import RegressionModel, compute_cluster_averages
 
-__all__ = ["Cell2location", "compute_cluster_averages"]
+__all__ = ["Cell2location", "RegressionModel", "compute_cluster_averages"]
 
 from pyro.distributions import constraints
 from pyro.distributions.transforms import SoftplusTransform
