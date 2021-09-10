af13fb657b77c132be5eacd26a88e047106a8c44
Justin Hong <jjhong922@berkeley.edu>
2021-09-10 15:39:29 -0700

    Fix CondSCVI docstring

--- scvi/model/_condscvi.py
+++ scvi/model/_condscvi.py
@@ -42,7 +42,7 @@ class CondSCVI(RNASeqMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass)
     Examples
     --------
     >>> adata = anndata.read_h5ad(path_to_anndata)
-    >>> scvi.data.setup_anndata(adata, batch_key="batch")
+    >>> scvi.data.setup_anndata(adata, labels_key="labels")
     >>> vae = scvi.external.CondSCVI(adata)
     >>> vae.train()
     >>> adata.obsm["X_CondSCVI"] = vae.get_latent_representation()
