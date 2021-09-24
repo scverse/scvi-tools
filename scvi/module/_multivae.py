f4595520b6c90b1df81f2ba0f387b2f3d4615e07
marianogabitto <mariano.gabitto@gmail.com>
2021-09-23 18:27:55 -0700

    Updata Protein Mask 1

--- scvi/module/_multivae.py
+++ scvi/module/_multivae.py
@@ -503,8 +503,7 @@ class MULTIVAE(BaseModuleClass):
 
         mask_expr = x_rna.sum(dim=1) > 0
         mask_acc = x_chr.sum(dim=1) > 0
-        mask_pro = 1
-        # TO DO adjust mask protein
+        mask_pro = y.sum(dim=1) > 0
 
         if cont_covs is not None and self.encode_covariates:
             encoder_input_expression = torch.cat((x_rna, cont_covs), dim=-1)
@@ -748,8 +747,7 @@ class MULTIVAE(BaseModuleClass):
 
         mask_expr = x_rna.sum(dim=1) > 0
         mask_acc = x_chr.sum(dim=1) > 0
-        mask_pro = 1
-        # TO DO adjust mask protein
+        mask_pro = y.sum(dim=1) > 0
 
         pro_batch_mask_minibatch = None
         if self.protein_batch_mask is not None:
