5b634338cea720035491f44def405f6b4befb46c
talashuach <tal.ashuach@gmail.com>
2021-10-04 15:28:55 -0700

    return dataframe instead of matrix (#1198)
    * return dataframe instead of matrix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update scvi/model/_multivi.py
    
    Co-authored-by: Adam Gayoso <adamgayoso@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- scvi/model/_peakvi.py
+++ scvi/model/_peakvi.py
@@ -285,14 +285,15 @@ class PEAKVI(ArchesMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
         adata: Optional[AnnData] = None,
         indices: Sequence[int] = None,
         n_samples_overall: Optional[int] = None,
-        region_indices: Sequence[int] = None,
+        region_list: Optional[Sequence[str]] = None,
         transform_batch: Optional[Union[str, int]] = None,
         use_z_mean: bool = True,
         threshold: Optional[float] = None,
         normalize_cells: bool = False,
         normalize_regions: bool = False,
         batch_size: int = 128,
-    ) -> Union[np.ndarray, csr_matrix]:
+        return_numpy: bool = False,
+    ) -> Union[pd.DataFrame, np.ndarray, csr_matrix]:
         """
         Impute the full accessibility matrix.
 
@@ -308,8 +309,9 @@ class PEAKVI(ArchesMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
             Indices of cells in adata to use. If `None`, all cells are used.
         n_samples_overall
             Number of samples to return in total
-        region_indices
-            Indices of regions to use. if `None`, all regions are used.
+        region_list
+            Return accessibility estimates for this subset of regions. if `None`, all regions are used.
+            This can save memory when dealing with large datasets.
         transform_batch
             Batch to condition on.
             If transform_batch is:
@@ -331,6 +333,10 @@ class PEAKVI(ArchesMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
             default.
         batch_size
             Minibatch size for data loading into model
+        return_numpy
+            If `True` and `threshold=None`, return :class:`~numpy.ndarray`. If `True` and `threshold` is
+            given, return :class:`~scipy.sparse.csr_matrix`. If `False`, return :class:`~pandas.DataFrame`.
+            DataFrame includes regions names as columns.
         """
         adata = self._validate_anndata(adata)
         if indices is None:
@@ -341,6 +347,12 @@ class PEAKVI(ArchesMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
             adata=adata, indices=indices, batch_size=batch_size
         )
         transform_batch = _get_batch_code_from_category(adata, transform_batch)
+
+        if region_list is None:
+            region_mask = slice(None)
+        else:
+            all_regions = _get_var_names_from_setup_anndata(adata)
+            region_mask = [region in region_list for region in all_regions]
 
         if threshold is not None and (threshold < 0 or threshold > 1):
             raise ValueError("the provided threshold must be between 0 and 1")
@@ -364,8 +376,8 @@ class PEAKVI(ArchesMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
             if threshold:
                 p[p < threshold] = 0
                 p = csr_matrix(p.numpy())
-            if region_indices is not None:
-                p = p[:, region_indices]
+            if region_list is not None:
+                p = p[:, region_mask]
             imputed.append(p)
 
         if threshold:  # imputed is a list of csr_matrix objects
@@ -373,7 +385,20 @@ class PEAKVI(ArchesMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
         else:  # imputed is a list of tensors
             imputed = torch.cat(imputed).numpy()
 
-        return imputed
+        if return_numpy:
+            return imputed
+        elif threshold:
+            return pd.DataFrame.sparse.from_spmatrix(
+                imputed,
+                index=adata.obs_names[indices],
+                columns=adata.var_names[region_mask],
+            )
+        else:
+            return pd.DataFrame(
+                imputed,
+                index=adata.obs_names[indices],
+                columns=adata.var_names[region_mask],
+            )
 
     @_doc_params(
         doc_differential_expression=doc_differential_expression,
@@ -493,7 +518,7 @@ class PEAKVI(ArchesMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
                 "emp_prob2": result.emp_mean2,
             },
         )
-        return result.reindex(adata.var.index)
+        return result
 
     @staticmethod
     @setup_anndata_dsp.dedent
