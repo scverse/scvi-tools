5f6b9e48731e7ed91581a6e5e6cb7f9f3458bb8a
adamgayoso <adamgayoso@users.noreply.github.com>
2021-11-12 13:42:53 -0800

    fix tests to use save path, cleanup

--- tests/autotune/test_autotune.py
+++ tests/autotune/test_autotune.py
@@ -4,17 +4,17 @@ from random import randint
 from ray.tune import loguniform
 from ray.tune.schedulers import ASHAScheduler
 
-from scvi.autotune import Autotune, silhouette_metric, tune_scvi
+from scvi.autotune import Autotune, silhouette_metric_labels_batch, tune_scvi
 from scvi.data import synthetic_iid
 from scvi.model import SCVI
 
 
-def test_tune_scvi():
+def test_tune_scvi(save_path):
     adata = synthetic_iid()
-    tune_scvi(adata, 2)
+    tune_scvi(adata, 2, run_kwargs=dict(local_dir=save_path))
 
 
-def test_autotune():
+def test_autotune(save_path):
     adata = synthetic_iid()
     metrics = [
         "elbo_validation",
@@ -31,10 +31,12 @@ def test_autotune():
         plan_hyperparams=plan_config,
     )
     asha_scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
-    model, analysis = tuner.run(metric="elbo_validation", scheduler=asha_scheduler)
+    model, analysis = tuner.run(
+        metric="elbo_validation", scheduler=asha_scheduler, local_dir=save_path
+    )
 
 
-def test_metric_function_dummy():
+def test_metric_function_dummy(save_path):
     adata = synthetic_iid()
     metrics = [
         "elbo_validation",
@@ -53,10 +55,12 @@ def test_metric_function_dummy():
         plan_hyperparams=plan_config,
     )
     asha_scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
-    model, analysis = tuner.run(metric="elbo_validation", scheduler=asha_scheduler)
+    model, analysis = tuner.run(
+        metric="elbo_validation", scheduler=asha_scheduler, local_dir=save_path
+    )
 
 
-def test_silhouette():
+def test_silhouette(save_path):
 
     adata = synthetic_iid()
 
@@ -65,7 +69,9 @@ def test_silhouette():
         "reconstruction_loss_validation",
     ]
     metric_functions = {
-        "silhouette_score": partial(silhouette_metric, labels_key="labels")
+        "silhouette_score": partial(
+            silhouette_metric_labels_batch, labels_key="labels", batch_key="batch"
+        )
     }
     model_config = {"dropout_rate": loguniform(1e-4, 1e-1)}
     plan_config = {"lr": loguniform(1e-4, 1e-1)}
@@ -79,4 +85,9 @@ def test_silhouette():
         plan_hyperparams=plan_config,
     )
     asha_scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
-    model, analysis = tuner.run(metric="elbo_validation", scheduler=asha_scheduler)
+    model, analysis = tuner.run(
+        metric="silhouette_score",
+        mode="max",
+        scheduler=asha_scheduler,
+        local_dir=save_path,
+    )
