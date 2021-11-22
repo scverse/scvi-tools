503d9270bbabdc9243f2f91a3d41371c9be4377e
adamgayoso <adamgayoso@users.noreply.github.com>
2021-11-22 14:01:27 -0800

    fix muris thumbnail

--- docs/conf.py
+++ docs/conf.py
@@ -211,7 +211,7 @@ nbsphinx_thumbnails = {
     "tutorials/notebooks/totalVI_reference_mapping": "_static/tutorials/totalvi_cell.svg",
     "tutorials/notebooks/amortized_lda": "_static/tutorials/lda.png",
     "tutorials/notebooks/cell2location_lymph_node_spatial_tutorial": "_static/tutorials/cell2location.png",
-    "tutorials/notebooks/tabula_muris": "_static/tutorials/muris_mouse.png",
+    "tutorials/notebooks/tabula_muris": "_static/tutorials/muris-mouse.png",
 }
 
 
