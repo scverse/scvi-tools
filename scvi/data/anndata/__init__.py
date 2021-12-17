c984e31e5a4f5e497387aeb26afe0df7fbc16aa6
Justin Hong <jjhong922@berkeley.edu>
2021-12-16 17:17:41 -0800

    Add anndata registration classes to developer API (#1292)
    * add anndata registration classes to developer API
    
    * move manager to anndata submodule to fix docs

--- scvi/data/anndata/__init__.py
+++ scvi/data/anndata/__init__.py
@@ -1,7 +1,9 @@
 from ._utils import register_tensor_from_anndata, setup_anndata, transfer_anndata_setup
+from .manager import AnnDataManager
 
 __all__ = [
     "setup_anndata",
     "transfer_anndata_setup",
     "register_tensor_from_anndata",
+    "AnnDataManager",
 ]
