953cb17c719c9b5e3fbcdb5f0d61bee8ce4e48f0
Justin Hong <jjhong922@berkeley.edu>
2022-02-04 15:45:29 -0800

    comments in constants file

--- scvi/data/anndata/_constants.py
+++ scvi/data/anndata/_constants.py
@@ -1,15 +1,15 @@
 from typing import NamedTuple
 
-################################
-# scVI Manager Store Constants #
-################################
+# scVI Manager Store Constants
+# ----------------------------
+# Keys for UUIDs used for referencing model class manager stores.
 
 _SCVI_UUID_KEY = "_scvi_uuid"
 _SOURCE_SCVI_UUID_KEY = "_source_scvi_uuid"
 
-#############################
-# scVI Registry Constants #
-#############################
+# scVI Registry Constants
+# -----------------------
+# Keys used in the scVI registry.
 
 _SCVI_VERSION_KEY = "scvi_version"
 _MODEL_NAME_KEY = "model_name"
@@ -19,16 +19,16 @@ _DATA_REGISTRY_KEY = "data_registry"
 _STATE_REGISTRY_KEY = "state_registry"
 _SUMMARY_STATS_KEY = "summary_stats"
 
-################################
-# scVI Data Registry Constants #
-################################
+# scVI Data Registry Constants
+# ----------------------------
+# Keys used in the data registry.
 
 _DR_ATTR_NAME = "attr_name"
 _DR_ATTR_KEY = "attr_key"
 
-############################
-# AnnData Object Constants #
-############################
+# AnnData Object Constants
+# ------------------------
+# AnnData object attribute names.
 
 
 class _ADATA_ATTRS_NT(NamedTuple):
