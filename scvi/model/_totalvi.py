1227323524c9567c5ad3819f73e15b5491048281
Pierre Boyeau <pierre.boyeau@gmail.com>
2022-02-15 15:43:23 -0800

    clean

--- scvi/model/_totalvi.py
+++ scvi/model/_totalvi.py
@@ -339,7 +339,7 @@ class TOTALVI(RNASeqMixin, VAEMixin, ArchesMixin, BaseModelClass):
         for tensors in post:
             inference_inputs = self.module._get_inference_input(tensors)
             outputs = self.module.inference(**inference_inputs)
-            if give_mean is True:
+            if give_mean:
                 ql = outputs["ql"]
                 library = torch.exp(ql.loc + 0.5 * (ql.scale**2))
             else:
