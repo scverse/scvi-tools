7b248fe1185d7d36f841c8af7f4c6976f6823fd5
Adam Gayoso <adamgayoso@users.noreply.github.com>
2022-03-02 15:53:08 -0800

    Backport PR #1386: scvi-tools NB parameterization for numpyro

--- scvi/distributions/__init__.py
+++ scvi/distributions/__init__.py
@@ -1,4 +1,5 @@
 from ._negative_binomial import (
+    JaxNegativeBinomialMeanDisp,
     NegativeBinomial,
     NegativeBinomialMixture,
     ZeroInflatedNegativeBinomial,
@@ -8,4 +9,5 @@ __all__ = [
     "NegativeBinomial",
     "NegativeBinomialMixture",
     "ZeroInflatedNegativeBinomial",
+    "JaxNegativeBinomialMeanDisp",
 ]
