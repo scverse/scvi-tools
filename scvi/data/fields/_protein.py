67711bbe9631c13456bbb73868389356beda9ed9
Justin Hong <jjhong922@berkeley.edu>
2022-03-15 15:19:03 -0400

    refactor protein

--- scvi/data/fields/_protein.py
+++ scvi/data/fields/_protein.py
@@ -73,29 +73,30 @@ class ProteinObsmField(ObsmField):
         The mask is over cell measurement columns that are present (observed)
         in each batch. Absence is defined by all 0 for that protein in that batch.
         """
-        pro_exp = self.get_field_data(adata)
-        pro_exp = pro_exp.to_numpy() if isinstance(pro_exp, pd.DataFrame) else pro_exp
-        batches = adata.obs[self.batch_key].values
-        batch_mask = {}
-        for b in np.unique(batches):
-            b_inds = np.where(batches.ravel() == b)[0]
-            batch_sum = pro_exp[b_inds, :].sum(axis=0)
-            all_zero = batch_sum == 0
-            batch_mask[b] = ~all_zero
+        if self.use_batch_mask:
+            pro_exp = self.get_field_data(adata)
+            pro_exp = (
+                pro_exp.to_numpy() if isinstance(pro_exp, pd.DataFrame) else pro_exp
+            )
+            batches = adata.obs[self.batch_key].values
+            batch_mask = {}
+            for b in np.unique(batches):
+                b_inds = np.where(batches.ravel() == b)[0]
+                batch_sum = pro_exp[b_inds, :].sum(axis=0)
+                all_zero = batch_sum == 0
+                batch_mask[b] = ~all_zero
 
-        if np.sum([~b[1] for b in batch_mask.items()]) > 0:
-            logger.info("Found batches with missing protein expression")
-            return batch_mask
-
+            if np.sum([~b[1] for b in batch_mask.items()]) > 0:
+                logger.info("Found batches with missing protein expression")
+                return batch_mask
         return None
 
     def register_field(self, adata: AnnData) -> dict:
         state_registry = super().register_field(adata)
 
-        if self.use_batch_mask:
-            batch_mask = self._get_batch_mask_protein_data(adata)
-            if batch_mask is not None:
-                state_registry[self.PROTEIN_BATCH_MASK] = batch_mask
+        batch_mask = self._get_batch_mask_protein_data(adata)
+        if batch_mask is not None:
+            state_registry[self.PROTEIN_BATCH_MASK] = batch_mask
 
         return state_registry
 
