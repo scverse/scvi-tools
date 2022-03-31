d55dbacc631b1d7b11dc3275cfadf239d83b194c
Adam Gayoso <adamgayoso@users.noreply.github.com>
2022-03-31 14:18:41 -0700

    Lightning 1.6 official (#1477)
    * prepare for next pylit
    
    * Update pyproject.toml

--- scvi/train/_progress.py
+++ scvi/train/_progress.py
@@ -70,8 +70,8 @@ class ProgressBar(ProgressBarBase):
         super().on_train_start(trainer, pl_module)
         self.main_progress_bar = self.init_train_tqdm(trainer)
 
-    def on_epoch_start(self, trainer, pl_module):
-        super().on_epoch_start(trainer, pl_module)
+    def on_train_epoch_start(self, trainer, pl_module):
+        super().on_train_epoch_start(trainer, pl_module)
         if self._should_update(self.trainer.current_epoch, self.trainer.max_epochs):
             epoch = trainer.current_epoch + 1
             self.main_progress_bar.set_description(
