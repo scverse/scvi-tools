49fb3655ea69fbc0ee634e1024922f189b0c201a
Justin Hong <jjhong922@berkeley.edu>
2022-07-11 16:55:49 -0700

    set full state update to false

--- scvi/train/_metrics.py
+++ scvi/train/_metrics.py
@@ -24,6 +24,7 @@ class ElboMetric(Metric):
         Keyword args for :class:`torchmetrics.Metric`
     """
 
+    full_state_update = True
     _N_OBS_MINIBATCH_KEY = "n_obs_minibatch"
 
     def __init__(
