91be3393c81f0075c7219b5d826432f5ab2f3b6c
Justin Hong <jjhong922@berkeley.edu>
2022-07-12 10:02:05 -0700

    Update _metrics.py

--- scvi/train/_metrics.py
+++ scvi/train/_metrics.py
@@ -24,7 +24,7 @@ class ElboMetric(Metric):
         Keyword args for :class:`torchmetrics.Metric`
     """
 
-    full_state_update = True
+    full_state_update = True # Needs to be explicitly set to avoid TorchMetrics UserWarning.
     _N_OBS_MINIBATCH_KEY = "n_obs_minibatch"
 
     def __init__(
