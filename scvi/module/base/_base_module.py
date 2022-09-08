a7457fd666829a03fd79c342817685c276658c2d
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2022-09-08 10:51:07 -0700

    more codacy appeasing

--- scvi/module/base/_base_module.py
+++ scvi/module/base/_base_module.py
@@ -205,7 +205,7 @@ class BaseModuleClass(nn.Module):
 
     @abstractmethod
     def inference_no_encode(self, qz_m, qz_v, latent_data_type, n_samples=1):
-        """Runs the inference routine but skips encoding, used in latent mode"""
+        """Runs the inference routine but skips encoding, used in latent mode."""
         pass
 
     @abstractmethod
