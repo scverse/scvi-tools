eb95542659789c91bd207720a91ca2dc12f567ba
Martin Kim <martinkim@berkeley.edu>
2022-09-28 11:29:04 -0700

    Remove custom attrdict class and tests

--- scvi/utils/__init__.py
+++ scvi/utils/__init__.py
@@ -1,6 +1,5 @@
-from ._attrdict import attrdict
 from ._docstrings import setup_anndata_dsp
 from ._jax import device_selecting_PRNGKey
 from ._track import track
 
-__all__ = ["track", "setup_anndata_dsp", "attrdict", "device_selecting_PRNGKey"]
+__all__ = ["track", "setup_anndata_dsp", "device_selecting_PRNGKey"]
