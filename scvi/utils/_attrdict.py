ff532fc43811afcc891af366425fe3cc288c457d
Martin Kim <martinkim@berkeley.edu>
2022-10-06 11:49:58 -0700

    Add ml_collections to conf.py, update attrdict doc

--- scvi/utils/_attrdict.py
+++ scvi/utils/_attrdict.py
@@ -3,8 +3,9 @@ from ml_collections.config_dict import FrozenConfigDict
 
 class attrdict(FrozenConfigDict):
     """
-    Dummy class to allow for backwards compatibility with old attrdict
-    Now uses ml-collections FrozenConfigDict.
+    A dictionary that allows for attribute-style access. Dummy class that allows
+    for backwards compatibility with the previous custom ``attrdict``. Inherits
+    from :class:`~ml_collections.config_dict.FrozenConfigDict`.
     """
 
     def __init__(self, *args, **kwargs):
