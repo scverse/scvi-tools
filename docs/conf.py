e0d8479d57e029eae88b854eb5bc90a2d50a88a9
adamgayoso <adamgayoso@users.noreply.github.com>
2022-10-09 21:17:52 -0700

    remove sphinx gallery

--- docs/conf.py
+++ docs/conf.py
@@ -150,7 +150,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["css/override.css", "css/sphinx_gallery.css"]
+html_css_files = ["css/override.css"]
 html_show_sphinx = False
 
 # -- Nbsphinx prolog -------------------------------------------
