3f94aa3eead3aec1d451a06d488854b01e1c9112
adamgayoso <adamgayoso@users.noreply.github.com>
2022-10-17 15:08:08 -0700

    Revert "more efficient metrics"
    This reverts commit 5905efb93892cec83138070658c74ed16646b4fe.

--- scvi/train/_metrics.py
+++ scvi/train/_metrics.py
@@ -25,7 +25,7 @@ class ElboMetric(Metric):
     """
 
     # Needs to be explicitly set to avoid TorchMetrics UserWarning.
-    full_state_update = False
+    full_state_update = True
     _N_OBS_MINIBATCH_KEY = "n_obs_minibatch"
 
     def __init__(
@@ -33,9 +33,10 @@ class ElboMetric(Metric):
         name: str,
         mode: Literal["train", "validation"],
         interval: Literal["obs", "batch"],
+        dist_sync_on_step: bool = False,
         **kwargs,
     ):
-        super().__init__(**kwargs)
+        super().__init__(dist_sync_on_step=dist_sync_on_step, **kwargs)
 
         self._name = name
         self._mode = mode
