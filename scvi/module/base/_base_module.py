9d5f20a8ae0775cb99b9fbc1f162e055fc2d5dd2
Justin Hong <jjhong922@berkeley.edu>
2022-10-26 14:37:15 -0400

    change default rngs (#1769)
    Co-authored-by: Adam Gayoso <adamgayoso@users.noreply.github.com>

--- scvi/module/base/_base_module.py
+++ scvi/module/base/_base_module.py
@@ -564,7 +564,7 @@ class JaxBaseModuleClass(flax.linen.Module):
     @abstractmethod
     def required_rngs(self):
         """Returns a tuple of rng sequence names required for this Flax module."""
-        return tuple()
+        return ("params",)
 
     def __call__(
         self,
