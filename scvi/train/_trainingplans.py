04a31e0c642a05f54f136d4c89a5e51cec77090f
Martin Kim <46072231+martinkim0@users.noreply.github.com>
2022-11-16 17:20:07 -0800

    Backport PR #1791: Fix `JaxTrainingPlan` to update `global_step`

--- scvi/train/_trainingplans.py
+++ scvi/train/_trainingplans.py
@@ -1143,6 +1143,9 @@ class JaxTrainingPlan(TrainingPlan):
             prog_bar=True,
         )
         self.compute_and_log_metrics(loss_output, self.train_metrics, "train")
+        # Update the dummy optimizer to update the global step
+        _opt = self.optimizers()
+        _opt.step()
 
     @partial(jax.jit, static_argnums=(0,))
     def jit_validation_step(
