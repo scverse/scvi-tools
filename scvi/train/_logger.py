a4db8521f8f3d4ef74ab5f0d4a4da7e37e6b51a8
Martin Kim <martinkim@berkeley.edu>
2023-01-04 13:02:13 -0800

    Address deprecation warnings

--- scvi/train/_logger.py
+++ scvi/train/_logger.py
@@ -2,8 +2,7 @@ from typing import Any, Dict, Optional, Union
 
 import pandas as pd
 import torch
-from pytorch_lightning.loggers.base import rank_zero_experiment
-from pytorch_lightning.loggers.logger import Logger
+from pytorch_lightning.loggers.logger import Logger, rank_zero_experiment
 from pytorch_lightning.utilities import rank_zero_only
 
 
