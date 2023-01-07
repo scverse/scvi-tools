f878376e7962ab4212085b1fe5dbb05c63dc84f8
adamgayoso <adamgayoso@users.noreply.github.com>
2023-01-07 11:24:29 -0800

    reconst loss

--- scvi/external/scbasset/_module.py
+++ scvi/external/scbasset/_module.py
@@ -317,6 +317,10 @@ class ScBassetModule(BaseModuleClass):
         """Loss function for the model."""
         reconstruction = generative_outputs["reconstruction"]
         target = tensors[REGISTRY_KEYS.X_KEY]
-        loss_fn = nn.BCELoss()
-        loss = loss_fn(reconstruction, target)
-        return LossOutput(loss=loss, n_obs_minibatch=target.shape[0])
+        loss_fn = nn.BCELoss(reduction="none")
+        full_loss = loss_fn(reconstruction, target)
+        reconstruction_loss = full_loss.sum(dim=-1)
+        loss = reconstruction.sum() / (
+            reconstruction.shape[0] * reconstruction.shape[1]
+        )
+        return LossOutput(loss=loss, reconstruction_loss=reconstruction_loss)
