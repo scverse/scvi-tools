45e44460f62faeb9f36d98a5d0c9d7582943acb7
adamgayoso <adamgayoso@users.noreply.github.com>
2023-01-09 14:24:06 -0800

    add test

--- tests/dataset/test_preprocessing.py
+++ tests/dataset/test_preprocessing.py
@@ -1,12 +1,13 @@
 import numpy as np
+import pytest
 from scipy import sparse
 
-from scvi.data import poisson_gene_selection, synthetic_iid
+from scvi.data import add_dna_sequence, poisson_gene_selection, synthetic_iid
 
 
 def test_poisson_gene_selection():
-    adata = synthetic_iid()
     n_top_genes = 10
+    adata = synthetic_iid()
     poisson_gene_selection(adata, batch_key="batch", n_top_genes=n_top_genes)
     keys = [
         "highly_variable",
@@ -23,3 +24,17 @@ def test_poisson_gene_selection():
     adata.X = sparse.csr_matrix(adata.X)
     poisson_gene_selection(adata, batch_key="batch", n_top_genes=n_top_genes)
     assert np.sum(adata.var["highly_variable"]) == n_top_genes
+
+
+@pytest.mark.internet
+def test_add_dna_sequence(save_path):
+    adata = synthetic_iid()
+    adata = adata[:, :2].copy()
+    adata.var["chr"] = "chr1"
+    adata.var["start"] = [629395, 633578]
+    adata.var["start"] = [630394, 634591]
+
+    add_dna_sequence(adata, seq_len=100, genome="hg38", genome_dir=save_path)
+    assert "dna_sequence" in adata.varm.keys()
+    assert "dna_code" in adata.varm.keys()
+    assert adata.varm["dna_code"].values.shape[1] == 100
