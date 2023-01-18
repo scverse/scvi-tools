f19856bf9f45afaac15efeb623b70962719f1f52
Martin Kim <martinkim@berkeley.edu>
2023-01-18 15:07:58 -0800

    Fix docs

--- scvi/autotune/__init__.py
+++ scvi/autotune/__init__.py
@@ -1,5 +1,5 @@
-from ._manager import TunerManager
+from ._manager import TuneAnalysis, TunerManager
 from ._tuner import ModelTuner
 from ._types import Tunable, TunableMixin
 
-__all__ = ["ModelTuner", "Tunable", "TunableMixin", "TunerManager"]
+__all__ = ["ModelTuner", "Tunable", "TunableMixin", "TunerManager", "TuneAnalysis"]
