0897d6a7fd00e53482fef810c35d998ea481c35e
Martin Kim <martinkim@berkeley.edu>
2023-01-19 16:04:25 -0800

    Use save_path for autotune test

--- tests/autotune/test_tuner.py
+++ tests/autotune/test_tuner.py
@@ -1,6 +1,3 @@
-import os
-import shutil
-
 import scvi
 
 
@@ -9,22 +6,20 @@ def test_model_tuner_init():
     scvi.autotune.ModelTuner(model_cls)
 
 
-def test_model_tuner_fit():
+def test_model_tuner_fit(save_path):
     model_cls = scvi.model.SCVI
     tuner = scvi.autotune.ModelTuner(model_cls)
 
     adata = scvi.data.synthetic_iid()
     model_cls.setup_anndata(adata)
-    logging_dir = os.path.join(os.getcwd(), "tests", "data", "autotune_logs")
     results = tuner.fit(
         adata,
         use_defaults=True,
         num_samples=1,
         max_epochs=1,
-        logging_dir=logging_dir,
+        logging_dir=save_path,
     )
     assert results is not None
-    shutil.rmtree(logging_dir)
 
 
 def test_model_tuner_info():
