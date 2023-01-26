508ed0b12435370b7d12e2601a593687cdad1461
Martin Kim <46072231+martinkim0@users.noreply.github.com>
2023-01-25 16:43:10 -0800

    Remove `LossRecorder` (#1869)
    * Remove LossRecorder
    
    * Fix docstring
    
    * Update release notes

--- scvi/module/base/_base_module.py
+++ scvi/module/base/_base_module.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import warnings
 from abc import abstractmethod
 from dataclasses import field
 from typing import Any, Callable, Dict, Iterable, Optional, Tuple, Union
@@ -27,99 +26,6 @@ from scvi.utils._jax import device_selecting_PRNGKey
 
 from ._decorators import auto_move_data
 from ._pyro import AutoMoveDataPredictive
-
-
-class LossRecorder:
-    """
-    Loss signature for models.
-
-    This class provides an organized way to record the model loss, as well as
-    the components of the ELBO. This may also be used in MLE, MAP, EM methods.
-    The loss is used for backpropagation during inference. The other parameters
-    are used for logging/early stopping during inference.
-
-    Parameters
-    ----------
-    loss
-        Tensor with loss for minibatch. Should be one dimensional with one value.
-        Note that loss should be a :class:`~torch.Tensor` and not the result of ``.item()``.
-    reconstruction_loss
-        Reconstruction loss for each observation in the minibatch. If a tensor, converted to
-        a dictionary with key "reconstruction_loss" and value as tensor
-    kl_local
-        KL divergence associated with each observation in the minibatch. If a tensor, converted to
-        a dictionary with key "kl_local" and value as tensor
-    kl_global
-        Global kl divergence term. Should be one dimensional with one value. If a tensor, converted to
-        a dictionary with key "kl_global" and value as tensor
-    **kwargs
-        Additional metrics can be passed as keyword arguments and will
-        be available as attributes of the object.
-    """
-
-    def __init__(
-        self,
-        loss: LossRecord,
-        reconstruction_loss: Optional[LossRecord] = None,
-        kl_local: Optional[LossRecord] = None,
-        kl_global: Optional[LossRecord] = None,
-        **kwargs,
-    ):
-        warnings.warn(
-            "LossRecorder is deprecated and will be removed in version 0.20.0. Please use LossOutput",
-            category=DeprecationWarning,
-        )
-        self._loss_output = LossOutput(
-            loss=loss,
-            reconstruction_loss=reconstruction_loss,
-            kl_local=kl_local,
-            kl_global=kl_global,
-            extra_metrics=kwargs,
-        )
-        self.extra_metric_attrs = []
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-            self.extra_metric_attrs.append(key)
-
-    @property
-    def loss(self) -> Union[torch.Tensor, jnp.ndarray]:  # noqa: D102
-        return self._loss_output.loss
-
-    @property
-    def reconstruction_loss(self) -> Union[torch.Tensor, jnp.ndarray]:  # noqa: D102
-        return self.dict_sum(self._loss_output.reconstruction_loss)
-
-    @property
-    def _reconstruction_loss(self):
-        return self._loss_output.reconstruction_loss
-
-    @property
-    def kl_local(self) -> Union[torch.Tensor, jnp.ndarray]:  # noqa: D102
-        return self.dict_sum(self._loss_output.kl_local)
-
-    @property
-    def _kl_local(self):
-        return self._loss_output.kl_local
-
-    @property
-    def reconstruction_loss_sum(self) -> Union[torch.Tensor, jnp.ndarray]:  # noqa: D102
-        return self._loss_output.reconstruction_loss_sum
-
-    @property
-    def kl_local_sum(self) -> Union[torch.Tensor, jnp.ndarray]:  # noqa: D102
-        return self._loss_output.kl_local_sum
-
-    @property
-    def kl_global_sum(self) -> Union[torch.Tensor, jnp.ndarray]:  # noqa: D102
-        return self._loss_output.kl_global_sum
-
-    @property
-    def kl_global(self) -> Union[torch.Tensor, jnp.ndarray]:  # noqa: D102
-        return self.dict_sum(self._loss_output.kl_global)
-
-    def dict_sum(self, x):
-        """Wrapper of LossOutput.dict_sum."""
-        return self._loss_output.dict_sum(x)
 
 
 @chex.dataclass
@@ -251,7 +157,7 @@ class BaseModuleClass(TunableMixin, nn.Module):
         compute_loss=True,
     ) -> Union[
         Tuple[torch.Tensor, torch.Tensor],
-        Tuple[torch.Tensor, torch.Tensor, LossRecorder],
+        Tuple[torch.Tensor, torch.Tensor, LossOutput],
     ]:
         """
         Forward pass through the network.
@@ -585,7 +491,7 @@ class JaxBaseModuleClass(TunableMixin, flax.linen.Module):
         compute_loss=True,
     ) -> Union[
         Tuple[jnp.ndarray, jnp.ndarray],
-        Tuple[jnp.ndarray, jnp.ndarray, LossRecorder],
+        Tuple[jnp.ndarray, jnp.ndarray, LossOutput],
     ]:
         """
         Forward pass through the network.
