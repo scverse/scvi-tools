05f2973b6cc19e36ddcc7729e5d8c941927a5602
Martin Kim <martinkim@berkeley.edu>
2023-03-29 14:43:18 -0700

    Refactor split validation

--- scvi/dataloaders/_data_splitting.py
+++ scvi/dataloaders/_data_splitting.py
@@ -1,6 +1,6 @@
 import logging
 from math import ceil, floor
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import lightning.pytorch as pl
 import numpy as np
@@ -23,6 +23,83 @@ from scvi.utils._docstrings import devices_dsp
 from scvi.utils._exceptions import InvalidParameterError
 
 logger = logging.getLogger(__name__)
+
+
+def _validate_data_split_sizes(
+    n_obs: int,
+    train_size: float,
+    validation_size: Optional[float],
+) -> Tuple[int, int, int]:
+    if train_size > 1.0 or train_size <= 0.0:
+        raise InvalidParameterError(
+            "train_size",
+            train_size,
+            additional_message="`train_size` must be between 0 and 1.",
+        )
+
+    n_train = ceil(train_size * n_obs)
+
+    if validation_size is None:
+        n_val = n_obs - n_train
+    elif validation_size >= 1.0 or validation_size < 0.0:
+        raise InvalidParameterError(
+            "validation_size",
+            validation_size,
+            additional_message="`validation_size` must be between 0 and 1.",
+        )
+    elif (train_size + validation_size) > 1:
+        raise InvalidParameterError(
+            "train_size + validation_size",
+            train_size + validation_size,
+            additional_message="`train_size + validation_size` must be between 0 and 1.",
+        )
+    else:
+        n_val = floor(n_obs * validation_size)
+
+    n_test = n_obs - n_train - n_val
+
+    logging.info(
+        f"Using {n_train} observations for training, {n_val} for validation "
+        f"and {n_test} for testing."
+    )
+    return n_train, n_val, n_test
+
+
+def _validate_data_split_indices(
+    n_obs: int,
+    train_indices: List[int],
+    validation_indices: Optional[List[int]],
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    train_indices = np.array(train_indices)
+    if validation_indices is not None:
+        validation_indices = np.array(validation_indices)
+
+    if np.amax(train_indices) >= n_obs or np.amin(train_indices) < 0:
+        raise InvalidParameterError(
+            "train_indices",
+            train_indices,
+            additional_message="`train_indices` contains invalid indices.",
+        )
+
+    if validation_indices is None:
+        validation_indices = np.setdiff1d(np.arange(n_obs), train_indices)
+    elif np.amax(validation_indices) >= n_obs or np.amin(validation_indices) < 0:
+        raise InvalidParameterError(
+            "validation_indices",
+            validation_indices,
+            additional_message="`validation_indices` contains invalid indices.",
+        )
+
+    union_indices = np.union1d(train_indices, validation_indices)
+    test_indices = np.setdiff1d(np.arange(n_obs), union_indices)
+
+    logging.info(
+        f"Using {len(train_indices)} observations for training, "
+        f"{len(validation_indices)} for validation and {len(test_indices)} for "
+        "testing."
+    )
+
+    return train_indices, validation_indices, test_indices
 
 
 def validate_data_split(
@@ -72,71 +149,9 @@ def validate_data_split(
         )
 
     if train_size is not None:
-        if train_size > 1.0 or train_size <= 0.0:
-            raise InvalidParameterError(
-                "train_size",
-                train_size,
-                additional_message="`train_size` must be between 0 and 1.",
-            )
-
-        n_train = ceil(train_size * n_obs)
-
-        if validation_size is None:
-            n_val = n_obs - n_train
-        elif validation_size >= 1.0 or validation_size < 0.0:
-            raise InvalidParameterError(
-                "validation_size",
-                validation_size,
-                additional_message="`validation_size` must be between 0 and 1.",
-            )
-        elif (train_size + validation_size) > 1:
-            raise InvalidParameterError(
-                "train_size + validation_size",
-                train_size + validation_size,
-                additional_message="`train_size + validation_size` must be between 0 and 1.",
-            )
-        else:
-            n_val = floor(n_obs * validation_size)
-
-        n_test = n_obs - n_train - n_val
-
-        logging.info(
-            f"Using {n_train} observations for training, {n_val} for validation "
-            f"and {n_test} for testing."
-        )
-        return n_train, n_val, n_test
-
-    if train_indices is not None:
-        train_indices = np.array(train_indices)
-        if validation_indices is not None:
-            validation_indices = np.array(validation_indices)
-
-        if np.amax(train_indices) >= n_obs or np.amin(train_indices) < 0:
-            raise InvalidParameterError(
-                "train_indices",
-                train_indices,
-                additional_message="`train_indices` contains invalid indices.",
-            )
-
-        if validation_indices is None:
-            validation_indices = np.setdiff1d(np.arange(n_obs), train_indices)
-        elif np.amax(validation_indices) >= n_obs or np.amin(validation_indices) < 0:
-            raise InvalidParameterError(
-                "validation_indices",
-                validation_indices,
-                additional_message="`validation_indices` contains invalid indices.",
-            )
-
-        union_indices = np.union1d(train_indices, validation_indices)
-        test_indices = np.setdiff1d(np.arange(n_obs), union_indices)
-
-        logging.info(
-            f"Using {len(train_indices)} observations for training, "
-            f"{len(validation_indices)} for validation and {len(test_indices)} for "
-            "testing."
-        )
-
-        return train_indices, validation_indices, test_indices
+        return _validate_data_split_sizes(n_obs, train_size, validation_size)
+    else:
+        return _validate_data_split_indices(n_obs, train_indices, validation_indices)
 
 
 class DataSplitter(pl.LightningDataModule):
