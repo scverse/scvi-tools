7761ed06b4f04196101f3bc346de6b0cab897c7d
Martin Kim <martinkim@berkeley.edu>
2023-07-06 12:21:23 -0700

    Add basic integration test

---
+++ tests/train/test_distributed.py
@@ -0,0 +1,14 @@
+import scvi
+
+
+def test_scvi_train_ddp():
+    adata = scvi.data.synthetic_iid()
+    scvi.model.SCVI.setup_anndata(adata)
+    model = scvi.model.SCVI(adata)
+
+    model.train(
+        max_epochs=1,
+        accelerator="cpu",
+        devices=2,
+        strategy="ddp_find_unused_parameters_true",
+    )
