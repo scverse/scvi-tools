132955b1e7cabf398c96f4f25e121beb0ec07a83
Martin Kim <46072231+martinkim0@users.noreply.github.com>
2023-08-02 13:28:48 -0700

    Backport PR #2222: Migrate flax frozen dicts to dict

--- scvi/train/_callbacks.py
+++ scvi/train/_callbacks.py
@@ -2,6 +2,7 @@ import warnings
 from copy import deepcopy
 from typing import Optional, Tuple
 
+import flax
 import lightning.pytorch as pl
 import numpy as np
 import torch
@@ -164,5 +165,5 @@ class JaxModuleInit(Callback):
         else:
             dl = self.dataloader
         module_init = module.init(module.rngs, next(iter(dl)))
-        state, params = module_init.pop("params")
+        state, params = flax.core.pop(module_init, "params")
         pl_module.set_train_state(params, state)
