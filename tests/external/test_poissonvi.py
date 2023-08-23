a512ab0b4e0a743b4b80d9be0fadb3ef4e4e2a35
Laura <laura.d.martens@icloud.com>
2023-08-23 17:35:44 +0200

    Add test for model

---
+++ tests/external/test_poissonvi.py
@@ -0,0 +1,22 @@
+from typing import Optional
+import numpy as np
+
+from scvi.data import synthetic_iid
+from scvi.external import POISSONVI
+
+
+def _get_adata(sparse_format: Optional[str] = None):
+    dataset1 = synthetic_iid(batch_size=100, sparse_format=sparse_format)
+    return dataset1
+
+
+def test_poissonvi():
+    adata = _get_adata()
+    print(adata.obs)
+    POISSONVI.setup_anndata(
+        adata,
+    )
+    model = POISSONVI(adata)
+    model.train(max_epochs=1)
+    model.get_latent_representation()
+    model.get_accessibility_estimates()
