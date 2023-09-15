20dbfd63002812f20fc797e77abbf157ed6926f6
Chris Lin <chris522229197@gmail.com>
2023-09-14 17:02:10 -0700

    Add distributed_sampler and load_sparse_tensor as args to comply with AnnDataLoader format

--- scvi/dataloaders/_contrastive_dataloader.py
+++ scvi/dataloaders/_contrastive_dataloader.py
@@ -1,6 +1,8 @@
+import warnings
 from itertools import cycle
 from typing import List, Optional, Union
 
+from scvi import settings
 from scvi.data import AnnDataManager
 
 from ._concat_dataloader import ConcatDataLoader
@@ -55,6 +57,14 @@ class ContrastiveDataLoader(ConcatDataLoader):
             If ``int``, drops the last batch if its length is less than
             ``drop_last``. If ``drop_last == True``, drops last non-full batch.
             If ``drop_last == False``, iterate over all batches.
+        distributed_sampler
+            Whether to use :class:`~scvi.dataloaders.BatchDistributedSampler` as the
+            sampler. If `True`, `sampler` must be `None`. Not applicable here.
+        load_sparse_tensor
+            `EXPERIMENTAL` If ``True``, loads data with sparse CSR or CSC layout as a
+            :class:`~torch.Tensor` with the same layout. Can lead to speedups in data
+            transfers to GPUs, depending on the sparsity of the data. Not applicable
+            here.
         **data_loader_kwargs: Keyword arguments for `torch.utils.data.DataLoader`.
     """
 
@@ -67,6 +77,8 @@ class ContrastiveDataLoader(ConcatDataLoader):
         batch_size: int = 128,
         data_and_attributes: Optional[dict] = None,
         drop_last: Union[bool, int] = False,
+        distributed_sampler: bool = False,
+        load_sparse_tensor: bool = False,
         **data_loader_kwargs,
     ) -> None:
         super().__init__(
@@ -80,6 +92,28 @@ class ContrastiveDataLoader(ConcatDataLoader):
         )
         self.background_indices = background_indices
         self.target_indices = target_indices
+        if distributed_sampler:
+            warnings.warn(
+                (
+                    "distributed_sampler=True is not implemented for "
+                    "ContrastiveDataLoader. Setting distributed_sampler=False"
+                ),
+                UserWarning,
+                stacklevel=settings.warnings_stacklevel,
+            )
+            distributed_sampler = False
+        if load_sparse_tensor:
+            warnings.warn(
+                (
+                    "load_sparse_tensor=True is not implemented for "
+                    "ContrastiveDataLoader. Setting load_sparse_tensor=False"
+                ),
+                UserWarning,
+                stacklevel=settings.warnings_stacklevel,
+            )
+            load_sparse_tensor = False
+        self.distributed_sampler = distributed_sampler
+        self.load_sparse_tensor = load_sparse_tensor
 
     def __iter__(self):
         """Iter method for contrastive dataloader.
