cd39f24c5d19f38fe978b741948bc50e4be07e68
Martin Kim <martinkim126@gmail.com>
2023-10-10 14:19:34 -0700

    Try removing some conftest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,10 @@
 import shutil
-from distutils.dir_util import copy_tree
 
 import pytest
-
-import scvi
 
 
 def pytest_addoption(parser):
     """Docstring for pytest_addoption."""
-    parser.addoption(
-        "--model_fit",
-        action="store_true",
-        default=False,
-        dest="model_fit",
-        help="Option to run full training model for test_model_fit",
-    )
     parser.addoption(
         "--internet-tests",
         action="store_true",
@@ -66,25 +56,12 @@ def pytest_collection_modifyitems(config, items):
 
 
 @pytest.fixture(scope="session")
-def save_path(tmpdir_factory):
+def save_path(tmp_path_factory):
     """Docstring for save_path."""
-    dir = tmpdir_factory.mktemp("temp_data", numbered=False)
+    dir = tmp_path_factory.mktemp("temp_data", numbered=False)
     path = str(dir)
-    copy_tree("tests/data", path)
     yield path + "/"
-    shutil.rmtree(str(tmpdir_factory.getbasetemp()))
-
-
-@pytest.fixture(scope="session")
-def synthetic_adata():
-    """Docstring for model_fit."""
-    return scvi.data.synthetic_iid()
-
-
-@pytest.fixture(scope="session")
-def model_fit(request):
-    """Docstring for model_fit."""
-    return request.config.getoption("--model_fit")
+    shutil.rmtree(str(tmp_path_factory.getbasetemp()))
 
 
 @pytest.fixture(scope="session")
