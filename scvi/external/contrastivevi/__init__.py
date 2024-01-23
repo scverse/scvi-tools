04613513f3ccfb24b99feccf9d5b1ee8a2f324d0
Martin Kim <martinkim126@gmail.com>
2024-01-22 19:27:11 -0800

    Wwip

--- scvi/external/contrastivevi/__init__.py
+++ scvi/external/contrastivevi/__init__.py
@@ -1,5 +1,5 @@
-from ._contrastive_data_splitting import ContrastiveDataSplitter
-from ._contrastive_dataloader import ContrastiveDataLoader
+from ._data_splitting import ContrastiveDataSplitter
+from ._dataloader import ContrastiveDataLoader
 from ._model import ContrastiveVI
 from ._module import ContrastiveVAE
 
