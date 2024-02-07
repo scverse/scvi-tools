182669b1cc128069cac52ddc47b3a4e925557fef
Martin Kim <martinkim126@gmail.com>
2024-02-07 11:17:10 -0800

    Move xarray and sparse to optional deps

--- scvi/criticism/__init__.py
+++ scvi/criticism/__init__.py
@@ -1,3 +1,8 @@
-from ._ppc import PosteriorPredictiveCheck
+from scvi.utils import error_on_missing_dependencies
+
+error_on_missing_dependencies("xarray", "sparse")
+
+
+from ._ppc import PosteriorPredictiveCheck  # noqa
 
 __all__ = ["PosteriorPredictiveCheck"]
