3ad811a08f04fcc8205df879d902e84758a29792
Martin Kim <martinkim126@gmail.com>
2024-02-07 14:24:59 -0800

    Make xarray and sparse optional deps, lazy load criticism

--- scvi/criticism/__init__.py
+++ scvi/criticism/__init__.py
@@ -1,3 +1,8 @@
-from ._ppc import PosteriorPredictiveCheck
+from scvi.utils import error_on_missing_dependencies
+
+error_on_missing_dependencies("sparse", "xarray")
+
+
+from ._ppc import PosteriorPredictiveCheck  # noqa
 
 __all__ = ["PosteriorPredictiveCheck"]
