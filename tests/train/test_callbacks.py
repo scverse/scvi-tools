ac575795be2c6b1e718f69267f7c525ed8babefe
Martin Kim <martinkim126@gmail.com>
2024-04-02 11:34:50 -0700

    chore(train): remove metrics callback and additional_val_metrics

--- tests/train/test_callbacks.py
+++ tests/train/test_callbacks.py
@@ -82,36 +82,3 @@ def test_metricscallback_init():
 
     with pytest.raises(TypeError):
         MetricsCallback([0])
-
-
-def test_metricscallback_with_scvi():
-    adata = scvi.data.synthetic_iid()
-    scvi.model.SCVI.setup_anndata(adata)
-    model = scvi.model.SCVI(adata)
-
-    def dummy_metric(model: scvi.model.SCVI) -> float:
-        _ = model.get_latent_representation()
-        return 0.0
-
-    model.train(
-        max_epochs=1,
-        check_val_every_n_epoch=1,
-        additional_val_metrics={"dummy_metric": dummy_metric},
-    )
-    assert "dummy_metric" in model.history
-
-    model = scvi.model.SCVI(adata)
-    model.train(
-        max_epochs=1,
-        check_val_every_n_epoch=1,
-        additional_val_metrics=[dummy_metric],
-    )
-    assert "dummy_metric" in model.history
-
-    with pytest.warns(UserWarning):
-        # no validation step
-        model.train(
-            max_epochs=1,
-            check_val_every_n_epoch=None,
-            additional_val_metrics=[dummy_metric],
-        )
