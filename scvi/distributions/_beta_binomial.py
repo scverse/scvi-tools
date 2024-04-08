54650ada0e4d15c69a94a7c186cf36229d7fccb5
Ethan Weinberger <ethanweinberger@gmail.com>
2024-04-08 13:56:10 -0700

    Update scvi/distributions/_beta_binomial.py
    Co-authored-by: Martin Kim <46072231+martinkim0@users.noreply.github.com>

--- scvi/distributions/_beta_binomial.py
+++ scvi/distributions/_beta_binomial.py
@@ -15,7 +15,8 @@ class BetaBinomial(BetaBinomialDistribution):
 
     1. (`alpha`, `beta`, `total_counts`) where alpha and beta are the shape parameters of 
     the beta distribution and total_counts is the number of trials.
-    (2), (`mu`, `gamma`, `total_counts`), which is the one used by methylVI. These
+
+    2. (`mu`, `gamma`, `total_counts`), which is the one used by methylVI. These
     parameters respectively control the mean and dispersion of the distribution.
 
     In the (`mu`, `gamma`) parameterization, samples from the beta-binomial are generated
