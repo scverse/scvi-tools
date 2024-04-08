3f2b851bb76963f99f8e011e219f52a90dc0efaa
Ethan Weinberger <ethanweinberger@Ethans-MacBook-Pro.local>
2024-04-07 18:09:03 -0700

    Validate mdata var_names

--- scvi/model/base/_utils.py
+++ scvi/model/base/_utils.py
@@ -156,15 +156,27 @@ def _initialize_model(cls, adata, attr_dict):
 
 
 def _validate_var_names(adata, source_var_names):
-    user_var_names = adata.var_names.astype(str)
-    if not np.array_equal(source_var_names, user_var_names):
-        warnings.warn(
-            "var_names for adata passed in does not match var_names of adata used to "
-            "train the model. For valid results, the vars need to be the same and in "
-            "the same order as the adata used to train the model.",
-            UserWarning,
-            stacklevel=settings.warnings_stacklevel,
-        )
+    if isinstance(adata, mudata.MuData):
+        for modality in source_var_names.keys():
+            user_var_names = adata[modality].var_names.astype(str)
+            if not np.array_equal(source_var_names[modality], user_var_names):
+                warnings.warn(
+                    f"var_names for {modality} in mudata passed in does not match var_names"
+                    f"of mudata used to train the model. For valid results, the vars need to be"
+                    f"the same and in the same order as the mdata used to train the model.",
+                    UserWarning,
+                    stacklevel=settings.warnings_stacklevel,
+                )
+    else:
+        user_var_names = adata.var_names.astype(str)
+        if not np.array_equal(source_var_names, user_var_names):
+            warnings.warn(
+                "var_names for adata passed in does not match var_names of adata used to "
+                "train the model. For valid results, the vars need to be the same and in "
+                "the same order as the adata used to train the model.",
+                UserWarning,
+                stacklevel=settings.warnings_stacklevel,
+            )
 
 
 def _prepare_obs(
