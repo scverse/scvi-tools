91679616e8e45c403803ae5bd2f09890732926b6
Martin Kim <martinkim126@gmail.com>
2024-04-29 09:40:07 -0700

    fix import

--- src/scvi/external/mrvi/_module.py
+++ src/scvi/external/mrvi/_module.py
@@ -10,13 +10,13 @@ import numpyro.distributions as dist
 
 from scvi import REGISTRY_KEYS
 from scvi.distributions import JaxNegativeBinomialMeanDisp as NegativeBinomial
-from scvi.external.mrvi_components import (
+from scvi.external.mrvi._components import (
     AttentionBlock,
     ConditionalNormalization,
     Dense,
     NormalDistOutputNN,
 )
-from scvi.external.mrvi_constants import MRVI_REGISTRY_KEYS
+from scvi.external.mrvi._constants import MRVI_REGISTRY_KEYS
 from scvi.module.base import JaxBaseModuleClass, LossOutput, flax_configure
 
 DEFAULT_PX_KWARGS = {
