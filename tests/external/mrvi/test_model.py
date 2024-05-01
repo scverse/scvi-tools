87372db1895eb1a498e82e43474357aa013395a2
Justin Hong <justin.hong@columbia.edu>
2024-05-01 19:15:03 -0400

    Speed up MrVI tests (#2771)
    Reduces test time ~30% by caching trained model where possible
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Martin Kim <46072231+martinkim0@users.noreply.github.com>

--- tests/external/mrvi/test_model.py
+++ tests/external/mrvi/test_model.py
@@ -5,7 +5,7 @@ from scvi.data import synthetic_iid
 from scvi.external import MRVI
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def adata():
     adata = synthetic_iid()
     adata.obs.index.name = "cell_id"
@@ -25,10 +25,15 @@ def adata():
     return adata
 
 
-def test_mrvi(adata):
+@pytest.fixture(scope="session")
+def model(adata):
     MRVI.setup_anndata(adata, sample_key="sample_str", batch_key="batch")
     model = MRVI(adata)
-    model.train(1, check_val_every_n_epoch=1, train_size=0.5)
+    model.train(max_steps=2, train_size=0.5)
+    return model
+
+
+def test_mrvi(model):
     model.get_local_sample_distances()
     model.get_local_sample_distances(normalize_distances=True)
     model.get_latent_representation(give_z=False)
@@ -49,34 +54,14 @@ def test_mrvi(adata):
                 {
                     "sample_cov_keys": ["meta1_cat", "meta2", "cont_cov"],
                     "store_lfc": True,
-                    "lambd": 1e-1,
                     "add_batch_specific_offsets": True,
-                },
-                {
-                    "sample_cov_keys": ["meta1_cat", "meta2", "cont_cov"],
-                    "store_lfc": True,
                     "filter_inadmissible_samples": True,
-                    "add_batch_specific_offsets": True,
                 },
                 {
                     "sample_cov_keys": ["meta1_cat", "meta2", "cont_cov"],
                     "store_lfc": True,
                     "add_batch_specific_offsets": False,
                 },
-            ],
-        ),
-        (
-            {
-                "sample_key": "sample_str",
-                "batch_key": "batch",
-                "continuous_covariate_keys": ["cont_cov"],
-            },
-            [
-                {
-                    "sample_cov_keys": ["meta1_cat", "meta2", "cont_cov"],
-                    "store_lfc": True,
-                    "add_batch_specific_offsets": False,
-                }
             ],
         ),
         (
@@ -100,10 +85,7 @@ def test_mrvi(adata):
         ),
     ],
 )
-def test_mrvi_de(adata, setup_kwargs, de_kwargs):
-    MRVI.setup_anndata(adata, **setup_kwargs)
-    model = MRVI(adata)
-    model.train(1, check_val_every_n_epoch=1, train_size=0.5)
+def test_mrvi_de(model, setup_kwargs, de_kwargs):
     for de_kwarg in de_kwargs:
         model.differential_expression(**de_kwarg)
 
@@ -121,10 +103,7 @@ def test_mrvi_de(adata, setup_kwargs, de_kwargs):
         {"sample_cov_keys": ["meta1_cat", "batch"], "compute_log_enrichment": True},
     ],
 )
-def test_mrvi_da(adata, sample_key, da_kwargs):
-    MRVI.setup_anndata(adata, sample_key=sample_key, batch_key="batch")
-    model = MRVI(adata)
-    model.train(1, check_val_every_n_epoch=1, train_size=0.5)
+def test_mrvi_da(model, sample_key, da_kwargs):
     model.differential_abundance(**da_kwargs)
 
 
@@ -165,20 +144,10 @@ def test_mrvi_model_kwargs(adata, model_kwargs):
         continuous_covariate_keys=["cont_cov"],
     )
     model = MRVI(adata, n_latent=10, scale_observations=True, **model_kwargs)
-    model.train(1, check_val_every_n_epoch=1, train_size=0.5)
-    model.get_local_sample_distances()
-    model.get_local_sample_distances(normalize_distances=True)
+    model.train(max_steps=2, train_size=0.5)
 
 
-def test_mrvi_sample_subset(adata):
-    MRVI.setup_anndata(
-        adata,
-        sample_key="sample_str",
-        batch_key="batch",
-        continuous_covariate_keys=["cont_cov"],
-    )
-    model = MRVI(adata)
-    model.train(1, check_val_every_n_epoch=1, train_size=0.5)
+def test_mrvi_sample_subset(model):
     sample_cov_keys = ["meta1_cat", "meta2", "cont_cov"]
     sample_subset = [chr(i + ord("a")) for i in range(8)]
     model.differential_expression(sample_cov_keys=sample_cov_keys, sample_subset=sample_subset)
@@ -192,10 +161,8 @@ def test_mrvi_shrink_u(adata):
         continuous_covariate_keys=["cont_cov"],
     )
     model = MRVI(adata, n_latent=10, n_latent_u=5)
-    model.train(1, check_val_every_n_epoch=1, train_size=0.5)
+    model.train(max_steps=2, train_size=0.5)
     model.get_local_sample_distances()
-    model.is_trained_ = True
-    _ = model.history
 
     assert model.get_latent_representation().shape == (
         adata.shape[0],
@@ -225,9 +192,7 @@ def test_mrvi_stratifications(adata_stratifications):
         continuous_covariate_keys=["cont_cov"],
     )
     model = MRVI(adata_stratifications, n_latent=10)
-    model.train(1, check_val_every_n_epoch=1, train_size=0.5)
-    model.is_trained_ = True
-    _ = model.history
+    model.train(max_steps=2, train_size=0.5)
 
     dists = model.get_local_sample_distances(groupby=["labels", "label_2"])
     cell_dists = dists["cell"]
