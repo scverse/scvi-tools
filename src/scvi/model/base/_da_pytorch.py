13ee1a0cd10ad2af38f18c53ccaf506e199c8940
Alexander Becker <beckera@berkeley.edu>
2024-07-18 11:34:06 -0400

    modify to remove redundant get_latent_representations call

--- src/scvi/model/base/_da_pytorch.py
+++ src/scvi/model/base/_da_pytorch.py
@@ -14,36 +14,46 @@ from tqdm import tqdm
 def get_aggregated_posterior(
     self,
     adata: AnnData | None = None,
+    # below params let you pass in the latent reps already, if we already have computed them
+    # locs and scales must already be for the cells of the desired sample, as indices and sample
+    # are ignored if locs and scales are passed in
+    locs: np.ndarray | None = None,  # TODO add explanation of these vars
+    scales: np.ndarray | None = None,
     sample: str | int | None = None,
     indices: Sequence[int] | None = None,
     batch_size: int | None = None,
 ) -> Distribution:
     self._check_if_trained(warn=False)
-    adata = self._validate_anndata(adata)
 
-    if indices is None:
-        indices = np.arrange(self.adata.n_obs)
-    if sample is not None:
-        indices = np.intersect1d(
-            np.array(indices), np.where(adata.obs[self.sample_key] == sample)[0]
-        )
+    if locs and scales:
+        qu_loc = torch.from_numpy(locs)
+        qu_scale = torch.from_numpy(scales)
+    else:
+        adata = self._validate_anndata(adata)
 
-    dataloader = self._make_data_loader(adata=adata, indices=indices, batch_size=batch_size)
+        if indices is None:
+            indices = np.arrange(self.adata.n_obs)
+        if sample is not None:
+            indices = np.intersect1d(
+                np.array(indices), np.where(adata.obs[self.sample_key] == sample)[0]
+            )
 
-    qu_locs: list[Tensor] = []
-    qu_scales: list[Tensor] = []
-    for tensors in dataloader:
-        """ Not sure how to get the u estimated posteriors.
-        I don't think the following would work because the location of the
-        code would be in _vaemixin.py, and I think this would work if it
-        was still in mrvi/_model.py"""
+        dataloader = self._make_data_loader(adata=adata, indices=indices, batch_size=batch_size)
 
-        """Also I think this would return a jax distribution.
-        Do I need to create a new inference fn?"""
-        outputs = self.module.inference(self.module._get_inference_input(tensors))
+        qu_locs: list[Tensor] = []
+        qu_scales: list[Tensor] = []
+        for tensors in dataloader:
+            """ Not sure how to get the u estimated posteriors.
+            I don't think the following would work because the location of the
+            code would be in _vaemixin.py, and I think this would work if it
+            was still in mrvi/_model.py"""
 
-        qu_locs.append(outputs["qu"].loc)
-        qu_scales.append(outputs["qu"].scale)
+            """Also I think this would return a jax distribution.
+            Do I need to create a new inference fn?"""
+            outputs = self.module.inference(self.module._get_inference_input(tensors))
+
+            qu_locs.append(outputs["qu"].loc)
+            qu_scales.append(outputs["qu"].scale)
 
     # transpose because we need num cells to be rightmost dimension for mixture
     qu_loc = torch.cat(qu_locs, 0).T
@@ -69,10 +79,23 @@ def differential_abundance(
     representation, and get_latent_representation for mrvi uses jax."""
     us = self.get_latent_representation(adata, use_mean=True, give_z=False, batch_size=batch_size)
 
+    # return dist so that we can also get the vars, and don't have redundantly get the latent
+    # reps again in get_aggregated_posterior
+    us, variances = self.get_latent_representation(
+        adata, use_mean=True, give_z=False, batch_size=batch_size, return_dist=True
+    )
+
     log_probs = []
     unique_samples = adata.obs[self.sample_key].unique()
     for sample_name in tqdm(unique_samples):
-        ap = self.get_aggregated_posterior(adata=adata, sample=sample_name, batch_size=batch_size)
+        # ap = self.get_aggregated_posterior(
+        # adata=adata, sample=sample_name, batch_size=batch_size)
+
+        # below code to prevent getting latent reps twice.
+        indices = np.where(adata.obs[self.sample_key] == sample_name)[0]
+        locs_per_sample = us[indices]
+        scales_per_sample = variances[indices]
+        ap = self.get_aggregated_posterior(locs=locs_per_sample, scales=scales_per_sample)
 
         log_probs_ = []
         n_splits = max(adata.n_obs // batch_size, 1)
