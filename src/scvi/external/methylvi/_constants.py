6b9898ccf5de9bfb823995a17588b969d2ade664
Ethan Weinberger <ethanweinberger@Ethans-MacBook-Pro.local>
2024-08-11 17:12:08 -0700

    Remove placeholder modality key

--- src/scvi/external/methylvi/_constants.py
+++ src/scvi/external/methylvi/_constants.py
@@ -4,7 +4,6 @@ from typing import NamedTuple
 class _METHYLVI_REGISTRY_KEYS_NT(NamedTuple):
     MC_KEY: str = "mc"
     COV_KEY: str = "cov"
-    ANNDATA_MODALITY_PLACEHOLDER: str = "methylation"
 
 
 METHYLVI_REGISTRY_KEYS = _METHYLVI_REGISTRY_KEYS_NT()
