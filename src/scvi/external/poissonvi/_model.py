a2218a5e10020ed65d44ef747cfd9a2f52c32e3d
ori-kron-wis <ori.kronfeld@weizmann.ac.il>
2024-08-19 14:21:06 +0300

    Add CHANELOG remark

--- src/scvi/external/poissonvi/_model.py
+++ src/scvi/external/poissonvi/_model.py
@@ -232,7 +232,7 @@ class POISSONVI(PEAKVI, RNASeqMixin):
     @torch.inference_mode()
     def get_region_factors(self):
         """Return region-specific factors."""
-        region_factors = self.module.decoder.px_scale_decoder[-2].bias
+        region_factors = self.module.decoder.px_scale_decoder[-2].bias.numpy()
         if region_factors is None:
             raise RuntimeError("region factors were not included in this model")
         return region_factors
