a0cc0a3a9091f9afc9390fd87f600cea8d720aec
ori-kron-wis <ori.kronfeld@weizmann.ac.il>
2024-09-04 12:07:56 +0300

    added leave parameter to tqdm

--- src/scvi/utils/_track.py
+++ src/scvi/utils/_track.py
@@ -50,7 +50,7 @@ def track(
         if hasattr(tqdm_base, "_instances"):
             for instance in list(tqdm_base._instances):
                 tqdm_base._decr_instances(instance)
-        return tqdm_base(sequence, desc=description, file=sys.stdout, **kwargs)
+        return tqdm_base(sequence, leave=False, desc=description, file=sys.stdout, **kwargs)
     else:
         in_colab = "google.colab" in sys.modules
         force_jupyter = None if not in_colab else True
