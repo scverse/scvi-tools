59cf71c71bee79567d07028d605aaf2dc25a50f3
ori-kron-wis <ori.kronfeld@weizmann.ac.il>
2024-09-19 15:14:58 +0300

    Added a check on wehter repo exists before creating it, which failed previous test

--- tests/hub/test_hub_model.py
+++ tests/hub/test_hub_model.py
@@ -222,14 +222,15 @@ def test_hub_model_large_training_adata(request, save_path):
 
 @pytest.mark.private
 def test_hub_model_create_repo_hf(save_path: str):
-    from huggingface_hub import delete_repo
+    from huggingface_hub import delete_repo, repo_exists
 
-    hub_model = prep_scvi_hub_model(save_path)
-    hub_model.push_to_huggingface_hub(
-        "scvi-tools/test-scvi-create",
-        os.environ["HF_API_TOKEN"],
-        repo_create=True,
-    )
+    if not repo_exists("scvi-tools/test-scvi-create"):
+        hub_model = prep_scvi_hub_model(save_path)
+        hub_model.push_to_huggingface_hub(
+            "scvi-tools/test-scvi-create",
+            os.environ["HF_API_TOKEN"],
+            repo_create=True,
+        )
     delete_repo("scvi-tools/test-scvi-create", token=os.environ["HF_API_TOKEN"])
 
 
