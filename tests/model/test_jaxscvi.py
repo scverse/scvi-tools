2b9fe3e74c5b26c033583e13766192dc8451742b
Ori Kronfeld <ori.kronfeld@weizmann.ac.il>
2024-12-03 15:59:04 +0200

    added jax accelerator for tests

--- tests/model/test_jaxscvi.py
+++ tests/model/test_jaxscvi.py
@@ -2,6 +2,7 @@ from unittest import mock
 
 import numpy as np
 import pytest
+import torch
 from flax import linen as nn
 
 from scvi.data import synthetic_iid
@@ -10,17 +11,19 @@ from scvi.utils import attrdict
 
 
 def test_jax_scvi(n_latent=5):
+    accelerator = "gpu" if torch.cuda.is_available() else "cpu"
+
     adata = synthetic_iid()
     JaxSCVI.setup_anndata(
         adata,
         batch_key="batch",
     )
     model = JaxSCVI(adata, n_latent=n_latent)
-    model.train(2, train_size=0.5, check_val_every_n_epoch=1)
+    model.train(2, train_size=0.5, check_val_every_n_epoch=1, accelerator=accelerator)
     model.get_latent_representation()
 
     model = JaxSCVI(adata, n_latent=n_latent, gene_likelihood="poisson")
-    model.train(1, train_size=0.5)
+    model.train(1, train_size=0.5, accelerator=accelerator)
     z1 = model.get_latent_representation(give_mean=True, n_samples=1)
     assert z1.ndim == 2
     z2 = model.get_latent_representation(give_mean=False, n_samples=15)
@@ -29,6 +32,7 @@ def test_jax_scvi(n_latent=5):
 
 
 def test_jax_scvi_training(n_latent: int = 5, dropout_rate: float = 0.1):
+    accelerator = "gpu" if torch.cuda.is_available() else "cpu"
     adata = synthetic_iid()
     JaxSCVI.setup_anndata(
         adata,
@@ -42,7 +46,7 @@ def test_jax_scvi_training(n_latent: int = 5, dropout_rate: float = 0.1):
         mock_dropout = mock.Mock()
         mock_dropout.side_effect = lambda h, **_kwargs: h
         mock_dropout_cls.return_value = mock_dropout
-        model.train(1, train_size=0.5, check_val_every_n_epoch=1)
+        model.train(1, train_size=0.5, check_val_every_n_epoch=1, accelerator=accelerator)
 
         assert not model.module.training
         mock_dropout_cls.assert_called()
@@ -53,13 +57,14 @@ def test_jax_scvi_training(n_latent: int = 5, dropout_rate: float = 0.1):
 
 
 def test_jax_scvi_save_load(save_path: str, n_latent: int = 5):
+    accelerator = "gpu" if torch.cuda.is_available() else "cpu"
     adata = synthetic_iid()
     JaxSCVI.setup_anndata(
         adata,
         batch_key="batch",
     )
     model = JaxSCVI(adata, n_latent=n_latent)
-    model.train(2, train_size=0.5, check_val_every_n_epoch=1)
+    model.train(2, train_size=0.5, check_val_every_n_epoch=1, accelerator=accelerator)
     z1 = model.get_latent_representation(adata)
     model.save(save_path, overwrite=True, save_anndata=True)
     model.view_setup_args(save_path)
