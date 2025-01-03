8298ec555e2845167352eafd6ee10eca943abd6f
Ethan Weinberger <ethanweinberger@Ethans-MacBook-Pro.local>
2025-01-03 14:34:57 -0800

    Remove classifier logits check

--- src/scvi/external/methylvi/_methylanvi_module.py
+++ src/scvi/external/methylvi/_methylanvi_module.py
@@ -333,9 +333,7 @@ class METHYLANVAE(METHYLVAE, BSSeqModuleMixin):
                 kl_local=kl_locals,
             )
 
-        probs = self.classifier(z1)
-        if self.classifier.logits:
-            probs = F.softmax(probs, dim=-1)
+        probs = F.softmax(self.classifier(z1), dim=-1)
 
         reconst_loss += loss_z1_weight + (
             (loss_z1_unweight).view(self.n_labels, -1).t() * probs
