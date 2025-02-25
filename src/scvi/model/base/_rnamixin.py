c7739fec8c3c54ff444c8a17410c275de5072131
ori-kron-wis <ori.kronfeld@weizmann.ac.il>
2025-02-25 16:10:27 +0200

    DE should be mode="change" by default

--- src/scvi/model/base/_rnamixin.py
+++ src/scvi/model/base/_rnamixin.py
@@ -330,7 +330,7 @@ class RNASeqMixin:
         group2: str | None = None,
         idx1: list[int] | list[bool] | str | None = None,
         idx2: list[int] | list[bool] | str | None = None,
-        mode: Literal["vanilla", "change"] = "vanilla",
+        mode: Literal["vanilla", "change"] = "change",
         delta: float = 0.25,
         batch_size: int | None = None,
         all_stats: bool = True,
