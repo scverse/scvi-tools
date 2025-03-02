cbf2b3561c49670089bd6f884b499af2901df36e
Nathan <nathan.levy@weizmann.ac.il>
2025-03-03 01:17:45 +0200

    update dataclass/classifier names

--- src/scvi/external/nichevi/differential_expression/__init__.py
+++ src/scvi/external/nichevi/differential_expression/__init__.py
@@ -1,7 +1,7 @@
-from ._dataclass import DifferentialExpressionResults
 from ._de_core import _niche_de_core
 from ._de_utils import adjusted_nearest_neighbors
 from ._marker_classifier import _gaussian_process_classifier, plot_DE_results
+from ._results_dataclass import DifferentialExpressionResults
 
 __all__ = [
     "_niche_de_core",
