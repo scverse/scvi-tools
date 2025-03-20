9828782fad3fc7c42ab5e496b4627dcfb700d912
Alexander Becker <beckera@berkeley.edu>
2025-03-20 14:51:50 -0700

    add headers

--- docs/_ext/tutorial_filter.py
+++ docs/_ext/tutorial_filter.py
@@ -1,42 +1,56 @@
 from docutils import nodes
-from docutils.parsers.rst import Directive
 from docutils.statemachine import StringList
 
 from sphinx.application import Sphinx
-from sphinx.locale import _
 from sphinx.util.docutils import SphinxDirective
 from sphinx.util.typing import ExtensionMetadata
 
+
 class tutorialcardnode(nodes.General, nodes.TextElement):
+    """Node for a single tutorial card."""
+
     pass
+
 
 class tutoriallistnode(nodes.General, nodes.Element):
+    """Node for the list of tutorial cards."""
+
     pass
 
+
 def visit_tutorialcard_node(self, node):
+    """Visit a tutorial card node."""
     self.visit_paragraph(node)
 
+
 def depart_tutorialcard_node(self, node):
+    """Depart from a tutorial card node."""
     self.depart_paragraph(node)
 
+
 def purge_tutorial_cards(app, env, docname):
-    if not hasattr(env, 'tutorial_card_node_list'):
+    """Purge tutorial cards from the environment when a document is removed."""
+    if not hasattr(env, "tutorial_card_node_list"):
         return
 
     env.tutorial_card_node_list = [
-        node for node in env.tutorial_card_node_list if node['docname'] != docname]
-    
+        node for node in env.tutorial_card_node_list if node["docname"] != docname
+    ]
+
+
 def merge_tutorial_cards(app, env, docnames, other):
-    if not hasattr(env, 'tutorial_card_node_lists'):
+    """Merge tutorial cards from different environments."""
+    if not hasattr(env, "tutorial_card_node_lists"):
         env.tutorial_card_node_lists = []
-    if hasattr(other, 'tutorial_card_node_list'):
+    if hasattr(other, "tutorial_card_node_list"):
         env.tutorial_card_node_list.extend(other.tutorial_card_node_list)
 
+
 class TutorialListDirective(SphinxDirective):
-    # Insert header and footer markup for tutorial card list
-    # then put a placeholder node for the tutorial cards
+    """Directive to insert a list of tutorial cards."""
 
     def run(self):
+        """Run the directive to insert header and footer markup for tutorial card list."""
         header_list = StringList(TUTORIAL_LIST_START.split("\n"))
         header_node = nodes.paragraph()
         self.state.nested_parse(header_list, self.content_offset, header_node)
@@ -46,7 +60,6 @@ class TutorialListDirective(SphinxDirective):
         self.state.nested_parse(footer_list, self.content_offset, footer_node)
 
         return header_node.children + [tutoriallistnode("")] + footer_node.children
-    
 
 
 # TODO: HTML templates below borrowed from ____
@@ -96,19 +109,22 @@ TUTORIAL_LIST_END = """
    <br>
 """
 
+
 class TutorialCardDirective(SphinxDirective):
+    """Directive to create a single tutorial card."""
 
     option_spec = {
-        'link': str,
-        'description': str,
-        'tags': str,
+        "link": str,
+        "description": str,
+        "tags": str,
     }
 
     def run(self) -> list[nodes.Node]:
+        """Run the directive to create a tutorial card."""
         link = self.options["link"]
         description = self.options["description"]
 
-        if "tags" in self.options: # TODO: how should these be processed?
+        if "tags" in self.options:  # TODO: how should these be processed?
             tags = self.options["tags"]
 
         doctree = self.env.get_doctree(link)
@@ -131,17 +147,17 @@ class TutorialCardDirective(SphinxDirective):
             link=link,
             header=title,
             card_description=description,
-
+            tags=tags,
+            model_group=model_group,
         )
         tutorial_card_list = StringList(tutorial_card_rst.split("\n"))
-        node = tutorialcardnode(tutorial_card_list, self.content_offset, node)
+        node = tutorialcardnode()
+        self.state.nested_parse(tutorial_card_list, self.content_offset, node)
 
         if not hasattr(self.env, "tutorial_card_node_list"):
             self.env.tutorial_card_node_list = []
-        self.env.tutorial_card_node_list.append(node)
+        self.env.tutorial_card_node_list.append({"docname": self.env.docname, "node": node})
         return [node]
-
-
 
 
 # HTML template for each tutorial card
@@ -160,8 +176,9 @@ TUTORIAL_CARD_TEMPLATE = """
     </div>
 """
 
-def process_tutorial_card_nodes(app, doctree, fromdocname):
 
+def process_tutorial_card_nodes(app, doctree, fromdocname):
+    """Process tutorial card nodes in the document tree."""
     # Remove all tutorial card nodes in the document. We don't want these to display
     for card in doctree.traverse(tutorialcardnode):
         card.parent.remove(card)
@@ -175,7 +192,9 @@ def process_tutorial_card_nodes(app, doctree, fromdocname):
 
         node.replace_self(content)
 
+
 def setup(app: Sphinx) -> ExtensionMetadata:
+    """Set up the Sphinx extension."""
     app.add_node(tutoriallistnode)
     app.add_node(
         tutorialcardnode,
@@ -192,6 +211,6 @@ def setup(app: Sphinx) -> ExtensionMetadata:
     app.connect("doctree-resolved", process_tutorial_card_nodes)
 
     return {
-        'parallel_read_safe': True,
-        'parallel_write_safe': True,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
     }
