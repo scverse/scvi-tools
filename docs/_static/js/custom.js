ace42cca8caeab15ac52a10d7163dafd82ae11e2
Alexander Becker <beckera@berkeley.edu>
2025-03-26 16:13:37 -0700

    add debugging console log for js

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,11 +1,17 @@
+console.log("check card containers:", $(".card-container").length);
+console.log("check filter menus:", $(".filter-menu").length);
+
 // Create a set of all unique tags that appear in the cards
 function getUniqueTags() {
     let tagSet = new Set();
+    console.log("getUniqueTags is running"); // Debugging log
 
     $(".card-container").each(function() {
         let tags = $(this).data('tags').split(",")
         .map(tag => tag.trim()) // Get rid of whitespace
         .filter(tag => tag !== ""); // Make sure there are no empty tags
+
+        console.log("Tags found:", tags); // See if tags are being detected
 
         tags.forEach(tag => tagSet.add(tag)); // Add tag to the set
     });
@@ -25,4 +31,9 @@ function createMenu() {
     });
 }
 
-createMenu();
+$(document).ready(function() {
+    createMenu();
+});
+
+
+// Now add filtering functionality to the tag buttons
