b98c4adca6e92f7256dd8ec665f64680a845d07d
Alexander Becker <beckera@berkeley.edu>
2025-03-28 10:20:42 -0700

    debug button clicking not working

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,17 +1,11 @@
-console.log("check card containers:", $(".card-container").length);
-console.log("check filter menus:", $(".filter-menu").length);
-
 // Create a set of all unique tags that appear in the cards
 function getUniqueTags() {
     let tagSet = new Set();
-    console.log("getUniqueTags is running"); // Debugging log
 
     $(".card-container").each(function() {
         let tags = $(this).data('tags').split(",")
         .map(tag => tag.trim()) // Get rid of whitespace
         .filter(tag => tag !== ""); // Make sure there are no empty tags
-
-        console.log("Tags found:", tags); // See if tags are being detected
 
         tags.forEach(tag => tagSet.add(tag)); // Add tag to the set
     });
@@ -41,6 +35,7 @@ let selectedTagSet = new Set();
 
 // Handle tag button selection and filtering
 $(".filter-btn").on("click", function () {
+    console.log("button clicked!"); // for debugging
     let parent = $(this).closest(".filter-menu");
     let tag = $(this).data("tag");
 
