21a5d379467ae50850e6ef25159ec9bb5c8858db
Alexander Becker <beckera@berkeley.edu>
2025-04-28 16:29:28 -0700

    implement MRVAE in vanilla torch

--- src/scvi/external/mrvi/_module.py
+++ src/scvi/external/mrvi/_module.py
@@ -5,7 +5,6 @@ from typing import TYPE_CHECKING
 
 # import flax.linen as nn
 import jax
-import jax.numpy as jnp
 import torch
 import torch.distributions as dist
 import torch.nn as nn
@@ -14,11 +13,12 @@ import torch.nn.init as init
 from scvi import REGISTRY_KEYS, settings
 from scvi.distributions import NegativeBinomial
 from scvi.external.mrvi._components import AttentionBlock
-from scvi.module.base import JaxBaseModuleClass, LossOutput, flax_configure
+
+# from scvi.module.base import JaxBaseModuleClass, LossOutput, flax_configure
+from scvi.module.base import BaseModuleClass, LossOutput, auto_move_data
 
 if TYPE_CHECKING:
     from collections.abc import Callable
-    from typing import Any
 
 DEFAULT_PX_KWARGS = {
     "n_hidden": 32,
@@ -321,79 +321,52 @@ class EncoderXU(nn.Module):
         return self.normal_dist_output(inputs, training=training)
 
 
-@flax_configure
-class MRVAE(JaxBaseModuleClass):
-    """Multi-resolution Variational Inference (MrVI) module.
+class MRVAE(BaseModuleClass):
+    def __init__(
+        self,
+        n_input: int,
+        n_sample: int,
+        n_batch: int,
+        n_labels: int,
+        n_latent: int = 30,
+        n_latent_u: int = 10,
+        encoder_n_hidden: int = 128,
+        encoder_n_layers: int = 2,
+        z_u_prior: bool = True,
+        z_u_prior_scale: float = 0.0,
+        u_prior_scale: float = 0.0,
+        u_prior_mixture: bool = True,
+        u_prior_mixture_k: int = 20,
+        learn_z_u_prior_scale: bool = False,
+        scale_observations: bool = False,
+        px_kwargs: dict | None = None,
+        qz_kwargs: dict | None = None,
+        qu_kwargs: dict | None = None,
+        training: bool = True,
+        n_obs_per_sample: torch.Tensor | None = None,
+    ):
+        super().__init__()
+        self.n_input = n_input
+        self.n_sample = n_sample
+        self.n_batch = n_batch
+        self.n_labels = n_labels
+        self.n_latent = n_latent
+        self.n_latent_u = n_latent_u
+        self.encoder_n_hidden = encoder_n_hidden
+        self.encoder_n_layers = encoder_n_layers
+        self.z_u_prior = z_u_prior
+        self.z_u_prior_scale = z_u_prior_scale
+        self.u_prior_scale = u_prior_scale
+        self.u_prior_mixture = u_prior_mixture
+        self.u_prior_mixture_k = u_prior_mixture_k
+        self.learn_z_u_prior_scale = learn_z_u_prior_scale
+        self.scale_observations = scale_observations
+        self.px_kwargs = px_kwargs
+        self.qz_kwargs = qz_kwargs
+        self.qu_kwargs = qu_kwargs
+        self.training = training
+        self.n_obs_per_sample = n_obs_per_sample
 
-    Parameters
-    ----------
-    n_input
-        Number of input features.
-    n_sample
-        Number of samples.
-    n_batch
-        Number of batches.
-    n_labels
-        Number of labels.
-    n_latent
-        Number of latent variables for ``z``.
-    n_latent_u
-        Number of latent variables for ``u``.
-    encoder_n_hidden
-        Number of hidden units in the encoder.
-    encoder_n_layers
-        Number of layers in the encoder.
-    z_u_prior
-        Whether to place a Gaussian prior on ``z`` given ``u``.
-    z_u_prior_scale
-        Natural log of the scale parameter of the Gaussian prior placed on ``z`` given ``u``. Only
-        applies of ``learn_z_u_prior_scale`` is ``False``.
-    u_prior_scale
-        Natural log of the scale parameter of the Gaussian prior placed on ``u``. If
-        ``u_prior_mixture`` is ``True``, this scale applies to each mixture component distribution.
-    u_prior_mixture
-        Whether to use a mixture of Gaussians prior for ``u``.
-    u_prior_mixture_k
-        Number of mixture components to use for the mixture of Gaussians prior on ``u``.
-    learn_z_u_prior_scale
-        Whether to learn the scale parameter of the prior distribution of ``z`` given ``u``.
-    scale_observations
-        Whether to scale the loss associated with each observation by the total number of
-        observations linked to the associated sample.
-    px_kwargs
-        Keyword arguments for the generative model.
-    qz_kwargs
-        Keyword arguments for the inference model from ``u`` to ``z``.
-    qu_kwargs
-        Keyword arguments for the inference model from ``x`` to ``u``.
-    training
-        Whether the model is in training mode.
-    n_obs_per_sample
-        Number of observations per sample.
-    """
-
-    n_input: int
-    n_sample: int
-    n_batch: int
-    n_labels: int
-    n_latent: int = 30
-    n_latent_u: int = 10
-    encoder_n_hidden: int = 128
-    encoder_n_layers: int = 2
-    z_u_prior: bool = True
-    z_u_prior_scale: float = 0.0
-    u_prior_scale: float = 0.0
-    u_prior_mixture: bool = True
-    u_prior_mixture_k: int = 20
-    learn_z_u_prior_scale: bool = False
-    scale_observations: bool = False
-    px_kwargs: dict | None = None
-    qz_kwargs: dict | None = None
-    qu_kwargs: dict | None = None
-    training: bool = True
-    n_obs_per_sample: jax.typing.ArrayLike | None = None
-
-    def setup(self):
         px_kwargs = DEFAULT_PX_KWARGS.copy()
         if self.px_kwargs is not None:
             px_kwargs.update(self.px_kwargs)
@@ -444,7 +417,7 @@ class MRVAE(JaxBaseModuleClass):
         )
 
         if self.learn_z_u_prior_scale:
-            self.pz_scale = self.param("pz_scale", nn.initializers.zeros, (self.n_latent,))
+            self.pz_scale = nn.Parameter(torch.zeros(self.n_latent))
         else:
             self.pz_scale = self.z_u_prior_scale
 
@@ -453,42 +426,34 @@ class MRVAE(JaxBaseModuleClass):
                 u_prior_mixture_k = self.n_labels
             else:
                 u_prior_mixture_k = self.u_prior_mixture_k
+
             u_dim = self.n_latent_u if self.n_latent_u is not None else self.n_latent
-            self.u_prior_logits = self.param(
-                "u_prior_logits", nn.initializers.zeros, (u_prior_mixture_k,)
-            )
-            self.u_prior_means = self.param(
-                "u_prior_means", jax.random.normal, (u_prior_mixture_k, u_dim)
-            )
-            self.u_prior_scales = self.param(
-                "u_prior_scales", nn.initializers.zeros, (u_prior_mixture_k, u_dim)
-            )
+            self.u_prior_logits = nn.Parameter(torch.zeros(u_prior_mixture_k))
+            self.u_prior_means = nn.Parameter(
+                torch.randn(u_prior_mixture_k, u_dim)
+            )  # TODO: double check this
+            self.u_prior_scales = nn.Parameter(torch.zeros(u_prior_mixture_k, u_dim))
 
-    @property
-    def required_rngs(self):
-        return ("params", "u", "dropout", "eps")
-
-    def _get_inference_input(self, tensors: dict[str, jax.typing.ArrayLike]) -> dict[str, Any]:
+    def _get_inference_input(self, tensors: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
         x = tensors[REGISTRY_KEYS.X_KEY]
         sample_index = tensors[REGISTRY_KEYS.SAMPLE_KEY]
         return {"x": x, "sample_index": sample_index}
 
+    @auto_move_data  # TODO: what is this
     def inference(
         self,
-        x: jax.typing.ArrayLike,
-        sample_index: jax.typing.ArrayLike,
+        x: torch.Tensor,
+        sample_index: torch.Tensor,
         mc_samples: int | None = None,
-        cf_sample: jax.typing.ArrayLike | None = None,
+        cf_sample: torch.Tensor | None = None,
         use_mean: bool = False,
-    ) -> dict[str, jax.Array | dist.Distribution]:
-        """Latent variable inference."""
+    ) -> dict[str, torch.Tensor]:
         qu = self.qu(x, sample_index, training=self.training)
         if use_mean:
             u = qu.mean
         else:
-            u_rng = self.make_rng("u")
             sample_shape = (mc_samples,) if mc_samples is not None else ()
-            u = qu.rsample(u_rng, sample_shape=sample_shape)
+            u = qu.rsample(sample_shape=sample_shape)
 
         sample_index_cf = sample_index if cf_sample is None else cf_sample
 
@@ -498,10 +463,10 @@ class MRVAE(JaxBaseModuleClass):
         qeps = None
         if qeps_.shape[-1] == 2 * self.n_latent:
             loc_, scale_ = qeps_[..., : self.n_latent], qeps_[..., self.n_latent :]
-            qeps = dist.Normal(loc_, nn.softplus(scale_) + 1e-3)
-            eps = qeps.mean if use_mean else qeps.rsample(self.make_rng("eps"))
+            qeps = dist.Normal(loc_, nn.functional.softplus(scale_) + 1e-3)
+            eps = qeps.mean if use_mean else qeps.rsample()
         z = z_base + eps
-        library = jnp.log(x.sum(1, keepdims=True))
+        library = torch.log(x.sum(1, keepdims=True))
 
         return {
             "qu": qu,
@@ -515,9 +480,9 @@ class MRVAE(JaxBaseModuleClass):
 
     def _get_generative_input(
         self,
-        tensors: dict[str, jax.typing.ArrayLike],
-        inference_outputs: dict[str, jax.Array | dist.Distribution],
-    ) -> dict[str, jax.Array]:
+        tensors: dict[str, torch.Tensor],
+        inference_outputs: dict[str, torch.Tensor],
+    ) -> dict[str, torch.Tensor]:
         z = inference_outputs["z"]
         library = inference_outputs["library"]
         batch_index = tensors[REGISTRY_KEYS.BATCH_KEY]
@@ -529,15 +494,16 @@ class MRVAE(JaxBaseModuleClass):
             "label_index": label_index,
         }
 
+    @auto_move_data  # TODO: what is this
     def generative(
         self,
-        z: jax.typing.ArrayLike,
-        library: jax.typing.ArrayLike,
-        batch_index: jax.typing.ArrayLike,
-        label_index: jax.typing.ArrayLike,
-    ) -> dict[str, jax.Array | dist.Distribution]:
+        z: torch.Tensor,
+        library: torch.Tensor,
+        batch_index: torch.Tensor,
+        label_index: torch.Tensor,
+    ) -> dict[str, torch.Tensor | dist.Distribution]:
         """Generative model."""
-        library_exp = jnp.exp(library)
+        library_exp = torch.exp(library)
         px = self.px(
             z,
             batch_index,
@@ -548,22 +514,26 @@ class MRVAE(JaxBaseModuleClass):
 
         if self.u_prior_mixture:
             offset = (
-                10.0 * jax.nn.one_hot(label_index, self.n_labels) if self.n_labels >= 2 else 0.0
+                10.0 * nn.functional.one_hot(label_index, self.n_labels)
+                if self.n_labels >= 2
+                else 0.0
             )
             cats = dist.Categorical(logits=self.u_prior_logits + offset)
-            normal_dists = dist.Normal(self.u_prior_means, jnp.exp(self.u_prior_scales)).to_event(
+            normal_dists = dist.Normal(
+                self.u_prior_means, torch.exp(self.u_prior_scales)
+            ).to_event(  # TODO: what is to_event
                 1
             )
             pu = dist.MixtureSameFamily(cats, normal_dists)
         else:
-            pu = dist.Normal(0, jnp.exp(self.u_prior_scale))
+            pu = dist.Normal(0, torch.exp(self.u_prior_scale))
         return {"px": px, "pu": pu, "h": h}
 
     def loss(
         self,
-        tensors: dict[str, jax.typing.ArrayLike],
-        inference_outputs: dict[str, jax.Array | dist.Distribution],
-        generative_outputs: dict[str, jax.Array | dist.Distribution],
+        tensors: dict[str, torch.Tensor],
+        inference_outputs: dict[str, torch.Tensor],
+        generative_outputs: dict[str, torch.Tensor],
         kl_weight: float = 1.0,
     ) -> LossOutput:
         """Compute the loss function value."""
@@ -581,7 +551,7 @@ class MRVAE(JaxBaseModuleClass):
         kl_z = 0.0
         eps = inference_outputs["z"] - inference_outputs["z_base"]
         if self.z_u_prior:
-            peps = dist.Normal(0, jnp.exp(self.pz_scale))
+            peps = dist.Normal(0, torch.exp(self.pz_scale))
             kl_z = -peps.log_prob(eps).sum(-1)
 
         weighted_kl_local = kl_weight * (kl_u + kl_z)
@@ -592,7 +562,7 @@ class MRVAE(JaxBaseModuleClass):
             prefactors = self.n_obs_per_sample[sample_index]
             loss = loss / prefactors
 
-        loss = jnp.mean(loss)
+        loss = torch.mean(loss)
 
         return LossOutput(
             loss=loss,
@@ -602,15 +572,15 @@ class MRVAE(JaxBaseModuleClass):
 
     def compute_h_from_x_eps(
         self,
-        x: jax.typing.ArrayLike,
-        sample_index: jax.typing.ArrayLike,
-        batch_index: jax.typing.ArrayLike,
+        x: torch.Tensor,
+        sample_index: torch.Tensor,
+        batch_index: torch.Tensor,
         extra_eps: float,
-        cf_sample: jax.typing.ArrayLike | None = None,
+        cf_sample: torch.Tensor | None = None,
         mc_samples: int = 10,
     ):
         """Compute normalized gene expression from observations using predefined eps"""
-        library = 7.0 * jnp.ones_like(
+        library = 7.0 * torch.ones_like(
             sample_index
         )  # placeholder, has no effect on the value of h.
         inference_outputs = self.inference(
@@ -620,7 +590,7 @@ class MRVAE(JaxBaseModuleClass):
             "z": inference_outputs["z_base"] + extra_eps,
             "library": library,
             "batch_index": batch_index,
-            "label_index": jnp.zeros([x.shape[0], 1]),
+            "label_index": torch.zeros([x.shape[0], 1]),
         }
         generative_outputs = self.generative(**generative_inputs)
         return generative_outputs["h"]
