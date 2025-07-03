d1d2677a3ff91b743314522e259f3bc30e5fb98f
florianingelfinger <florian@ingelfinger.eu>
2025-07-03 14:25:55 +0300

    ppc compatibility with non-count data, store FA samples, methods for mean and std, wrapper for computation of metrices

--- src/scvi/criticism/_constants.py
+++ src/scvi/criticism/_constants.py
@@ -4,11 +4,17 @@ METRIC_DIFF_EXP = "diff_exp"
 METRIC_CALIBRATION = "calibration"
 METRIC_ZERO_FRACTION = "zero_fraction"
 
+METRIC_MEAN_CELL = "mean_cell"
+METRIC_MEAN_GENE = "mean_gene"
+METRIC_STD_CELL = "std_cell"
+METRIC_STD_GENE = "std_gene"
+
 DEFAULT_DE_N_TOP_GENES = 2
 DEFAULT_DE_P_VAL_THRESHOLD = 0.001
 DEFAULT_DE_N_TOP_GENES_OVERLAP = 100
 
 DATA_VAR_RAW = "Raw"
+FA_VAR = "FA"
 
 UNS_NAME_RGG_RAW = "rank_genes_groups_raw"
 UNS_NAME_RGG_PPC = "rank_genes_groups_ppc"
