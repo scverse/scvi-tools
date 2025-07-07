bdda0f9e62d85929d8070f6cb36837fe1a0d7d29
ori-kron-wis <ori.kronfeld@weizmann.ac.il>
2025-07-07 11:22:53 +0300

    git hooks try

--- tests/external/cytovi/test_cytovi.py
+++ tests/external/cytovi/test_cytovi.py
@@ -1,60 +1,49 @@
+import numpy as np
 import pytest
 
 from scvi.data import synthetic_iid
 from scvi.external import cytovi
 
-
-from anndata import AnnData
-import numpy as np
-
-
-
-RAW_LAYER_KEY = 'raw'
-SCALED_LAYER_KEY = 'scaled'
-NAN_LAYER_KEY = '_nan_mask'
-RNA_LAYER_KEY = 'rna'
-LATENT_REP_KEY = 'X_CytoVI'
-BATCH_KEY = 'batch'
-LABELS_KEY = 'labels'
-SAMPLE_KEY = 'sample_key'
+RAW_LAYER_KEY = "raw"
+SCALED_LAYER_KEY = "scaled"
+NAN_LAYER_KEY = "_nan_mask"
+RNA_LAYER_KEY = "rna"
+LATENT_REP_KEY = "X_CytoVI"
+BATCH_KEY = "batch"
+LABELS_KEY = "labels"
+SAMPLE_KEY = "sample_key"
 N_EPOCHS = 2
+
 
 @pytest.fixture(scope="session")
 def adata():
-    adata = synthetic_iid(batch_size=256,
-                       n_genes=30,
-                       n_proteins=0,
-                       n_regions=0,
-                       n_batches=2,
-                       n_labels=10)
+    adata = synthetic_iid(
+        batch_size=256, n_genes=30, n_proteins=0, n_regions=0, n_batches=2, n_labels=10
+    )
 
     adata.layers[RAW_LAYER_KEY] = adata.X.copy()
-    adata.obs[SAMPLE_KEY] = np.random.choice(['group_a', 'group_b'], size=adata.shape[0])
+    adata.obs[SAMPLE_KEY] = np.random.choice(["group_a", "group_b"], size=adata.shape[0])
     return adata
+
 
 @pytest.fixture(scope="session")
 def overlapping_adatas():
-    adata1 = synthetic_iid(batch_size=256,
-                       n_genes=30,
-                       n_proteins=0,
-                       n_regions=0,
-                       n_batches=1,
-                       n_labels=10)
+    adata1 = synthetic_iid(
+        batch_size=256, n_genes=30, n_proteins=0, n_regions=0, n_batches=1, n_labels=10
+    )
 
-    adata2 = synthetic_iid(batch_size=256,
-                       n_genes=20,
-                       n_proteins=0,
-                       n_regions=0,
-                       n_batches=1,
-                       n_labels=10)
+    adata2 = synthetic_iid(
+        batch_size=256, n_genes=20, n_proteins=0, n_regions=0, n_batches=1, n_labels=10
+    )
 
     adata1.layers[RAW_LAYER_KEY] = adata1.X.copy()
     adata2.layers[RAW_LAYER_KEY] = adata2.X.copy()
 
-    adata1.obs_names = 'adata1_' + adata1.obs_names
-    adata2.obs_names = 'adata2_' + adata2.obs_names
+    adata1.obs_names = "adata1_" + adata1.obs_names
+    adata2.obs_names = "adata2_" + adata2.obs_names
 
     return adata1, adata2
+
 
 def test_cytovi_preprocess(adata, overlapping_adatas):
     cytovi.logp(adata)
@@ -71,6 +60,7 @@ def test_cytovi_preprocess(adata, overlapping_adatas):
     adata_merged = cytovi.merge_batches([adata1, adata2])
     assert NAN_LAYER_KEY in adata_merged.layers
 
+
 def test_cytovi_plotting(adata):
     cytovi.biaxial(adata, layer_key=RAW_LAYER_KEY, marker_x=adata.var_names[0])
     cytovi.histogram(adata, layer_key=RAW_LAYER_KEY)
@@ -80,15 +70,16 @@ def test_cytovi(adata):
     cytovi.arcsinh(adata)
     cytovi.scale(adata)
 
-    cytovi.CYTOVI.setup_anndata(adata,
-                                layer=SCALED_LAYER_KEY,
-                                batch_key=BATCH_KEY,
-                                sample_key=SAMPLE_KEY,
-                                )
+    cytovi.CYTOVI.setup_anndata(
+        adata,
+        layer=SCALED_LAYER_KEY,
+        batch_key=BATCH_KEY,
+        sample_key=SAMPLE_KEY,
+    )
 
     model = cytovi.CYTOVI(adata)
 
-    model.train(max_epochs= N_EPOCHS)
+    model.train(max_epochs=N_EPOCHS)
     assert model.is_trained
 
     latent = model.get_latent_representation()
@@ -101,20 +92,19 @@ def test_cytovi(adata):
     da_res = model.differential_abundance()
     assert da_res.shape == (adata.n_obs, adata.obs[SAMPLE_KEY].nunique())
 
-    model.differential_expression(groupby = SAMPLE_KEY)
+    model.differential_expression(groupby=SAMPLE_KEY)
 
     # test label informed prior
-    cytovi.CYTOVI.setup_anndata(adata,
-                                layer=SCALED_LAYER_KEY,
-                                batch_key=BATCH_KEY,
-                                sample_key=SAMPLE_KEY,
-                                labels_key=LABELS_KEY
-                                )
+    cytovi.CYTOVI.setup_anndata(
+        adata,
+        layer=SCALED_LAYER_KEY,
+        batch_key=BATCH_KEY,
+        sample_key=SAMPLE_KEY,
+        labels_key=LABELS_KEY,
+    )
 
     model = cytovi.CYTOVI(adata)
-    model.train(max_epochs= N_EPOCHS)
-
-
+    model.train(max_epochs=N_EPOCHS)
 
 
 def test_cytovi_overlapping(overlapping_adatas):
@@ -125,14 +115,15 @@ def test_cytovi_overlapping(overlapping_adatas):
     cytovi.scale(adata2)
     adata_merged = cytovi.merge_batches([adata1, adata2])
 
-    cytovi.CYTOVI.setup_anndata(adata_merged,
-                                layer=SCALED_LAYER_KEY,
-                                batch_key=BATCH_KEY,
-                                )
+    cytovi.CYTOVI.setup_anndata(
+        adata_merged,
+        layer=SCALED_LAYER_KEY,
+        batch_key=BATCH_KEY,
+    )
 
     model = cytovi.CYTOVI(adata_merged)
 
-    model.train(max_epochs= N_EPOCHS)
+    model.train(max_epochs=N_EPOCHS)
     assert model.is_trained
 
     imp_exp = model.get_normalized_expression()
@@ -144,11 +135,12 @@ def test_cytovi_overlapping(overlapping_adatas):
     adata2.obsm[LATENT_REP_KEY] = np.random.randint(0, 1, (adata2.shape[0], model.module.n_latent))
     model_query = cytovi.CYTOVI.load_query_data(adata1, model)
     model_query.is_trained = True
-    imp_cats = model_query.impute_categories_from_reference(adata2, cat_key = LABELS_KEY)
+    imp_cats = model_query.impute_categories_from_reference(adata2, cat_key=LABELS_KEY)
     assert imp_cats.shape[0] == adata1.n_obs
 
     # test RNA imputation
     adata2.layers[RNA_LAYER_KEY] = np.random.randint(0, 100, size=adata2.shape)
-    adata_imp_rna = model.impute_rna_from_reference(reference_batch = '1', adata_rna = adata2, layer_key=RNA_LAYER_KEY)
+    adata_imp_rna = model.impute_rna_from_reference(
+        reference_batch="1", adata_rna=adata2, layer_key=RNA_LAYER_KEY
+    )
     assert adata_imp_rna.shape == (adata_merged.shape[0], adata2.n_vars)
-
