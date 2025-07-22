e3c12c1adef2d3dcfe73495c9cdf2291afb48426
ori-kron-wis <ori.kronfeld@weizmann.ac.il>
2025-07-22 15:37:15 +0300

    add SupervisedModuleClass to the classifier and test for save and load SOLO model

--- src/scvi/external/methylvi/_methylanvi_model.py
+++ src/scvi/external/methylvi/_methylanvi_model.py
@@ -60,7 +60,7 @@ class METHYLANVI(VAEMixin, SemisupervisedTrainingMixin, BSSeqMixin, ArchesMixin,
         If ``True``, uses a single linear layer for classification instead of a
         multi-layer perceptron.
     **model_kwargs
-        Keyword args for :class:`~scvi.module.SCANVAE`
+        Keyword args for :class:`~scvi.module.METHYLANVAE`
 
     Examples
     --------
