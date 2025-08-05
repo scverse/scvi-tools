52f8e2e1a750145cc5a10e7dbd6bc92135aa2a45
ori-kron-wis <ori.kronfeld@weizmann.ac.il>
2025-08-05 12:26:21 +0300

    update for jax tests functions

--- tests/external/mrvi_jax/test_jaxmrvi_components.py
+++ tests/external/mrvi_jax/test_jaxmrvi_components.py
@@ -1,7 +1,9 @@
+import flax.linen as nn
 import jax
 import jax.numpy as jnp
 
 from scvi.external.mrvi_jax._components import (
+    MLP,
     AttentionBlock,
     ConditionalNormalization,
     Dense,
@@ -15,24 +17,36 @@ def test_jaxmrvi_dense():
     x = jnp.ones((20, 10))
     dense = Dense(10)
     params = dense.init(key, x)
-    dense.apply(params, x)
+    output = dense.apply(params, x)
+    assert output.shape == (20, 10)
 
 
 def test_jaxmrvi_resnetblock():
     key = jax.random.PRNGKey(0)
     x = jnp.ones((20, 10))
-    block = ResnetBlock(10, 30, training=True)
+    block = ResnetBlock(n_out=30, n_hidden=128, training=True)
     params = block.init(key, x)
-    block.apply(params, x, mutable=["batch_stats"])
+    output = block.apply(params, x, mutable=["batch_stats"])
+    assert output[0].shape == (20, 30)
 
 
 def test_jaxmrvi_normalnn():
     key = jax.random.PRNGKey(0)
     key, subkey = jax.random.split(key)
     x = jnp.ones((20, 10))
-    nn = NormalDistOutputNN(10, 30, 3, training=True)
+    nn = NormalDistOutputNN(n_out=30, n_hidden=128, n_layers=3, training=True)
     params = nn.init(key, x)
-    nn.apply(params, x, mutable=["batch_stats"])
+    output = nn.apply(params, x, mutable=["batch_stats"])
+    assert output[0].batch_shape == (20, 30)
+
+
+def test_jaxmrvi_mlp():
+    key = jax.random.PRNGKey(0)
+    x = jnp.ones((20, 10))
+    mlp = MLP(n_out=30, n_hidden=128, n_layers=3, activation=nn.relu, training=True)
+    params = mlp.init(key, x)
+    output = mlp.apply(params, x, mutable=["batch_stats"])
+    assert output[0].shape == (20, 30)
 
 
 def test_jaxmrvi_conditionalbatchnorm1d():
@@ -40,10 +54,11 @@ def test_jaxmrvi_conditionalbatchnorm1d():
     x = jnp.ones((20, 10))
     y = jnp.ones((20, 1))
     conditionalbatchnorm1d = ConditionalNormalization(
-        10, 3, normalization_type="batch", training=True
+        n_features=10, n_conditions=3, normalization_type="batch", training=True
     )
     params = conditionalbatchnorm1d.init(key, x, y)
-    conditionalbatchnorm1d.apply(params, x, y, mutable=["batch_stats"])
+    output = conditionalbatchnorm1d.apply(params, x, y, mutable=["batch_stats"])
+    assert output[0].shape == (20, 10)
 
 
 def test_jaxmrvi_attention():
