fc6c9be30adfb19db8cee2aec914a5e349dd6c7a
Alexander Becker <beckera@berkeley.edu>
2025-12-11 16:47:56 -0800

    tests: finish da tests

--- tests/model/test_differential_abundance.py
+++ tests/model/test_differential_abundance.py
@@ -1,29 +1,67 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+import numpy as np
+import pandas as pd
 import pytest
+import torch.distributions as dist
+
+from scvi.data import synthetic_iid
+from scvi.model import SCVI
+
+if TYPE_CHECKING:
+    from anndata import AnnData
 
 
-# TODO: use fixtures for models, anndata that will be used in tests
 @pytest.fixture(scope="session")
 def adata():
-    pass
+    adata = synthetic_iid(batch_size=500)
+    adata.obs["sample"] = np.random.choice(10, size=adata.n_obs)
+    adata.obs["sample_str"] = [chr(i + ord("a")) for i in adata.obs["sample"]]
+    return adata
 
 
-def model():
-    pass
+@pytest.fixture(scope="session")
+def model(adata):
+    SCVI.setup_anndata(adata=adata, batch_key="batch")
+    model = SCVI(adata)
+    model.train(max_epochs=1, train_size=0.5)
+    return model
 
 
-@pytest.mark.parametrize
-def test_get_aggregated_posterior():
-    pass
+@pytest.mark.parametrize(
+    "ap_kwargs",
+    [
+        {},
+        {"indices": None},
+        {"indices": []},
+        {"indices": np.random.choice(1500, 100, replace=False)},
+        {"indices": list(range(150)), "dof": None},
+        {"indices": list(range(150)), "dof": 5},
+    ],
+)
+def test_get_aggregated_posterior(model: SCVI, adata: AnnData, ap_kwargs):
+    ap = model.get_aggregated_posterior(adata, **ap_kwargs)
+    assert isinstance(ap, dist.Distribution)
+
+    subset_indices = np.random.choice(adata.n_obs, adata.n_obs // 2, replace=False)
+    adata_subset = adata[subset_indices, :].copy()
+    ap = model.get_aggregated_posterior(adata_subset, **ap_kwargs)
+    assert isinstance(ap, dist.Distribution)
 
 
-# TODO: use parametrize to test multiple values for the parameters
-@pytest.mark.parametrize
+@pytest.mark.parametrize(
+    "da_kwargs",
+    [
+        {"adata": adata, "sample_key": "sample_str", "num_cells_posterior": 1000, "dof": 3},
+    ],
+)
+def test_differential_abundance(model: SCVI, adata: AnnData, da_kwargs):
+    model.differential_abundance(adata, **da_kwargs)
+    assert isinstance(adata.obsm["da_log_probs"], pd.DataFrame)
 
-# TODO: define the actual test
-def test_differential_abundance():
-    pass
-
-
-# TODO: repeat the above 2 steps as many times as needed (may actually only need one test)
+    subset_indices = np.random.choice(adata.n_obs, adata.n_obs // 2, replace=False)
+    adata_subset = adata[subset_indices, :].copy()
+    model.differential_abundance(adata_subset, **da_kwargs)
+    assert isinstance(adata_subset.obsm["da_log_probs"], pd.DataFrame)
