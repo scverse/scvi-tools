dd7f0a14c9f5b6d74b68e6f85de05c49e387b08b
Michel Tarnow <m.tarnow00@gmail.com>
2026-01-28 12:59:16 +0100

    Add comments to DecoderSinglePathway and DecoderDualPathway for clarity

--- src/scvi/external/diagvi/_base_components.py
+++ src/scvi/external/diagvi/_base_components.py
@@ -97,6 +97,7 @@ class DecoderSinglePathway(nn.Module):
             px_rate = torch.exp(l) * px_scale
         else:
             # Continuous data: no normalization, scale is positive, rate is unbounded
+            # inspired by CytoVI
             px_scale = F.softplus(raw_px_scale) + EPS
             px_rate = raw_px_scale
 
@@ -199,6 +200,7 @@ class DecoderDualPathway(nn.Module):
             px_rate_2 = torch.exp(l) * px_scale_2
         else:
             # Continuous data: no normalization, scales are positive, rates are unbounded
+            # inspired by CytoVI
             px_scale_1 = F.softplus(raw_px_scale_1) + EPS
             px_scale_2 = F.softplus(raw_px_scale_2) + EPS
             px_rate_1 = raw_px_scale_1
