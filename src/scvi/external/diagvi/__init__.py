73a1a483a35abf5ca065a3db86f9e47910504c5f
Michel Tarnow <m.tarnow00@gmail.com>
2026-02-13 10:14:49 +0100

    Remove plotting and preprocessing utilities

--- src/scvi/external/diagvi/__init__.py
+++ src/scvi/external/diagvi/__init__.py
@@ -3,7 +3,5 @@
 from ._model import DIAGVI
 from ._module import DIAGVAE
 from ._task import DiagTrainingPlan
-from ._plotting import plot_histogram
-from ._preprocessing import scale, transform_arcsinh
 
 __all__ = ["DIAGVI", "DIAGVAE", "DiagTrainingPlan"]
