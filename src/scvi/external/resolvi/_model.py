db0900c86a491b3cfb3e918dafb0db5aca022429
Ori Kronfeld <ori.kronfeld@weizmann.ac.il>
2026-05-13 19:05:11 +0300

    feat: support for rapids singlecell in scvi-tools (#3811)

--- src/scvi/external/resolvi/_model.py
+++ src/scvi/external/resolvi/_model.py
@@ -360,7 +360,7 @@ class RESOLVI(
         if slice_key is not None:
             batch_key = slice_key
         try:
-            import scanpy
+            import scanpy as sc
             from sklearn.neighbors._base import _kneighbors_from_graph
         except ImportError as err:
             raise ImportError(
@@ -381,14 +381,12 @@ class RESOLVI(
         for index in indices:
             sub_data = adata[index].copy()
             try:
-                import rapids_singlecell
+                import rapids_singlecell as rsc
 
                 print("RAPIDS SingleCell is installed and can be imported")
-                rapids_singlecell.pp.neighbors(
-                    sub_data, n_neighbors=n_neighbors + 5, use_rep=spatial_rep
-                )
+                rsc.pp.neighbors(sub_data, n_neighbors=n_neighbors + 5, use_rep=spatial_rep)
             except ImportError:
-                scanpy.pp.neighbors(sub_data, n_neighbors=n_neighbors + 5, use_rep=spatial_rep)
+                sc.pp.neighbors(sub_data, n_neighbors=n_neighbors + 5, use_rep=spatial_rep)
             distances = sub_data.obsp["distances"] ** 2
 
             distance_neighbor[index, :], index_neighbor_batch = _kneighbors_from_graph(
