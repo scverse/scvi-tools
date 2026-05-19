3afa792a9388d45d1f94d42896b104a74e169270
Oier Etxezarreta <oier.etxezarreta@weizmann.ac.il>
2026-05-19 12:35:15 +0300

    refactor: replace tempfile/scanpy backup_url with pooch S3 downloads in datasets.py

--- src/scvi/external/harreman/datasets/datasets.py
+++ src/scvi/external/harreman/datasets/datasets.py
@@ -1,16 +1,20 @@
-import os
-import tempfile
-
-import scanpy as sc
-
-temp_dir_obj = tempfile.TemporaryDirectory()
+import anndata
+import pooch
 
 
 def load_visium_mouse_colon_dataset(
     sample: str | None = None,
-) -> "sc.AnnData":
+    save_path: str = "data/",
+) -> anndata.AnnData:
     """
-    Load the mouse colon 10x Visium dataset.
+    Load the mouse colon 10x Visium dataset (Parigi et al.).
+
+    Parameters
+    ----------
+    sample : str, optional
+        Sample to load. One of "d0", "d14". If None, loads the full unrolled dataset.
+    save_path : str, optional
+        Path to save the downloaded dataset.
 
     Returns
     -------
@@ -18,32 +22,47 @@ def load_visium_mouse_colon_dataset(
         The loaded 10x Visium dataset.
     """
     dataset_prefix = "Parigi_et_al_mouse_colon"
+    S3_BASE = "https://scverse-public-data.s3.eu-central-1.amazonaws.com/scvi-tools/harreman/datasets"
 
-    samples_path_dict = {
-        "d0": "https://figshare.com/ndownloader/files/59325113",
-        "d14": "https://figshare.com/ndownloader/files/59325116",
+    samples_info = {
+        "d0": (f"{dataset_prefix}_d0.h5ad", None),
+        "d14": (f"{dataset_prefix}_d14.h5ad", None),
     }
 
-    if sample:
-        if sample not in samples_path_dict.keys():
-            raise ValueError(f'"sample" needs to be one of: {list(samples_path_dict.keys())}')
-        else:
-            adata_path = os.path.join(temp_dir_obj.name, f"{dataset_prefix}_{sample}.h5ad")
-            backup_url = samples_path_dict[sample]
+    if sample is not None:
+        if sample not in samples_info:
+            raise ValueError(f'"sample" needs to be one of: {list(samples_info.keys())}')
+        fname, known_hash = samples_info[sample]
     else:
-        adata_path = os.path.join(temp_dir_obj.name, f"{dataset_prefix}_unrolled.h5ad")
-        backup_url = "https://figshare.com/ndownloader/files/59325119"
+        fname = f"{dataset_prefix}_unrolled.h5ad"
+        known_hash = None
 
-    adata = sc.read(adata_path, backup_url=backup_url)
+    path = pooch.retrieve(
+        url=f"{S3_BASE}/{fname}",
+        known_hash=known_hash,
+        fname=fname,
+        path=save_path,
+        progressbar=True,
+    )
 
-    return adata
+    return anndata.read_h5ad(path)
 
 
 def load_slide_seq_human_lung_dataset(
     sample: str | None = None,
-) -> "sc.AnnData":
+    save_path: str = "data/",
+) -> anndata.AnnData:
     """
-    Load the human lung Slide-seq dataset.
+    Load the human lung Slide-seq dataset (Liu et al.).
+
+    Parameters
+    ----------
+    sample : str, optional
+        Sample to load. One of "Puck_200727_08", "Puck_200727_09", "Puck_200727_10",
+        "Puck_220408_13", "Puck_220408_14", "Puck_220408_15", "Puck_220408_20".
+        If None, loads the full combined dataset.
+    save_path : str, optional
+        Path to save the downloaded dataset.
 
     Returns
     -------
@@ -51,27 +70,32 @@ def load_slide_seq_human_lung_dataset(
         The loaded Slide-seq dataset.
     """
     dataset_prefix = "Liu_et_al_human_lung"
+    S3_BASE = "https://scverse-public-data.s3.eu-central-1.amazonaws.com/scvi-tools/harreman/datasets"
 
-    samples_path_dict = {
-        "Puck_200727_08": "https://figshare.com/ndownloader/files/59325098",
-        "Puck_200727_09": "https://figshare.com/ndownloader/files/59325092",
-        "Puck_200727_10": "https://figshare.com/ndownloader/files/59325095",
-        "Puck_220408_13": "https://figshare.com/ndownloader/files/59325101",
-        "Puck_220408_14": "https://figshare.com/ndownloader/files/59325104",
-        "Puck_220408_15": "https://figshare.com/ndownloader/files/59325107",
-        "Puck_220408_20": "https://figshare.com/ndownloader/files/59325110",
+    samples_info = {
+        "Puck_200727_08": (f"{dataset_prefix}_Puck_200727_08.h5ad", None),
+        "Puck_200727_09": (f"{dataset_prefix}_Puck_200727_09.h5ad", None),
+        "Puck_200727_10": (f"{dataset_prefix}_Puck_200727_10.h5ad", None),
+        "Puck_220408_13": (f"{dataset_prefix}_Puck_220408_13.h5ad", None),
+        "Puck_220408_14": (f"{dataset_prefix}_Puck_220408_14.h5ad", None),
+        "Puck_220408_15": (f"{dataset_prefix}_Puck_220408_15.h5ad", None),
+        "Puck_220408_20": (f"{dataset_prefix}_Puck_220408_20.h5ad", None),
     }
 
-    if sample:
-        if sample not in samples_path_dict.keys():
-            raise ValueError(f'"sample" needs to be one of: {list(samples_path_dict.keys())}')
-        else:
-            adata_path = os.path.join(temp_dir_obj.name, f"{dataset_prefix}_{sample}.h5ad")
-            backup_url = samples_path_dict[sample]
+    if sample is not None:
+        if sample not in samples_info:
+            raise ValueError(f'"sample" needs to be one of: {list(samples_info.keys())}')
+        fname, known_hash = samples_info[sample]
     else:
-        adata_path = os.path.join(temp_dir_obj.name, f"{dataset_prefix}.h5ad")
-        backup_url = "https://figshare.com/ndownloader/files/59325125"
+        fname = f"{dataset_prefix}.h5ad"
+        known_hash = None
 
-    adata = sc.read(adata_path, backup_url=backup_url)
+    path = pooch.retrieve(
+        url=f"{S3_BASE}/{fname}",
+        known_hash=known_hash,
+        fname=fname,
+        path=save_path,
+        progressbar=True,
+    )
 
-    return adata
+    return anndata.read_h5ad(path)
