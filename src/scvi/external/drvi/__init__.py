9b1843ec0977e5346e619ab69843d305369b824f
Ori Kronfeld <ori.kronfeld@weizmann.ac.il>
2026-07-09 20:14:35 +0300

    fix: Removed stacked-linear-from-DRVI (#3910)

--- src/scvi/external/drvi/__init__.py
+++ src/scvi/external/drvi/__init__.py
@@ -2,5 +2,13 @@ from scvi.external.drvi._base_components import DecoderDRVI, SplitFCLayers
 from scvi.external.drvi._distributions import LogNegativeBinomial
 from scvi.external.drvi._model import DRVI
 from scvi.external.drvi._module import DRVIModule
+from scvi.external.drvi._utils import StackedLinearLayer
 
-__all__ = ["DRVI", "DRVIModule", "DecoderDRVI", "SplitFCLayers", "LogNegativeBinomial"]
+__all__ = [
+    "DRVI",
+    "DRVIModule",
+    "DecoderDRVI",
+    "SplitFCLayers",
+    "LogNegativeBinomial",
+    "StackedLinearLayer",
+]
